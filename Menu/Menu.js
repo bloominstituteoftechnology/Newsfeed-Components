257d90747fd7bc4fe57dc2d3018234926048fc23
Josh Knell <bigknell@gmail.com>
2018-05-29 22:33:09 -0600

    Added instructions

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,4 +1,9 @@
+// create a reference to the ".menu" class
 const menu;
+// create a reference to the ".menu-button" class
 const menuButton;
+// Using your menuButton reference, add a click handler that calls toggleMenu
 
-const toggleMenu = () => {}
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence. 
+}
