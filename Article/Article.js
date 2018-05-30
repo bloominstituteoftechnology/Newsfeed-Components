ac1900d1b673eb893af7dab0b8f69b73b9110f2e
Tyson Gushiken <tysonism@gmail.com>
2018-05-30 16:04:08 -0700

    Add article expand and close animation for stretch task

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,21 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle);
+
   }
 
   expandArticle(e) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
     e.target.parentNode.classList.toggle('article-open');
+
+    if (e.target.parentNode.classList.contains('article-open')) {
+      e.target.textContent = 'Click to Close';
+    } else {
+      e.target.textContent = 'Click to Expand';
+    }
   }
 }
 
