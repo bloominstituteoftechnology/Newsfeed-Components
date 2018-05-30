24b182760b60690c2f94a9ff3949629c0ceb1779
snowcoding <vimalshah77@gmail.com>
2018-05-30 13:13:48 -0700

    update on expandButton to reference this.element and not document!!

--- Article/Article.js
+++ Article/Article.js
@@ -6,7 +6,7 @@ class Article {
     this.element = element
 
     // create a reference to the ".expandButton" class. 
-    this.expandButton = Array.from(document.querySelectorAll('.expandButton'))
+    this.expandButton = Array.from(this.element.querySelectorAll('.expandButton'))
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.forEach( span => span.innerHTML = 'expand')
