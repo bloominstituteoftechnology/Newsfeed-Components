938fddc785d2ba825efcd328735bcbd280bebfa3
Austin Merando <ajmerando1@yahoo.com>
2018-05-30 15:43:05 -0700

    completed project, 1 stretch complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,15 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-}
+  menu.classList.toggle("menu--open");
+  console.log('hello out there');
+};
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+ menuButton.addEventListener("click", toggleMenu);
+
+ TweenLite.to(document.querySelector(".header"), 1, {width:"700px", height:"50px"});
