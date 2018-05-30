de91af7162f4ee082483fde39a433a74536e389d
Eric B <timberwolf.eric@gmail.com>
2018-05-30 17:54:55 -0500

    Fixed Menu.js typo & working to refactor expandArticle function in Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = this.element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
@@ -14,7 +14,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    
+
   }
 }
 
