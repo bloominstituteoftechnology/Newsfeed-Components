fee2a3d0f6f6868025f8fbb80c4a9506e6e6e8eb
Wdifulvio523 <w.difulvio523@gmail.com>
2018-05-30 17:24:05 -0400

    Completed Atricle.js with assistance from PM

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle("menu--open");
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -8,6 +9,4 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", function(e){
- 
-});
+menuButton.addEventListener("click", toggleMenu);
