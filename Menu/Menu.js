09429cbc9e6187ff1917ded0ce55178c471c7e36
Mark Hong <m4rkh0ng@gmail.com>
2018-05-30 18:47:14 -0400

    done with today's assignment; haven't gotten to stretch yet

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu reference. 
+  menu.classList.toggle("menu--open");
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
+
+// Only referencing the function expression, not invoking the function in the 2nd parameter (again), because it's already expressed for the callback; if you DO end up doing that, then it's going to kill the ability to call it more than once;
+
