bfb655635fd0a136fec0341014c43d710d2b78f6
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-30 15:19:30 -0700

    Added animation to header and reverted scroll bars on articles

--- Article/Article.js
+++ Article/Article.js
@@ -23,3 +23,6 @@ let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
+
+
+// GSAP
---
+++ Header/Header.js
@@ -0,0 +1,5 @@
+let headerTitle = document.querySelector(".header h1");
+
+TweenLite.from(headerTitle, 2, {autoAlpha:0, y:100});
+
+// TweenLite.from(".container nav a", 3, {autoAlpha:0, y: 100} );
