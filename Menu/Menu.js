600f3cc869aab90cb327132f593dddc878b31a7c
-tristan- <tristan.linehan@gmail.com>
2018-05-30 16:06:01 -0700

    Tristan Linehan / JS_Newsfeed-Components / daily standup

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,16 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+
+// const menu;
 // create a reference to the ".menu-button" class
-const menuButton;
+const menu = document.querySelector('.menu-button') ;
+
+// const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
+const menuButton = document.querySelector('menu-button') ;
+menuButton.addEventListener('click', toggleMenu) ;
