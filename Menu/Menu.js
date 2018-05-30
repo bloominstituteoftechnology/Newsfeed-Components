0290c050a18cf5086f17e06f8e5665ae0bd7d213
Vu Cao <cdhvu@yahoo.com>
2018-05-30 15:35:49 -0700

    Added sliding animation for menu
    Added greensock CDN link to index.html
    Changed display property and left property of .menu in menu.less
    Add script in Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -6,9 +6,21 @@ const toggleMenu = () => {
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+const article = document.querySelectorAll('.article');
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu);
+const slideMenu = () => {
+  if (menu.style.left === '-350px') {
+    TweenLite.to(menu, 1, {left:"0"}); 
+    TweenLite.to(article, 1, {left:"350px"}); 
+  }
+  else {
+    TweenLite.to(menu, 1, {left:"-350px"});
+    TweenLite.to(article, 1, {left:"0"}); 
+  }
+}
+
+menuButton.addEventListener('click', slideMenu);
