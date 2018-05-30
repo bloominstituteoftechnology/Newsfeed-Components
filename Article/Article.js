a124f1860860a7540183b23a29b1508b5423eb84
M.Real <mreal126@protonmail.com>
2018-05-30 17:38:57 -0400

    Functional butstill getting weird errors..

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,25 @@
 // Start at `let articles`
-
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = "expand"; //?
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", this.expandArticle);
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    this.parentNode.classList.toggle("article-open");
+    console.log("articles expanding")
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
-
+articles = Array.from(articles).map(element => new Article(element)); //?
