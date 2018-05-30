84213144dca0d2e3175fe99f33cf88b440bb409f
sean chavez <seanchavez@seanchavezsMBP.home>
2018-05-30 15:39:19 -0700

    it works

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,16 @@ class Article {
     // assign this.element to the passed in article element
     this.article = element;
     // create a reference to the ".expandButton " class. 
-    this.expandButton = element.querySelector('.expandButton');
+    this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle);
+    this.expandButton.addEventListener("click", () => {this.expandArticle()});
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.article.classList.toggle('hide-article');
+    this.article.classList.toggle('article-open');
   }
 }
 
