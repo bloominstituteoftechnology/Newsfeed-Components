b428458d82456733017d35cdcda8c43317882d6d
Luis <luis@Luiss-MacBook-Air.local>
2018-05-30 12:29:46 -0700

    created reference to .menu class

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,10 @@
-
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu')
+console.log(menu)
 // create a reference to the ".menu-button" class
-const menuButton;
+// const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
