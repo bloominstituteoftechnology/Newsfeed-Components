a838c8cfa5e492ef88ebfa0847ff1d45d3f1a64d
Kamry Bowman <kamry.bowman@gmail.com>
2018-05-30 17:00:41 -0600

    Menu closes if open and mouse clicks outside

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,18 @@
 
 let init = true;
+let menuHovered = false;
+
+function isHovered(e) {
+  return (e.parentElement.querySelector(':hover') === e);
+}
+
+function checkMenuHovered (e) {
+
+}
+
+
+
+
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
@@ -18,3 +31,21 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
+
+
+let body = document.querySelector('body');
+
+const closeOpenMenu = function(event) {
+  console.log ('click');
+  if (isHovered(menuButton)) {
+    return;
+  }
+
+  let isMenuOpen = Array.from(menu.classList).includes('menu--open');
+
+  if (isMenuOpen && !isHovered(menu)) {
+    toggleMenu();
+  }
+}
+
+body.addEventListener("click", closeOpenMenu);
