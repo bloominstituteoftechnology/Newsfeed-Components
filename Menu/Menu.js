e2e56fc3b16596cb1dfd81235cea9c264130f758
Kevin Tena <kevten09@gmail.com>
2018-05-30 17:13:10 -0500

    Implemented new articles along with article expansion

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,13 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
+  //let toggle = false;
   menu.classList.toggle('menu--open');
+ // toggled = !toggled;
+ // if(toggled == false);
+ // TweenMax.from(menu, 1, {left:-100});
+  //if(toggled == true);
+ // TweenMax.to(menu,1, {right:100});
 }
 
 // Start Here: Create a reference to the ".menu" class
