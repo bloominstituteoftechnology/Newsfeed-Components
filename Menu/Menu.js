6349ca93442e59f1833775db47fa273fcf013578
Your Name <you@example.com>
2018-05-30 15:00:35 -0700

    three done

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
-
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+  console.log("Did we even get in here?");
+  menu.classList.toggle("menu--open");
+  // Toggle the "menu--open" class on your menu refence.
+};
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
+console.log(menu);
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
+const studentButton = document.querySelector(".student-button");
