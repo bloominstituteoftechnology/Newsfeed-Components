6469d8783a9cf8d2862376cb8587a3d2db92b72f
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-05-30 16:22:40 -0400

    Blake Fletcher - Newsfeed-Components
    First commit
    Imported Menu and Article to index.less
    Completed menu functionality
    Start article functionality

--- Article/Article.js
+++ Article/Article.js
@@ -3,9 +3,9 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element // = element.element; CHECK LINE 6!!!
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
@@ -19,8 +19,7 @@ class Article {
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map();
-
