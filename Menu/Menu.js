5cb8e154c537c91c2f996a8ec1823ebd6c54b00f
snowcoding <vimalshah77@gmail.com>
2018-05-30 21:16:12 -0700

    Refactoring

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,40 +1,22 @@
 
-const toggleMenu = (e) => {
-  console.log('entering Toggle Menu Function')
-  console.log(e.target)
+// Determine which mouse clicks are happening
+// Only `e.target.menu-button` can open
+// Everything can close except `e.taget.menu-items`
+const mouseClickDisriminator = (e) => {
 
   //if it has menu-items, it means menu-items was open so don't do anything
   if (e.target.classList.contains('menu-items')){
   
     // if it has the menu-button...
   } else if (e.target.classList.contains('menu-button')) {
-    
-    //Always toggle
-    menu.classList.toggle('menu--open')
-    //Depending on the class figure out whether or not to slide in/out
-    if (menu.classList.contains('menu--open')){
-      console.log('slide out')
-      slideOut();
-    } else {
-      console.log('slide in')
-      slideIn();
-    }      
-  } else {
-    if (menu.classList.contains('menu--open')){
-      //Always toggle
-      menu.classList.toggle('menu--open')
-      //Depending on the class figure out whether or not to slide in/out
-      if (menu.classList.contains('menu--open')){
-        console.log('slide out')
-        slideOut();
-      } else {
-        console.log('slide in')
-        slideIn();
-      }      
-    }
+
+    toggleMenu();
+  
+  } else if (menu.classList.contains('menu--open')){
+
+    toggleMenu()
   }
 }
-  // Toggle the "menu--open" class on your menu refence. 
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu')
@@ -42,18 +24,11 @@ const menu = document.querySelector('.menu')
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button')
 
-// Using your menuButton reference, add a click handler that calls toggleMenu
-// menuButton.addEventListener('click',function(e){
-//   toggleMenu(e);
-// })
-
-
 //Grab the entire DOM to make the menu work on all parts of the DOM
 let doc = document.documentElement
 doc.addEventListener('click',function(e) {
-  toggleMenu(e);
+  mouseClickDisriminator(e);
 })
-
 
 //Basic Sliding Functionality
 function slideOut(){
@@ -68,3 +43,13 @@ function slideIn(){
   })
 }
 
+//Toggle the Menu
+function toggleMenu(){
+  menu.classList.toggle('menu--open')
+  
+  if (menu.classList.contains('menu--open')){
+    slideOut();
+  } else {
+    slideIn();
+  }      
+}
