778888ceb95e1506f466cab30b0bd3127e1b1705
snowcoding <vimalshah77@gmail.com>
2018-05-30 17:54:08 -0700

    Basic GSAP animation, added comments to understand `this` binding

--- Article/Article.js
+++ Article/Article.js
@@ -4,28 +4,58 @@ class Article {
   constructor(element) {
     // assign this.element to the passed in article element
     this.element = element
-
+    // console.log(this.element, 'this.element')
     // create a reference to the ".expandButton" class. 
-    this.expandButton = Array.from(this.element.querySelectorAll('.expandButton'))
+    // Learned that you can acutally run query selector on any piece of the DOM!!! That's fuckin crack
+    this.expandButton = this.element.querySelector('.expandButton')
+    // console.log(this.expandButton, 'this.expandButton')
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.forEach( span => span.innerHTML = 'expand')
+    this.expandButton.innerHTML = 'EXPAND'
 
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.element.addEventListener('click', this.expandArticle)
+    
+    // Now this right here was magic. I want to add the eventListener to expand button only. See below
+    
+    // Attempt #1 
+    // this.expandButton.addEventListener('click', this.expandArticle)
+    
+    // Problem with Attempt #1
+    // The expand button is part of the `this.element`. Doing the above results in the `this` keyword 
+    // in the eventHandler will have a context of expand Button. 
+
+      // this.expandArticle(){
+      //   console.log(this) // <span class="expandButton">EXPAND</span>  Uncaught TypeError: Cannot read property 'classList' of undefined at HTMLSpanElement.expandArticle
+      // }
+
+    // Since the 'this' isn't a DOMTokenList it doesn't have the classList method available
+    
+    // Attempt #2 - Working with Allen helped us discover that we could change the context of `this` by using .bind()
+    // this.expandButton.addEventListener('click', this.expandArticle.bind(this))
+    
+    // What's happening here is that .bind is taking the Article `this` and binding it to the local `this` which is for the .expandButton
+    
+    // Attempt #3 - ES6 arrow takes care of the context of `this` binding mentioned above.
+    this.element.addEventListener('click', () => {this.expandArticle()})  // Works!
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.classList.toggle('article-open')
+    console.log(this)
+    this.element.classList.toggle('article-open')
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
+// When doing querySelector it returns a nodeList. These can usually be accessed via list[index].attributeName
 let articles = document.querySelectorAll('.article')
-// console.log(articles)
+// console.log(articles, 'NodeList')
+// console.log(articles[0].className, 'NodeList[0].className')
+// console.log(articles[0].classList, 'NodeList[0].classList')
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map( art => new Article(art));
+articles = Array.from(articles).map( art => {
+  // console.log(art, 'article in map callback')
+  new Article(art)});
 // console.log(articles)
 
---
+++ Header/Header.js
@@ -0,0 +1,7 @@
+// window.onload = function(){
+//     var header = document.getElementsByClassName("header");
+//     TweenLite.from(header, 2, {
+//       left:"632px", 
+//       ease:Bounce.easeOut
+//     });
+// }
