54833f8aac28d130f24dcd72f4fc5877220e14b8
Rob <rob@users-MacBook-Air.local>
2018-05-30 20:31:07 -0500

    This was much easier than DOM I think the instructions made it easy to understand the steps

--- Article/Article.js
+++ Article/Article.js
@@ -3,23 +3,26 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
+    this.expandButton = this.element.querySelector("expandButton");
+    this.expandButton.innerText = "expand";
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton.addEventListener("click", this.expandArticle);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     
   }
 
-  expandArticle() {
+  expandArticle(e) {
+    e.target.parentNode.classList.toggle("article-open");
     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map();
