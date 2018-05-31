549a9edf575f4f18caa813f9b038a938ab51bfa5
JJ Ashcraft <jj@jjashcraft.com>
2018-05-31 15:13:19 -0400

    close button fixed

--- Article/Article.js
+++ Article/Article.js
@@ -1,44 +1,44 @@
 // Start at `let articles`
 
-// class Article {
-//   constructor(element) {
-//     // assign this.element to the passed in article element
-//     this.element = element;
-//     // create a reference to the ".expandButton" class. 
-//     this.expandButton = this.element.querySelector('.expandButton');
-//     this.closeButton = this.element.querySelector('.closeButton');
-//     // Using your expandButton reference, update the text on your expandButton to say "expand"
-//     this.expandButton.textContent = '-------------   expand   -------------';
-//     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-//     this.expandButton.addEventListener('click', () => {this.expandArticle()}); 
-//     this.closeButton.addEventListener('click', () => {this.expandArticle()});
-//   }
-
-//   expandArticle() {
-//     // Using our reference to the article element, add or remove a class to expand or hide the article.
-  
-//   this.element.classList.toggle('article-open');
-//   this.closeButton.classList.toggle('hidden');
-//   }
-
-// }
-
 class Article {
   constructor(element) {
+    // assign this.element to the passed in article element
     this.element = element;
+    // create a reference to the ".expandButton" class. 
     this.expandButton = this.element.querySelector('.expandButton');
-    this.expandButton.innerText = 'expand';
-    //this.expandArticle = this.expandArticle.call(this)
-    //this.expandButton.addEventListener('click', () => { this.expandArticle() })
-    this.expandButton.addEventListener('click', this.expandArticle);
+    this.closeButton = this.element.querySelector('.closeButton');
+    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = '-------------   expand   -------------';
+    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    this.expandButton.addEventListener('click', () => {this.expandArticle()}); 
+    this.closeButton.addEventListener('click', () => {this.expandArticle()});
   }
 
-  expandArticle(e) {
-    //this.element.classList.toggle('article-open');
-    //this.parentNode.classList.toggle('article-open');
-    e.target.parentNode.classList.toggle('article-open');
+  expandArticle() {
+    // Using our reference to the article element, add or remove a class to expand or hide the article.
+  
+  this.element.classList.toggle('article-open');
+  this.closeButton.classList.toggle('hidden');
   }
+
 }
+
+// class Article {
+//   constructor(element) {
+//     this.element = element;
+//     this.expandButton = this.element.querySelector('.expandButton');
+//     this.expandButton.innerText = 'expand';
+//     //this.expandArticle = this.expandArticle.call(this)
+//     //this.expandButton.addEventListener('click', () => { this.expandArticle() })
+//     this.expandButton.addEventListener('click', this.expandArticle);
+//   }
+
+//   expandArticle(e) {
+//     //this.element.classList.toggle('article-open');
+//     //this.parentNode.classList.toggle('article-open');
+//     e.target.parentNode.classList.toggle('article-open');
+//   }
+// }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
