f12f301505684c4c76a837c8757b0bbea34371d9
Luis <luis@Luiss-MacBook-Air.local>
2018-05-31 01:44:58 -0700

    menu button when clicked slides close & slides open

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,15 +1,28 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open')
-  TweenMax.fromTo(
-    '.menu',
-    1,
-    {
-      opacity: 1,
-      x: -400
-    },
-    { x: -80 }
-  )
+  if (!menu.classList.contains('menu--open')) {
+    menu.classList.add('menu--open')
+    TweenMax.fromTo(
+      '.menu',
+      1,
+      {
+        opacity: 1,
+        x: -400
+      },
+      { x: -80 }
+    )
+  } else {
+    menu.classList.remove('menu--open')
+    TweenMax.fromTo(
+      '.menu',
+      1,
+      {
+        opacity: 1,
+        x: -80
+      },
+      { x: -400 }
+    )
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -18,5 +31,16 @@ console.log(menu)
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button')
 console.log(menuButton)
+
+TweenMax.fromTo(
+  '.menu',
+  1,
+  {
+    opacity: 1,
+    x: -400
+  },
+  { x: -400 }
+)
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu)
