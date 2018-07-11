08ab1e32e69f203c53af6045ce98490af939ebfc
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-11 15:19:44 -0500

    slight change in the menu toggle code

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,5 +11,5 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', (event) => {
-  menu.classList.toggle('menu--open');
+  toggleMenu();
 })
