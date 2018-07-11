2a7329fd772caf7708815c9d4693d0318c8e4034
rverdi642 <rverdi642@gmail.com>
2018-07-11 15:48:25 -0500

    Working on Menu NavBar , btnclick not working on hamburger only in menu
    Richard Verdier

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,32 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+class Button {
+  constructor(button) {
+    this.item = button; // "DOM button"
+    // this.orangeBtn = this.item.querySelector(".custom-color").style.color = "orange";
+    // console.log(this.orangeBtn);
+    console.log(this.item);
+    this.item.addEventListener("click", this.btnClick);
+  }
+  // methods
+  btnClick() {
+    console.log("I was clicked!")
+    menuBtn.classList.toggle("display-flex");
+  }
 }
 
+
+const menuBtn = document.querySelector(".menu");
+let myButtons = document.querySelectorAll("li");
+console.log(menuBtn)
+console.log(myButtons)
+//extH4tag[1].innerHTML =
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+//const menu;
 // create a reference to the ".menu-button" class
-const menuButton;
+//const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+//}
+
+myButtons = Array.from(myButtons).map( button => new Button(button));
