c80d4002d9f182283166ed639d9e72a799457bce
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-11 16:09:41 -0400

    Minor bit of stretch
    -when menu is open, clicking elsewhere closes menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,13 @@
-
-const toggleMenu = () => {
+const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence.
+  e.stopPropagation();
   menu.classList.toggle('menu--open');
+}
+
+const closeMenu = () => {
+  if(menu.classList.contains('menu--open')){
+    menu.classList.remove('menu--open');
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,4 +15,6 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener('click', event => toggleMenu(event));
+
+document.querySelector('body').addEventListener('click', closeMenu);
