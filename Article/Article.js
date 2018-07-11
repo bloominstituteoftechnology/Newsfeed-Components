b6f03303ca34b10e720d6737f7fa86f6869b6ed4
David White <dw@Davids-iMac.local>
2018-07-11 15:20:21 -0600

    Menu and Articles CompleteFunctionality

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,28 @@
 // Start at `let articles`
 
 class Article {
-  constructor(element) {
+  constructor(article) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector(".expandButton")
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.element.addEventListener('click', (this.expandArticle.bind(this)));
+
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+    this.element.classList.toggle("article-open")
 
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map(article => new Article(article));
 
