9b307a0ae8a6e5fbbf3d5ec342321fa2741e0023
rverdi642 <rverdi642@gmail.com>
2018-07-11 17:20:22 -0500

    click on hamburger working, working on articles
    Richard Verdier

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,32 +1,27 @@
 
-class Button {
-  constructor(button) {
-    this.item = button; // "DOM button"
+ // "DOM button"
     // this.orangeBtn = this.item.querySelector(".custom-color").style.color = "orange";
     // console.log(this.orangeBtn);
-    console.log(this.item);
-    this.item.addEventListener("click", this.btnClick);
-  }
-  // methods
-  btnClick() {
-    console.log("I was clicked!")
-    menuBtn.classList.toggle("display-flex");
-  }
-}
-
-
-const menuBtn = document.querySelector(".menu");
-let myButtons = document.querySelectorAll("li");
-console.log(menuBtn)
-console.log(myButtons)
-//extH4tag[1].innerHTML =
+  
 
 // Start Here: Create a reference to the ".menu" class
 //const menu;
+let menu = document.querySelector(".menu");
+//let myButtons = document.querySelectorAll("li");
+let menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', (event)=> {
+  menu.classList.toggle("menu--open")
+})
+//console.log(info)
+console.log(menu)
+console.log(menuButton)
+
+
+
 // create a reference to the ".menu-button" class
 //const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 //}
 
-myButtons = Array.from(myButtons).map( button => new Button(button));
+//menuButton = Array.from(menuButton).map( button => new Button(button));
