757db399127728e7ec4059600bb97b237e7a17dc
Lauren <llwor94@gmail.com>
2018-07-11 13:38:24 -0700

    articles animated expand/collapse

--- Article/Article.js
+++ Article/Article.js
@@ -6,10 +6,12 @@ class Article {
     this.expandButton = this.element.querySelector(".expandButton");
     this.expandButton.innerHTML = "expand";
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
+    this.articleTween = TweenMax.to(this.element, 2, {height:400})
+    this.articleTween.reversed(true);
   }
 
   expandArticle(element) {
-    this.element.classList.toggle("article-open")
+    this.articleTween.reversed() ? this.articleTween.play() : this.articleTween.reverse();
   }
 }
 
@@ -17,4 +19,5 @@ class Article {
 let articles = document.querySelectorAll(".article");
 
 articles = Array.from(articles).map(article => new Article(article));
+
 
