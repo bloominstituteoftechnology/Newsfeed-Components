1fd3bd97a27fcc4177e9b8b7aea129b8f4f9a7cc
francistse23 <wtse.ucsd@gmail.com>
2018-07-11 13:00:56 -0700

    Part 2 completed. Expand button expands all articles instead of individual article. Need fix.

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,17 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.querySelectorAll('.expandButton');
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
+    this.expandButton.addEventListener('click', () => {this.expandArticle();});
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener('click', expandArticle);
-  }
 
+  }
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    console.log('clicked');
+    this.element.classList.toggle('article-open');
   }
 }
 
