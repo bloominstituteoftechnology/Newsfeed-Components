4a6baf96d9ac773a6c2844b1a165590803815b80
Lauren <llwor94@gmail.com>
2018-07-11 15:04:01 -0700

    added function that creates article and adds to page

--- Article/Article.js
+++ Article/Article.js
@@ -19,6 +19,30 @@ class Article {
 }
 
 
-let articles = document.querySelectorAll(".article");
-articles = Array.from(articles).map(article => new Article(article));
 
+let buildArticle = (header, date, text) => {
+  let newArticle = document.createElement("div");
+  newArticle.className = "article";
+  let newHeader = document.createElement("h2");
+  newHeader.innerHTML = header;
+  let newDate = document.createElement("p");
+  newDate.className = "date";
+  newDate.innerHTML = date;
+  let newText = document.createElement("p");
+  newText.innerHTML = text;
+  let newButton = document.createElement("span");
+  newButton.className = "expandButton";
+  newArticle.appendChild(newHeader);
+  newArticle.appendChild(newDate);
+  newArticle.appendChild(newText);
+  newArticle.appendChild(newButton);
+  return newArticle;
+};
+let articles = document.querySelectorAll(".article");
+let parent = articles[0].parentNode;
+
+
+let newA = buildArticle("hi","today","okay")
+parent.append(newA)
+articles = Array.from(articles).map(article => new Article(article));
+articles.push(new Article(newA))
