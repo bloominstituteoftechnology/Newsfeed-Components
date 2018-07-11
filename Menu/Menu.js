029b3c449607a612638edadd816678b141bfe0d9
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-11 16:40:56 -0600

    One GASP animation added.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle("menu--open");
+  TweenMax.from(".menu", 2, {x: -400, ease:Bounce.easeOut});
 }
 
 // Start Here: Create a reference to the ".menu" class
