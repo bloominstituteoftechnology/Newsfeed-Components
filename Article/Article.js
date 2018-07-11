dad8614b364c449f023517d0fb0731a835c7fb93
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-11 14:46:14 -0700

    change styling of button

--- Article/Article.js
+++ Article/Article.js
@@ -34,3 +34,7 @@ let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
+
+function buildArticle(title, date, content){
+  
+}
