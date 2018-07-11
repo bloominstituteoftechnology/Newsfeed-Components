05211b00f3d2d917e175f96605d93ae70efb6fd5
Grant Reighard <grantreighard@Grants-iMac.local>
2018-07-11 17:56:47 -0400

    Finished stretch tasks up to 'Close Button'.

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,5 @@
 // Start at `let articles`
-
+let expanded = false;
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
@@ -7,15 +7,33 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.element.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener("click", this.expandArticle);
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
+    this.removeButton = this.element.querySelector(".removeButton");
+    this.removeButton.textContent = "Remove Article";
+    this.removeButton.addEventListener("click", this.removeArticle.bind(this), true);
   }
 
   expandArticle(event) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    console.log(event);
-    event.path[1].classList.toggle("article-open");
+    if (expanded === false) {
+      event.stopPropagation();
+      // this.element.classList.add("article-open");
+      TweenMax.to(this.element, 1, {height:400});
+      this.expandButton.textContent = "Click to Close";
+      expanded = true;
+    } else {
+      event.stopPropagation();
+      TweenMax.to(this.element, 1, {height:50});
+      this.expandButton.textContent = "Click to Expand";
+      expanded = false;
+    }
+  }
+
+  removeArticle() {
+    event.stopPropagation();
+    this.element.style.display = "none";
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,17 @@
 
+let open = false;
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle("menu--open");
+  if (open === false) {
+    menu.classList.add("menu--open");
+    TweenMax.from(menu, 1, {x:-400});
+    TweenMax.to(menu, 1, {x:0});
+    open = true;
+  } else {
+    TweenMax.from(menu, 1, {x:0});
+    TweenMax.to(menu, 1, {x:-400});
+    open = false;
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,6 +19,12 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", event => {
-  toggleMenu();
+menuButton.addEventListener("click", () => {
+  // event.stopPropagation();
+  toggleMenu;
 });
+
+let HTMLBody = document.querySelector("body");
+HTMLBody.addEventListener("click", toggleMenu, false);
+
+menu.addEventListener("click", event => event.stopPropagation(), true);
