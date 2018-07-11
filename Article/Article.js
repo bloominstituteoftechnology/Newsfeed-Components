29bd10476235808a23bf9a1cde136b7e69bbbee5
johnoro <jjcummins102@gmail.com>
2018-07-11 17:43:25 -0400

    Added article animations.
    Also removed some of the uneccessary stuff I had added previously.

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,23 @@ class Article {
     // assign this.element to the passed in article element
     this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.article.querySelector('.expandButton');
+    this.expandBtn = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = 'expand';
+    this.expandBtn.innerText = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener('click', e => this.expandArticle());
+    this.expandBtn.addEventListener('click', e => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.article.classList.toggle('article-open');
+    if (this.article.classList.toggle('article-open')) {
+      this.article.style.transition = 'height 1s ease-out';
+      this.expandBtn.innerText = 'Click to collapse';
+    }
+    else {
+      this.article.style.transition = 'height .75s ease-in';
+      this.expandBtn.innerText = 'Click to expand';
+    }
   }
 }
 
@@ -23,4 +30,3 @@ let articles = document.querySelectorAll('.articles .article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
-
