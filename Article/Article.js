028e3fb4077eb1328b7f6cddc0c36ebcd5f9e548
LizBaker <lizbaker.pdx@gmail.com>
2018-07-11 16:05:15 -0700

    finished the dang thing -bad time, bad commit messages -i just need to go back to *this* and read up

--- Article/Article.js
+++ Article/Article.js
@@ -1,30 +1,24 @@
-// Start at `let articles`
+
 
 class Article {
   constructor(element) {
-    // assign this.element to the passed in article element
-    this.element = element;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.element.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerHTML = "expand";
     
-    this.expandButton.addEventListener("click", () => {this.expandArticle})
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    this.element = element;
+    
+    this.expandButton = this.element.querySelector('.expandButton');
+    
+    this.expandButton.innerText = "expand";
+    
+    this.expandButton.addEventListener("click", this.expandArticle)
   }
 
-  expandArticle() {
-    // Using our reference to the article element, add or remove a class to expand or hide the article.
-      articles.classList.toggle("article-open")
-      console.log ('y u no work')
-
+  expandArticle(event) {
+    event.target.parentNode.classList.toggle("article-open") 
   }
 }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
+
 let articles = document.querySelectorAll('.article');
 
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as 
-// a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
 
