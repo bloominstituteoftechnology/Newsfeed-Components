c927a114d8a82427697ec9c8892c332b59289b2b
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-11 16:58:17 -0500

    added articles variable

--- Article/Article.js
+++ Article/Article.js
@@ -19,8 +19,8 @@ class Article {
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
-
+let articles = document.querySelectorAll('.article');
+console.log(articles)
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+// articles = Array.from(articles).map();
 
