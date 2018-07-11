874e9446bbc3a11886009c86730ece5ee0194493
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-11 19:59:51 -0400

    Refactored close button -realized it should probably be in the class.

--- Article/Article.js
+++ Article/Article.js
@@ -8,8 +8,12 @@ class Article {
     this.expandButton = this.element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
+    //close button
+    this.closeButton = this.element.querySelector('.close-btn');
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    //close button event listener
+    this.closeButton.addEventListener('click', this.closeArticle);
   }
 
   expandArticle(){
@@ -21,6 +25,10 @@ class Article {
       TweenLite.to(this.element, 1, {height: 50})
     }
   }
+
+  closeArticle() {
+    event.target.parentNode.style.display = 'none';
+  }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
@@ -28,6 +36,3 @@ let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
-
-
-document.querySelectorAll('.close-btn').forEach(nav => nav.addEventListener('click', event => event.target.parentElement.style.display = 'none'));
