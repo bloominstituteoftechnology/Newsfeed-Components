becf7d05e6b0a3f0a1c086e55acd936648412ab3
johnoro <jjcummins102@gmail.com>
2018-07-11 16:55:08 -0400

    Added animations for menu open & close.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,9 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open');
+  menu.classList.toggle('menu--open') ?
+    TweenMax.to(menu, 1.5, {x: 325, ease: Power2.easeOut}) :
+    TweenMax.to(menu, 1.25, {x: -325, ease: Power3.easeIn});
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,4 +11,6 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuBtn = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuBtn.addEventListener('click', e => toggleMenu());
+menuBtn.addEventListener('click', e => {
+  toggleMenu();
+});
