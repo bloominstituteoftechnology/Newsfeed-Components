f6563cdac288213c2601e50f018782528c07eb55
Arthur Bates <aabatesj@gmail.com>
2018-07-11 17:03:46 -0400

    articles and expandButton
    created references to article and expandButton classes.

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.element to the passed in article element
     this.element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
 
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
@@ -19,7 +19,7 @@ class Article {
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map();
