1f55ba5a725c5d4ec72f185bd1ca34031b49b5ad
Charles Cortinas <crcortinas@gmail.com>
2018-07-11 16:27:47 -0500

    Main Project Complete
    Finished main objectives

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,5 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  console.log('Made it');
   menu.classList.toggle('menu--open');
 }
 
@@ -9,4 +8,4 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu());
+menuButton.addEventListener('click', toggleMenu);
