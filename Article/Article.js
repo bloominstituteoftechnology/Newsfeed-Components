960461c70f687b35d510f28b37c9967dcfff94c5
MichaelBoegner <cmikeb@gmail.com>
2018-07-11 16:54:05 -0400

    Charles Boegner
    articles functional

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,16 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+      this.element.classList.toggle("article-open");
   }
 }
 
