93027f47dc58b60afb90f0c4ae6720052b00d2ba
Lauren <llwor94@gmail.com>
2018-07-11 13:46:18 -0700

    expand/close button changes

--- Article/Article.js
+++ Article/Article.js
@@ -3,21 +3,22 @@
 class Article {
   constructor(element) {
     this.element = element;
+    this.expand = "Click to Expand";
+    this.close = "Click to Close";
     this.expandButton = this.element.querySelector(".expandButton");
-    this.expandButton.innerHTML = "expand";
+    this.expandButton.innerHTML = this.expand;
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
     this.articleTween = TweenMax.to(this.element, 2, {height:400})
     this.articleTween.reversed(true);
   }
 
-  expandArticle(element) {
+  expandArticle() {
     this.articleTween.reversed() ? this.articleTween.play() : this.articleTween.reverse();
+    this.expandButton.innerHTML === this.expand ? this.expandButton.innerHTML = this.close : this.expandButton.innerHTML = this.expand;
   }
 }
 
 
 let articles = document.querySelectorAll(".article");
-
 articles = Array.from(articles).map(article => new Article(article));
-
 
