30937bb0a0778e424301434c5110d1e1da8a1ae5
francistse23 <wtse.ucsd@gmail.com>
2018-07-11 13:41:23 -0700

    Fixed. now all articles can expand/collapse separately. For some reason when this.expandButton is used as the reference instead of this.element, the desired effect won't render.

--- Article/Article.js
+++ Article/Article.js
@@ -5,12 +5,11 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = document.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = 'expand';
-    this.expandButton.addEventListener('click', () => {this.expandArticle();});
+    this.expandButton = this.expandButton.forEach(index => index.innerText = 'expand');
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-
+    this.element.addEventListener('click', () => {this.expandArticle()});
   }
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
