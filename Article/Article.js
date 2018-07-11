1367320d47f79121792b3a9dba7e16b2bf88c14d
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-11 14:22:09 -0700

    add close button to remove article

--- Article/Article.js
+++ Article/Article.js
@@ -6,10 +6,12 @@ class Article {
     this.element = element;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.element.querySelector('.expandButton');
+    this.closeButton = this.element.querySelector('.close');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'Click to expand';
     // // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', ()=>this.expandArticle());
+    this.closeButton.addEventListener('click', () =>this.removeArticle());
   }
 
   expandArticle() {
@@ -21,6 +23,9 @@ class Article {
     } else {
       this.expandButton.innerText = 'Click to expand';
     }
+  }
+  removeArticle() {
+    this.element.style.display = 'none';
   }
 }
 
