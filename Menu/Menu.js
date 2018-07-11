f95097aaad7b7a61c33759f8c01c5a71499381c4
Carey <careycbaldwin@gmail.com>
2018-07-11 15:39:54 -0400

    menu is now functional with toggling behavior
    two errors resolved 1. used querySelectorAll when needed querySelector 2. attached toggling behavior to menu when should have used menuButton

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,24 +1,29 @@
 //Console Log Methods
-const itemClick = () => {
-  console.log("item was clicked");
-};
+// const itemClick = () => {
+//   console.log("item was clicked");
+// };
 
-const itemMouseover = () => {
-  console.log("item was moused");
-}
+// const itemMouseover = () => {
+//   console.log("item was moused");
+// }
+
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu reference. 
-  menu.classList.toggle("menu--open"); 
+  menu.classList.toggle("menu--open");
 };
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
-menu.addEventListener("mouseover", itemMouseover);
+menu.addEventListener("mouseover", () => {
+console.log("item was moused")
+});
 
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelectorAll(".menu-button");
-menuButton.addEventListener("click", itemClick);
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", () => {
+  console.log("item was clicked")
+});
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menu.addEventListener("click", toggleMenu)
+menuButton.addEventListener("click", toggleMenu)
