d655716b3f1c76a4f34822452a9cd4f12ffe36ff
Trevor <trevorfehrman@gmail.com>
2018-07-11 15:41:00 -0700

    stretch goals with ash

--- Article/Article.js
+++ Article/Article.js
@@ -24,3 +24,31 @@ let articles = document.querySelectorAll('.article');
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map( element => new Article(element));
 
+function createArticle (articleData) {
+  const articleDiv = document.createElement('div');
+  articleDiv.classList.add('article');
+
+  const articleHeading = document.createElement('h2');
+  articleHeading.textContent = articleData.headingData;
+  articleDiv.appendChild(articleHeading);
+  const articleParagraph = document.createElement('p');
+  articleParagraph.textContent = articleData.paragraphData;
+  
+  articleDiv.appendChild(articleParagraph);
+
+  const articleSpan = document.createElement('span');
+  articleSpan.classList.add('expandButton');
+
+  articleDiv.appendChild(articleSpan);
+  document.querySelector('.articles').appendChild(articleDiv);
+  new Article (articleDiv);
+}
+
+createArticle({
+  headingData: "This my article adder article.",
+  paragraphData: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+        moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth baba skywalker
+        watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau darth calamari.
+        Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine skywalker. Qui-gonn
+        jar twi'lek jinn leia jango skywalker mon.`,
+})
