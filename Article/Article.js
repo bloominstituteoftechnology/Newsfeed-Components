0951014cbcdd9bce9d79e0e14b13f8970ef13d6c
sliw <slioweng@gmail.com>
2018-07-12 05:54:48 +0800

    add close button

--- Article/Article.js
+++ Article/Article.js
@@ -1,11 +1,22 @@
 class Article {
   constructor(article) {
     this.element = article;
+    // tracking the state of article
     this.isOpened = false;
+    // expand button
     this.expandButton = this.element.querySelector('.expandButton');
     this.expandButton.innerHTML = 'Expand';
     this.expandButton.addEventListener('click', this.expand);
-  }
+    // close button
+    this.closeBtn = document.createElement('button');
+    this.closeBtn.innerHTML = ' X ';
+    this.closeBtn.style.marginTop = "5px";
+    this.closeBtn.style.border = '3px solid black';
+    this.closeBtn.style.borderRadius = '2px';
+    this.closeBtn.style.float = 'right';
+    this.element.insertAdjacentElement('afterbegin', this.closeBtn);
+    this.closeBtn.addEventListener('click', this.close);
+   }
 
   expand(e) {
     const individualArticle = e.target.parentNode;
@@ -39,6 +50,10 @@ class Article {
       e.target.innerHTML = 'Click to Expand';
     }
   }  
+
+  close(e) {
+    e.target.parentNode.style.display = 'none';
+  }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
