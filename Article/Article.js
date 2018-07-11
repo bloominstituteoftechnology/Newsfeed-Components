eebce0d5aadeabcf4aeddda9c77b788d685ce5b8
keegn <keegan.t.burkett@gmail.com>
2018-07-11 18:20:05 -0500

    playing around with styles

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,17 @@ class Article {
   constructor(element) {
     // assign this.element to the passed in article element
     this.element = element;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.element.querySelector('.expandButton');
+    // create a reference to the ".expandButton" class. see line 10 and 11, here i chained the .textContent method instead
+    this.expandButton = this.element.querySelector('.expandButton').textContent = 'expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    // this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.element.addEventListener("click", this.expandArticle);
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.classList.toggle("article-open");
+    event.target.parentNode.classList.toggle('article-open');
   }
 }
 
@@ -25,3 +25,4 @@ let articles = document.querySelectorAll('.article');
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map( element => new Article(element));
 console.log(articles)
+
