5620f64a068455134eee154179946fc2400bbc98
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-11 15:40:05 -0600

    non-stretch objectives complete

--- Article/Article.js
+++ Article/Article.js
@@ -5,17 +5,17 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.element.addEventListener("click", this.expandArticle);
-    console.log(Article);
+    this.expandButton = this.element.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = ("expand");
+    this.expandButton.style.color = 'green';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", this.expandArticle);
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    console.log("I was clicked!");
+    this.parentElement.classList.toggle("article-open");
   }
 }
 
