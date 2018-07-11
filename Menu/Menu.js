4457dddb84bc251a0707e91505d4ee38ce6d0065
William <kwontaewoongah@gmail.com>
2018-07-11 15:18:40 -0700

    finished animating slide out and slide in of menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,19 @@
 
+let open=false;
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  open=!open;
+  if (open) {
+    TweenLite.to(menu, 1, { opacity: 1, width: '350px', display:"block", ease: Power4.easeOut} );
+  } else {
+    TweenLite.to(menu, 1, { opacity: 1, width: '0px', display:"none", ease: Elastic.easeIn} );
+  }
+
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu=document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton=document.querySelector('.menu-button');
+const menuButton=document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click',toggleMenu);
