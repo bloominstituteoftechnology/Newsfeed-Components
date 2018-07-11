1f06d59e14cfe62a60238a8137338e5d69809f04
Fred <sohnf90@yahoo.com>
2018-07-11 13:20:06 -0700

    First commit for pull request

--- Article/Article.js
+++ Article/Article.js
@@ -24,3 +24,4 @@ let articles;
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map();
 
+ 
