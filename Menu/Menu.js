abaa735cb8eb8800f8b1a80ae39f0d0fdd2cea73
Carey <careycbaldwin@gmail.com>
2018-07-11 15:11:22 -0400

    latest work on js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,14 +1,24 @@
+//Console Log Methods
+const itemClick = () => {
+  console.log("item was clicked");
+};
+
+const itemMouseover = () => {
+  console.log("item was moused");
+}
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu reference. 
-   menu.classList.toggle("menu--open") 
-}
+  menu.classList.toggle("menu--open"); 
+};
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+menu.addEventListener("mouseover", itemMouseover);
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelectorAll(".menu-button");
+menuButton.addEventListener("click", itemClick);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menu.addEventListener("click",toggleMenu)
+menu.addEventListener("click", toggleMenu)
