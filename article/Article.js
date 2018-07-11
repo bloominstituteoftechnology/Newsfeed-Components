d091e191122e160905f21a092c9c1e2a26375f1c
Vance Leon <vanceleon44@gmail.com>
2018-07-11 19:12:19 -0400

    added mixins file started less nesting working on toggling the menu

--- article/Article.js
+++ article/Article.js
@@ -1,9 +1,12 @@
 // Start at `let articles`
 
+
+
+
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = button;
     // create a reference to the ".expandButton" class. 
     this.expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
@@ -11,6 +14,10 @@ class Article {
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     
   }
+
+
+
+
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
@@ -22,7 +29,7 @@ class Article {
 let articles;
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+// articles = Array.from(articles).map();
 
 //making changes and pulls 
 //less-watch-compiler is not found on computer and been configuring that
--- menu/Menu.js
+++ menu/Menu.js
@@ -1,10 +1,25 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
-
+let expandButtons = document.querySelectorAll(".expandButton");
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener("click", (event) => {
+  
+  
+  console.log("clicking buttons")
+})
+
+
+
+
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+
+// }
+
+
