a6e8eb64579a40641540b54c3b3df17e7ae86dbd
Alberto-Funk <laptopreneur@gmail.com>
2018-07-11 21:22:00 -0700

    Some Fun and Stretch Goals

--- Article/Article.js
+++ Article/Article.js
@@ -6,16 +6,39 @@ class Article {
     this.element = articleItem;
     // create a reference to the ".expandButton" class.
     this.expandButton = this.element.querySelector('.expandButton');
+    //this.expandButton = articleItem.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle);
-    //this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    //this.expandButton.addEventListener('click', this.expandArticle);
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.element.addEventListener('mouseover', this.colorMe.bind(this));
+  }
+
+  colorMe (e) {
+    let posX = e.offsetX + 75;
+    let posY = e.offsetY + 75;
+    let ranDom = Math.floor(Math.random() * (230 - 10)) + 10;
+    this.element.style.background = `linear-gradient(rgb(${posX}, ${ranDom}, ${posY}), rgb(${ranDom}, ${posX}, ${posY}))`;
   }
 
   expandArticle(e) {
+    
+    TweenMax.from(this.element, 3, {width:30});
+    TweenMax.from(this.element, 1, {rotation:0});
+    TweenMax.to(this.element, 1, {rotation:360});
+    
+    if (!this.element.classList.contains('article-open')) {
+      this.element.classList.add('article-open');
+      TweenMax.from(this.element, 2, {height:40});
+      TweenMax.to(this.element, 2, {height:400});
+    } else {
+      this.element.classList.remove('article-open');
+      TweenMax.to(this.element, 2, {height:40});
+    }
+
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    e.target.parentElement.classList.toggle('article-open');
+    //e.target.parentElement.classList.toggle('article-open');
     //this.element.classList.toggle('article-open');
   }
 }
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,74 @@
 
+// Main Menu
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
+
   menu.classList.toggle('menu--open');
+  TweenMax.from(menu, 2, {height:0});
+  TweenMax.to(menu, 2, {height:1000});
+
+  opacity.classList.toggle('opacity--open');
+  TweenMax.from(menuButton, .5, {rotation:0});
+  TweenMax.to(menuButton, .5, {rotation:360});
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+
+
+// Opacity
+opacity = document.querySelector('.opacity');
+opacity.addEventListener('click', () => {
+  opacity.classList.toggle('opacity--open');
+  menu.classList.toggle('menu--open');
+});
+
+
+
+// Hamburger Menu
+class Menu {
+  constructor (menuItem) {
+    this.element = menuItem;
+    this.menuButton = this.element.querySelector('.menu-item__button');
+    this.menuButton.style.color = 'white';
+    this.menuButton.addEventListener('click', this.doSomething.bind(this));
+
+  }
+
+  doSomething (e) {
+    let posX = e.offsetX;
+    let posY = e.offsetY;
+    let ranDom = Math.floor(Math.random() * (230 - 10)) + 10;
+
+    switch (this.menuButton.textContent) {
+      case 'Students':
+        menu.style.background = `linear-gradient(rgb(${posX}, ${ranDom}, ${posY}), rgb(${ranDom}, ${posX}, ${ranDom}))`;
+      break;
+      case 'Faculty':
+        menu.style.background = `linear-gradient(rgb(${posX}, ${ranDom}, ${posY}), rgb(${ranDom}, ${posX}, ${posY}))`;
+      break;
+      case `What's New`:
+        menu.style.background = `linear-gradient(rgb(${posX}, ${posY}, ${ranDom}), rgb(${ranDom}, ${posX}, ${posY}))`;
+      break;
+      case `Tech Trends`:
+        menu.style.background = `linear-gradient(rgb(${ranDom}, ${posX}, ${posY}), rgb(${ranDom}, ${posX}, ${posY}))`;
+      break;
+      case `Music`:
+        menu.style.background = `linear-gradient(rgb(${ranDom}, ${ranDom}, ${ranDom}), rgb(${ranDom}, ${posX}, ${posY}))`;
+      break;
+      case `Log Out`:
+        menu.style.background = `linear-gradient(rgb(${posX}, ${ranDom}, ${posY}), rgb(${ranDom}, ${posX}, ${posY}))`;
+      break;
+    }
+  }
+}
+
+let menuItems = document.querySelectorAll('.menu-item');
+menuItems = Array.from(menuItems).map(menuItem => new Menu(menuItem));
