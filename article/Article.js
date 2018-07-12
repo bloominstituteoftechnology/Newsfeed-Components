1560e053d05e2418683300e5d14f0c4e07c07d08
Vance Leon <vanceleon44@gmail.com>
2018-07-12 11:00:45 -0400

    toggling the articles

--- article/Article.js
+++ article/Article.js
@@ -1,35 +1,55 @@
 // Start at `let articles`
 
-
-
+let articles = document.querySelectorAll(".article");
 
 class Article {
-  constructor(element) {
-    // assign this.element to the passed in article element
+  constructor(button){
     this.element = button;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.btnClick);
+  }
+  btnClick(){
+    console.log("I was clicked")
   }
 
-
-
-
-
-  expandArticle() {
-    // Using our reference to the article element, add or remove a class to expand or hide the article.
-
-  }
 }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
 
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-// articles = Array.from(articles).map();
 
-//making changes and pulls 
-//less-watch-compiler is not found on computer and been configuring that
+articles = Array.from(articles).map(button => new Article(button))
+
+
+
+
+
+
+
+// class Article {
+//   constructor(element) {
+//     // assign this.element to the passed in article element
+//     this.element = button;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton;
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    
+//     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    
+//   }
+
+
+
+
+
+//   expandArticle() {
+//     // Using our reference to the article element, add or remove a class to expand or hide the article.
+
+//   }
+// }
+
+// // START HERE: Select all classes named ".article" and assign that value to the articles variable
+// let articles;
+
+// // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
+// // articles = Array.from(articles).map();
+
+// //making changes and pulls 
+// //less-watch-compiler is not found on computer and been configuring that
