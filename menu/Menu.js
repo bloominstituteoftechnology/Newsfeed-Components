d68fb0244282929453f127ec5b33e5e3ea48400e
Vance Leon <vanceleon44@gmail.com>
2018-07-12 00:37:39 -0400

    passed through toggle function

--- menu/Menu.js
+++ menu/Menu.js
@@ -1,36 +1,35 @@
 
-const menuList = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu-button');
-
-menuBtn.addEventListener('click', (event) => {
-  // menuList.style.display = "flex";
-  menuList.classList.toggle("menu--open");
-})
-
-
-
-
-
-
-
-// const toggleMenu = () => {
-//   // Toggle the "menu--open" class on your menu refence. 
-//   menu.classlist.toggle('menu--open');
-//   console.log("clicking menu")
-// }
-
-
-
 // // Start Here: Create a reference to the ".menu" class
-// const menu = document.querySelector(".menu");
 
 
 // // create a reference to the ".menu-button" class
-// const menuButton = document.querySelector(".menu-button");
 
 
-// // Using your menuButton reference, add a click handler that calls toggleMenu
-// menuButton.addEventListener('click', toggleMenu);
+const menuList = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-button');
+
+
+// Using your menuButton reference, add a click handler that calls toggleMenu
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence. 
+  menuList.classList.toggle("menu--open");
+  // console.log("got in");
+}
+
+
+menuBtn.addEventListener('click', toggleMenu); 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
