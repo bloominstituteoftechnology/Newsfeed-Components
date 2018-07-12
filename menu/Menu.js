6a9c4aa544f5a4e0a19635f58c4716782c9e3426
Vance Leon <vanceleon44@gmail.com>
2018-07-12 00:33:00 -0400

    toggling the menu button

--- menu/Menu.js
+++ menu/Menu.js
@@ -1,25 +1,47 @@
 
-let expandButtons = document.querySelectorAll(".expandButton");
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector(".menu");
+const menuList = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-button');
 
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
-
-// Using your menuButton reference, add a click handler that calls toggleMenu
-
-menuButton.addEventListener("click", (event) => {
-  
-  
-  console.log("clicking buttons")
+menuBtn.addEventListener('click', (event) => {
+  // menuList.style.display = "flex";
+  menuList.classList.toggle("menu--open");
 })
+
+
+
 
 
 
 
 // const toggleMenu = () => {
 //   // Toggle the "menu--open" class on your menu refence. 
-
+//   menu.classlist.toggle('menu--open');
+//   console.log("clicking menu")
 // }
+
+
+
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelector(".menu");
+
+
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector(".menu-button");
+
+
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener('click', toggleMenu);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
