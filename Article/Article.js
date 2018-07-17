b47141d6bf288efd261633056cedb5dec0bb1984
AI2EJAY <JHammett1314@gmail.com>
2018-07-17 15:17:52 -0500

    HTML & CSS Of homepage/Nav/footer almost done

--- Article/Article.js
+++ Article/Article.js
@@ -14,7 +14,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    console.log("bacon")
+ 
     this.element.classList.toggle("article-open");
   }
 }
@@ -25,5 +25,4 @@ let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(element => new Article(element));
-
 
