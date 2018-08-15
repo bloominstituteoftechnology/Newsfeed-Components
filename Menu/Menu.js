09999401d867423c46deac93986e0a20c21e816b
decagondev <decagondevelopment@gmail.com>
2018-08-15 21:10:24 +0100

    added a menu open anim

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -7,5 +7,6 @@ const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", () => {
   // Toggle the "menu--open" class on your menu refence. made this an anonymous function
-  menu.classList.toggle("menu-open"); 
+  menu.classList.toggle("menu-open");
+  menu.classList.toggle("menu-close");
 });
