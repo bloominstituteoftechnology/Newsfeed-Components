96f65e3bd22be426185604ea853f6574089c17d1
Max <maxkajiwara@gmail.com>
2018-08-15 16:15:33 -0700

    Added simple animation to menu on open

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
+  TweenMax.from(".menu", .3, {x:-600})
 }
 
 // Start Here: Create a reference to the ".menu" class
