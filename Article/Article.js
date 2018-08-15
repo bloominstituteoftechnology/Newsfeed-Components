ed8b7eefc7d81802548b58464c8545ae33a79a7c
Michael Agard <mdagard@gmail.com>
2018-08-15 15:46:19 -0400

    upload of currently unworking innerHTML function

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,19 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.article = this.element;
+    this.article = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.item.querySelector('.expandButton');
+    this.expandButton = this.article.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = 'expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.article.addEventListener('click', this.expandArticle()); 
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    // this.articles.classList.toggle('hide-this') 
+    console.log('this worked!')
   }
 }
 
