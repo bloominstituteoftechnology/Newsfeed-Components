58c23e46760d739ba88f50452ad3478bf5942bc2
lndubose <lndubose13@gmail.com>
2018-08-15 15:04:22 -0500

    Articles done and fixed the height

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,26 @@
-// // Start at `let articles`
+// Start at `let articles`
 
-// class Article {
-//   constructor(element) {
-//     // assign this.element to the passed in article element
-//     this.element;
-//     // create a reference to the ".expandButton" class. 
-//     this.expandButton;
-//     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-//     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
-//   }
+class Article {
+  constructor(article) {
+    // assign this.element to the passed in article element
+    this.item = article;
+    // create a reference to the ".expandButton" class. 
+    this.expandButton = this.item.querySelector(".expandButton");
+    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.innerText = "expand";
+    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    this.item.addEventListener('click', this.expandArticle);
+  }
 
-//   expandArticle() {
-//     // Using our reference to the article element, add or remove a class to expand or hide the article.
+  expandArticle() {
+    // Using our reference to the article element, add or remove a class to expand or hide the article.
+    event.currentTarget.classList.toggle("article-open");
+  }
+}
 
-//   }
-// }
+// START HERE: Select all classes named ".article" and assign that value to the articles variable
+let articles = document.querySelectorAll(".article");
 
-// // START HERE: Select all classes named ".article" and assign that value to the articles variable
-// let articles;
-
-// // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-// articles = Array.from(articles).map();
+// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
+articles = Array.from(articles).map(article => new Article(article));
 
