51d758caa15e15fd6c28f3f62b99236b3a2b3213
m-j-hacker <mjhacker@gmail.com>
2018-08-15 16:24:01 -0500

    Fixed issue where articles were not starting in hidden view

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,19 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class.
-    this.expandButton = document.querySelector(".expandButton");
+    this.expandButton = this.element.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.TextContent = "Expand";
+    this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', (e) => {
-      e.expandArticle();
+      e.expandArticle;
     })
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.currentTarget.classList.toggle("hide");
+    e.parentNode.classList.toggle("article-open");
+    e.parentNode.classList.toggle("article");
   }
 }
 
