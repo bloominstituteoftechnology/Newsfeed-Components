5e3989a44c5a7dee420ec15400a58caa243c67cd
Michael Agard <mdagard@gmail.com>
2018-08-15 18:59:17 -0400

    cleaned up code for the sliding menu.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,18 +3,22 @@
   // // Toggle the "menu--open" class on your menu refence.
   // menu.classList.toggle('menu--open')
 // }
-// My first jQuery! Copied straight from the docs. :)
-// On .menu-button click, the menu fades in and out.
-$(document).ready(function(){
-  $(".menu-button").click(function(){
-      $(".menu").fadeToggle(150);
-  });
+// My first jQuery!  :) On .menu-button click, the
+// menu slides in from the right-to-left.
+$(".menu-button").click(function(event){
+  event.stopPropagation();
+  $(".menu").animate({width:'toggle'},350);
+});
+// This jQuery just allows you to click anywhere on
+// the articles to close the menu.
+$('.articles').click(function(){
+  $(".menu").animate({width:'hide'},350);
 });
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+// const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
+// const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', (event) => {
-  toggleMenu();
-})
+// menuButton.addEventListener('click', (event) => {
+//   toggleMenu();
+// })
