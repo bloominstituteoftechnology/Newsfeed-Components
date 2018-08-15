d0247f40104be4c2bdf4882a8f22b5bfcc9ee73a
Michael Agard <mdagard@gmail.com>
2018-08-15 14:59:50 -0400

    Added a click handler that calls toggleMenu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,3 +8,6 @@ const menu = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelectorAll('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', (event) => {
+  toggleMenu();
+})
