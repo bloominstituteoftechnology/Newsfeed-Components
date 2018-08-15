b89321cd4d91ca03fab978f16b44aa1bf9ff9fc0
alejandrok93 <alejandrok93@gmail.com>
2018-08-15 15:55:54 -0700

    added animation with GSAP

--- Article/Article.js
+++ Article/Article.js
@@ -17,7 +17,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.element.classList.add("article-open");
+    this.element.classList.toggle("article-open");
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,8 +8,12 @@ const menu = document.querySelector(".menu");
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
-
+console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", function(e) {
   toggleMenu();
 });
+
+//Add animations using GSAP
+TweenMax.to(menuButton, 3, { x: 180, rotation: 180, ease: Elastic.easeOut });
+TweenMax.to(".menu--open", 5, { opacity: 1 });
