0b6ae5caced07b387f758cac6a45351278ab0052
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-15 16:15:54 -0400

    initial menu setup complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu;
+
+class Menu {}
+
 // create a reference to the ".menu-button" class
 const menuButton;
+
+class MenuButton {}
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+MenuButton.addEventListener('click', toggleMenu());
