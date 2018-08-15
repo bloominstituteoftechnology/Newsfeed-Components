bc0097ae77adb723ffcfc1c0b77a4ff475d8b6e6
Keiran <keirankozlowski@gmail.com>
2018-08-15 18:15:01 -0400

    Corrected type in Article.js, added extra articles to index.html.

--- Article/Article.js
+++ Article/Article.js
@@ -22,7 +22,7 @@ class Article {
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
     console.log("Toggled article.");
-    this.expandButton.classList.toggle('article-open');
+    this.element.classList.toggle('article-open');
   }
 }
 
