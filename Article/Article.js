ca1d44f4eb457b9771af7078a9d03413ef47b4bc
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-15 15:56:47 -0600

    Finished Project, Animation Stretch Goals, and Close Button Stretch Goal.

--- Article/Article.js
+++ Article/Article.js
@@ -3,24 +3,37 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector('.expandButton');
+    this.closeButton = this.element.querySelector('.closeButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = 'Expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.closeButton.addEventListener('click', this.deleteArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+    if(this.element.classList.contains('article-open')) {
+      this.expandButton.innerText = 'Expand';
+      TweenMax.to(this.element, .25, {height: 50});
+    } else {
+      this.expandButton.innerText = 'Close';
+      TweenMax.to(this.element, .25, {height: 400});
+    }
+    this.element.classList.toggle('article-open'); 
+  }
 
+  deleteArticle() {
+    TweenMax.to(this.element, .25, {opacity: 0, onComplete: () => {this.element.style.display = 'none'}});
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map(article => new Article(article));
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
+  if (menu.classList.contains("menu--open")) {
+   TweenMax.to(menu, .25, {left: -250, opacity: 0, onComplete: () => {menu.classList.remove("menu--open")}});
+  } else {
+   TweenMax.to(menu, .25, {left: 0, opacity: 1, onComplete: menu.classList.add("menu--open")});
+  }
+  
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
