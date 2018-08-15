3dfa9da67681181f0693e5d986ccb0964faf83e0
lndubose <lndubose13@gmail.com>
2018-08-15 14:42:17 -0500

    Menu is done

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,26 @@
-// Start at `let articles`
+// // Start at `let articles`
 
-class Article {
-  constructor(element) {
-    // assign this.element to the passed in article element
-    this.element;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+// class Article {
+//   constructor(element) {
+//     // assign this.element to the passed in article element
+//     this.element;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton;
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+//     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     
-  }
+//   }
 
-  expandArticle() {
-    // Using our reference to the article element, add or remove a class to expand or hide the article.
+//   expandArticle() {
+//     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
-  }
-}
+//   }
+// }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+// // START HERE: Select all classes named ".article" and assign that value to the articles variable
+// let articles;
 
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+// // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
+// articles = Array.from(articles).map();
 
