50b646394037ea4d0aaf166134cf75758ef76b7a
Casey Baker <caseye.baker@gmail.com>
2018-08-15 12:27:24 -0700

    slightly reformatted index.html for readability; imported Menu.js to index.less; completed Part 1 in Menu.js; didn't actually do anything to index.css, just looked at it

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => {
+  toggleMenu();
+});
