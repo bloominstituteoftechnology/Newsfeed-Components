7536af8226e53c068213794863773d85cc4dce0e
Jeremy Boggs <nicocchilover@gmail.com>
2018-08-15 16:42:04 -0400

    added animation to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,20 @@
 
+let menuTog = 0;
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  document.querySelector('.menu').classList.toggle('menu--open');
+  // menu.classList.toggle('menu--open');
+  console.log(menuTog);
+
+  if (menuTog === 0) {
+    enter();
+    menuTog ++;
+  }
+  else if (menuTog === 1) {
+    out();
+    menuTog--;
+  }
+
 };
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,3 +23,15 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+let tl = new TimelineLite({paused:true});
+tl.to(menu, 0.6, {left: 0 + 'px', opacity: 1, ease: Power2.easeIn});
+menu.animation = tl;
+
+function enter() {
+  menu.animation.play();
+}
+
+function out() {
+  menu.animation.reverse();
+}
