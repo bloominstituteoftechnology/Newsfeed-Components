d532319a125bd954f0c3df7cc887b65a251a3601
easyra <ezra.rr.davis@gmail.com>
2018-08-15 15:46:24 -0700

    update

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,60 @@
 // Start at `let articles`
 
+
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = element.querySelector('.expandButton');
+    this.expandButton.innerText = 'expand'
+       
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+
+    this.expandButton.addEventListener('click', this.expandArticle) 
+    this.element.addEventListener('wheel', this.scrollArticle)    
   }
 
-  expandArticle() {
+  expandArticle() {    
+    event.target.parentNode.classList.toggle('article-open')
+    
+    if(event.target.parentNode.classList.contains('article-open')){
+      event.target.innerText = 'close'
+    } else {
+      event.target.innerText = 'expand'
+    } 
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+  }
+  scrollArticle() {
+    let last_known_scroll_position = 0
+    let ticking = false;
+    if(event.target.parentNode.classList.contains('article-open')){
+      
+        last_known_scroll_position = window.scrollY;
 
+        if (!ticking) {
+
+       window.requestAnimationFrame(function() {
+            console.log(last_known_scroll_position);
+            event.target.style.height += last_known_scroll_position;
+            ticking = false;
+        });
+          
+          ticking = true;
+
+        }
+    }
   }
 }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
 
+
+// START HERE: Select all classes named ".article" and assign that value to the articles variable
+let articles = document.querySelectorAll('.article');
+Array.from(articles).map(article => console.log(article.querySelector('.expandButton')));
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map(article => new Article(article) );
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
-const toggleMenu = () => {
+let toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
+  console.log('click');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+let menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+let menuButton = document.querySelector('.menu-button');
+
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener('click', toggleMenu)
