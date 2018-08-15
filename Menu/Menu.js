7adf3a76cac61287e6dc37e39061ed88f6afb144
Michael Agard <mdagard@gmail.com>
2018-08-15 18:17:00 -0400

    Added jQuery animation to the menu and styled it to my liking

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,9 +1,15 @@
-
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open') 
-}
-
+// Disabled this funciton to allow jQuery to work.
+// const toggleMenu = () => {
+  // // Toggle the "menu--open" class on your menu refence.
+  // menu.classList.toggle('menu--open')
+// }
+// My first jQuery! Copied straight from the docs. :)
+// On .menu-button click, the menu fades in and out.
+$(document).ready(function(){
+  $(".menu-button").click(function(){
+      $(".menu").fadeToggle(150);
+  });
+});
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
