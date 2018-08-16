4322e51b449a8824209015fe941503749fa0a29d
mich-shrak <micah.shrak@gmail.com>
2018-08-16 00:26:11 -0700

    Completed Task 3

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,4 @@ menu.classList.add('menu--open');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', (e) => {
-	toggleMenu();
-});
+menuButton.addEventListener('click', toggleMenu);
