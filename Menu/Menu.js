f4e3a45e40a939e2fe7df5cea4a8cfb64ce14275
Kyle Baker <k@gnomiq.com>
2018-09-02 21:31:02 -0700

    Added toggle class to menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open');
+};
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
 // create a reference to the ".menu-button" class
-const menuButton;
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener('click', toggleMenu);
