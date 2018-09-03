446f84a8c4227f014983367b5979eeeaff61bf2d
Kyle Baker <k@gnomiq.com>
2018-09-02 21:42:48 -0700

    Completed Article component

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,30 @@
 // Start at `let articles`
 
 class Article {
-  constructor(element) {
-    // assign this.element to the passed in article element
-    this.element;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
-  }
+    constructor(element) {
+        // assign this.element to the passed in article element
+        this.element = element;
+        // create a reference to the ".expandButton" class.
+        this.expandButton = element.querySelector('.expandButton');
+        // Using your expandButton reference, update the text on your expandButton to say "expand"
 
-  expandArticle() {
-    // Using our reference to the article element, add or remove a class to expand or hide the article.
+        this.expandButton.innerHTML = "EXPAND";
 
-  }
+        // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+        this.expandButton.addEventListener('click', () => this.expandArticle())
+
+    }
+
+    expandArticle() {
+        // Using our reference to the article element, add or remove a class to expand or hide the article.
+        this.element.classList.toggle('article-open');
+        this.expandButton.innerHTML = this.expandButton.innerHTML === "EXPAND" ? "COLLAPSE" : "EXPAND"
+    }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.getElementsByClassName('article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map(article => new Article(article));
 
