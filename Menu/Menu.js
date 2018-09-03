29584860347e47066e091ab9328b9eb63c27ecbb
Ken Barlow <kenbrlow@gmail.com>
2018-09-02 18:28:09 -0700

    Got the articles expanding as expected.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,13 +2,15 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   console.log('toggle menu called');
+  console.log(menu);
+  menu.classList.toggle(".menu--open");
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector(".menu")
+const menu = document.querySelector(".menu");
 
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button")
+const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
