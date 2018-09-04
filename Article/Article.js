f44d3bc64ecc9e72f9237b5fddea3cbd5094812a
jbrock1981 <jbrock1981@gmail.com>
2018-09-04 14:09:47 -0500

    Jonathan Brock

--- Article/Article.js
+++ Article/Article.js
@@ -23,4 +23,3 @@ let articles;
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map();
-
