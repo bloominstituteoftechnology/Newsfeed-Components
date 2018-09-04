140510c0937643e1902e089d7b76f261b27b4df3
kurtve <kurt@vanetten.us>
2018-09-04 00:50:15 -0400

    Complete project (no stretch goals yet)

--- Article/Article.js
+++ Article/Article.js
@@ -3,24 +3,44 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.element.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.expandButton.innerText = 'expand';
+
+    // Set a click handler on the expandButton reference (or article element), calling
+    // the expandArticle method.
+    this.expandButton.addEventListener('click', this.expandArticle);
+
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+    event.stopPropagation();
+    
+    // ES6 style: this -> expand button
+    this.parentNode.classList.toggle('article-open');
 
+    if (this.innerText === 'expand') {
+      this.innerText = 'collapse';
+    } else {
+      this.innerText = 'expand';
+    }
   }
+  
 }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
 
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+// START HERE: Select all classes named ".article" and assign that value to the articles variable
+const articles = document.querySelectorAll('.article');
+
+// Use .map() to iterate over the articles array and create a new instance of Article by passing
+// in each article element as a parameter to the constructor.
+// articles = Array.from(articles).map();
+
+// this will work too:
+articles.forEach( (item) => new Article(item) );
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,22 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (e) => {
+  // Toggle the "menu--open" class on your menu refence.
+
+  // just in case there are click events higer up on the DOM being listened for
+  e.stopPropagation();
+  // display or hide the menu
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+
+
+// Saturday lecture codepen:
+// codepen.io/lambdaschool/pen/GBaBKB?editors=1011
