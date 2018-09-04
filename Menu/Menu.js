4acb11c2a5513d499671e8b5236d1f935ef87b22
iepoch <stekelham@outlook.com>
2018-09-03 20:27:54 -0700

    Added Greensock animation on Articles, and CSS animation on Menu.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,8 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open'); 
+
+  
 }
 
 // Start Here: Create a reference to the ".menu" class
