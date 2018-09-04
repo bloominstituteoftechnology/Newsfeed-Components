1ef012b431917221e4d25c728db22fe00736b780
Willie Yturralde <willieino@hotmail.com>
2018-09-04 16:29:23 -0700

    modified js files

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,42 +1,42 @@
 
 function reader() {
 
-let menu = document.querySelector(".menu");
 
-//menu.style.display = "none";
+
 
 function toggleMenu() {
-//const toggleMenu = () => {
+
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle("menu--open");  
- 
  }
 
 // Start Here: Create a reference to the ".menu" class
-//const menu = document.querySelector(".menu");
-//class Menu {
-//  constructor(menu) {
-//    this.item = menu;
+let menu = document.querySelector(".menu");
 
-//  }
-//}
-
-//console.log(menu);
-
-//const menu;
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
-
-
-
-
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
 
+let anchor = document.createElement('a');
+anchor.className = "link";
+anchor.innerHTML = "Students";
+
+anchor.addEventListener("click", btnClick);
+
+let li = document.querySelector("li");
+li.innerHTML = "";
+li.appendChild(anchor);
 
 
-
+function btnClick() {
+  console.log("click");
 }
+
+
+};
+
+
 
 reader();
