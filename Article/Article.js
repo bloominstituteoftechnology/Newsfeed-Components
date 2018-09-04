b4405eb19f917575a1b9c82012067409184065e8
paulapivat <paul.apivat@gmail.com>
2018-09-05 06:50:13 +0700

    add expand innerHTML through map function

--- Article/Article.js
+++ Article/Article.js
@@ -3,24 +3,29 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    // this.expandButton.innerHTML = "expand";
+    // console.log(expandButton);
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    element.addEventListener('click', this.expandArticle);
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    console.log(event.currentTarget);
+    event.currentTarget.classList.toggle('article');
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
+let expandButton = document.querySelectorAll('.expandButton');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map((element,index) => new Article(element));
+
+expandButton = Array.from(expandButton).map((element,index) => element.innerHTML = "expand");
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -13,5 +13,5 @@ const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu => {
-  console.log('toggleMenu clicked!');
+  console.log('toggleMenu clicked again!');
 });
