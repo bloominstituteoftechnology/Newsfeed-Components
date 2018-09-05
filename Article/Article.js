9ed8cce9186361ad8b6675d33ccb249dac9af588
stevekutz <stkutz@gmail.com>
2018-09-04 21:35:31 -0500

    fixed typo

--- Article/Article.js
+++ Article/Article.js
@@ -14,14 +14,15 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    if(this.innerText === 'expand') this.innerText = 'collapse'
-    this.innerText = 'expand';
-    this.parentNode.classList.toggle('article-open');
+    if(this.innerText === 'expand') this.innerText = 'collapse';
+     else this.innerText = 'expand';
+
+   this.parentNode.classList.toggle('article-open');
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles = document.querySelectorAll('article');
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
