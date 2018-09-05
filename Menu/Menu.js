aa0362ab051d577b9fb1349126de8ffc381bed72
paulapivat <paul.apivat@gmail.com>
2018-09-05 09:15:20 +0700

    add toggleMenu code

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,17 +1,38 @@
 
-const toggleMenu = (menu) => {
+const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  // event.currentTarget.classList.toggle('menu--open');
+  if(menuClass.style.display === "none") {
+    menuClass.style.display = "block";
+  } else {
+    menuClass.style.display = "none";
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector(".menu");
-
+let menuClass = document.querySelector(".menu");
 
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
+let menuButtonClass = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu => {
-  console.log('toggleMenu clicked again!');
-});
+let menuButtonClick = menuButtonClass.addEventListener('click', toggleMenu);
+
+// menuButton.addEventListener('click', toggleMenu);
+
+/// Ryan Solution
+
+// const toggleMenu = function(){
+//   if(navMenu.style.display === 'none'){
+//     navMenu.style.display = "block"; 
+//   } else {
+//     navMenu.style.display = 'none'; 
+//    }
+//  }
+
+// let navMenu = document.querySelector(".menu");
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelector(".menu-button");
+// // create a reference to the ".menu-button" class
+// const menuButton = menu.addEventListener("click", toggleMenu);
+// // Using your menuButton reference, add a click handler that calls toggleMenu
