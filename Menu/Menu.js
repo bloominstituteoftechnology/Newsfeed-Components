0153569aa6af3f528d27d7aef392fb0cdf1e0c8d
shannonhealy <sahealy525@gmail.com>
2018-09-04 20:44:55 -0400

    Completed Part 2: The Articles Component. Changed syntax in Menu.js to be ES6 instead of ES5

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -12,6 +12,6 @@ const menu = document.querySelectorAll(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", function(event) {
+menuButton.addEventListener("click", (event) => {
   toggleMenu();
 });
