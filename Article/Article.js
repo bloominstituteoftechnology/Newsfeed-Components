fa7e84e6e790bed59f6cc72ae063ffa6eecdbbd4
Kendra Williams <kmonique.williams@gmail.com>
2018-09-05 14:04:03 -0400

    standard goals complete

--- Article/Article.js
+++ Article/Article.js
@@ -8,7 +8,7 @@ class Article {
     this.e.expandButton = this.e.querySelector(".expandButton");
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.e.expandButton.innerHTML = "expand";
+    this.e.expandButton.innerHTML = "click to expand";
 
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.e.expandButton.addEventListener("click", this.expandArticle.bind(this));
@@ -16,7 +16,21 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+
+    // if(this.e.classList.length < 2){
+    //   this.e.classList.add("article-open");
+    //   this.e.expandButton.innerHTML = "click to close";
+    // } else {
+    //   this.e.classList.remove("article-open");
+    //   this.e.expandButton.innerHTML = "click to expand";
+    // }
+
       this.e.classList.toggle("article-open")
+      if(this.e.classList.length < 2) {
+        this.e.expandButton.innerHTML = "click to expand";
+      } else {
+        this.e.expandButton.innerHTML = "click to close";
+      }
   }
 }
 
