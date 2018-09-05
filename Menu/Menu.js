c1041790dbb82a7583455a12d391a58bbcf54168
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-04 23:14:00 -0400

    added animations to the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,14 @@
 
 const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open')
+  // menu.classList.toggle('menu--open')
+  if (menu.style.transform === '') {
+    TweenMax.to(".menu", 1, {x:350})
+  } else if (menu.style.transform === 'matrix(1, 0, 0, 1, 350, 0)') {  
+    TweenMax.to(".menu", 1, {x:-350})
+  } else {
+    TweenMax.to(".menu", 1, {x:350})
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,3 +17,5 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+// "matrix(1, 0, 0, 1, 350, 0);"
