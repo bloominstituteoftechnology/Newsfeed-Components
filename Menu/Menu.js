c10dc2c7faad925e739056b522023504b10e6f33
Rick Fleming <elfeming111@yahoo.com>
2018-09-04 20:17:25 -0700

    Added sliding animation to menu block in Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,21 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
+  
+  if(Array.from(menu.classList).includes('menu--open')){
+    menu.style.display = 'block';
+    TweenMax.to('.menu', 1, {left: 0});
+  }
+
+  else{
+    TweenMax.to('.menu', 1, {left: -350, onComplete:hideBlock});
+  }
 }
+
+function hideBlock(){
+  menu.style.display = 'none';
+}
+
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
@@ -10,3 +24,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
