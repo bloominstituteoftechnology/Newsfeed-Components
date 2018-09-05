e176498b675903eaa6902e2093d459361d09dd72
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-09-04 22:29:31 -0500

    finished everything. forgot to do pushes earlier

--- Article/Article.js
+++ Article/Article.js
@@ -3,24 +3,29 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = expandButton; 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    element.addEventListener("click", this.expandArticle); 
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    event.currentTarget.classList.toggle("article");
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
+let expandButton = document.querySelectorAll(".expandButton").innerHTML = "Expand";
+let open = document.querySelectorAll(".article-open");
+
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map(function(article){
+  new Article(article); 
+}); 
+
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+let navMenu = document.querySelector(".menu"); 
+
+
+const toggleMenu = function(){
+   if(navMenu.style.display === 'none'){
+     navMenu.style.display = "block"; 
+   } else {
+     navMenu.style.display = 'none'; 
+    }
+  }
+
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu-button");
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = menu.addEventListener("click", toggleMenu);
 // Using your menuButton reference, add a click handler that calls toggleMenu
