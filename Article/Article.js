c7326ecfd8cc5d9c3d0c1d9665cc36c7ddbe0c54
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2018-09-04 22:34:31 -0400

    completed Menu and working on Article.js. Cannot get articles to expand. Working on expanding.

--- Article/Article.js
+++ Article/Article.js
@@ -3,17 +3,21 @@
 class Article {
   constructor(element) {
     // assign this.element to the passed in article element
-    this.element;
+    this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
+
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = 'expand';
+
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    expandButton.addEventListener('click', this.expandArticle;
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+    articles.classList.toggle('article-open');
 
   }
 }
@@ -21,6 +25,8 @@ class Article {
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles;
 
+articles = document.querySelectorAll('.article');
+
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+articles = Array.from(articles).map( article => new Article(article));
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,23 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  let menuOpen = document.querySelector('.menu--open');
+  if(menuOpen.style.display === 'block'){
+    menuOpen.style.display = 'none';
+  } else if(menuOpen.style.display === 'none'){
+    menuOpen.style.display = 'block';
+  }
+
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu;
+menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
 const menuButton;
+menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener('click', toggleMenu); 
