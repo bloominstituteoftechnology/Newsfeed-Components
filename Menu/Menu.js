8b38d3d188cbe98b08b1ab333ad07eb6d905ed73
Denise Escobar <descobar1490@gmail.com>
2018-09-04 22:39:40 -0500

    subject: finished part 1: The Menu
    I added the reference to both the menu and menu-button class and was able to create the event that add the class

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
 
 const toggleMenu = (event) => {
   console.log(event.currentTarget);
-  menu.className += "menu--open";
+  menu.className += " menu--open";
 
   // Toggle the "menu--open" class on your menu refence. 
 }
