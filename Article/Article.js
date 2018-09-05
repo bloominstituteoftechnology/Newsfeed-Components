74371716ee23e7d43db2b8d884e3a56de54e6198
Abigayle Franklin <abifanklin@gmail.com>
2018-09-04 22:38:57 -0500

    Added animation and delete article

--- Article/Article.js
+++ Article/Article.js
@@ -6,20 +6,29 @@ class Article {
     this.element = element;
     // create a reference to the ".expandButton" class. 
     this.expandButton = element.querySelector('.expandButton');
+    this.closeButton = element.querySelector('.close');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = 'expand';
+    this.closeButton.innerHTML = '<i class="fas fa-times"></i>';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle);
+    this.closeButton.addEventListener('click', this.deleteArticle);
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    event.currentTarget.parentNode.classList.toggle('article-open');
-    if (event.currentTarget.parentNode.classList.contains('article-open')) {
-      event.currentTarget.innerHTML = 'close';
+    // event.currentTarget.parentNode.classList.toggle('article-open');
+    if (event.currentTarget.parentNode.style.height == "" || event.currentTarget.parentNode.style.height == "50px") {
+      TweenMax.to(event.currentTarget.parentNode, 1, {height: 400});
+      event.currentTarget.innerHTML = 'collapse';
     } else {
       event.currentTarget.innerHTML = 'expand';
+      TweenMax.to(event.currentTarget.parentNode, 1, {height: 50});
     }
+  }
+
+  deleteArticle() {
+    TweenMax.to(event.currentTarget.parentNode, .5, {opacity: 0, display: "none"});
   }
 }
 
