a76255557856c7af4903cb1e34960ec77f238498
Jesse <theiseja@gmail.com>
2018-09-04 21:35:59 -0500

    Completed assignment aside from stretch goals, going to work on those

--- Article/Article.js
+++ Article/Article.js
@@ -5,30 +5,27 @@ function makeArticles() {
     constructor(article) {
       // assign this.element to the passed in article element
       this.element = article;
-      this.expand = this.element.querySelector(".expandButton")
+      this.expand = this.element.querySelector('.expandButton')
+      
       // create a reference to the ".expandButton" class. 
     
-      let expandButton = document.querySelector(".expandButton");
+      let expandButton = document.querySelector('.expandButton');
 
-     this.expand.innerHTML = "expand";
+     this.expand.innerHTML = 'Click to Expand'
      this.expand.addEventListener("click", this.expandArticle);
 
     }
+
     expandArticle() {
       // Using our reference to the article element, add or remove a class to expand or hide the article
-      event.currentTarget.parentNode.classList.toggle("article-open");
-      if (event.currentTarget.innerHTML === "expand") {
-        event.currentTarget.innerHTML = "close";
-      } else {
-       if (event.currentTarget.innerHTML === "close") {
-         event.currentTarget.innerHTML = "expand";
-      }
+      event.currentTarget.parentNode.classList.toggle('article-open');
+      if (event.currentTarget.innerHTML = 'Click to Expand') {  // Stretch goal to Expand / Close articles Figuring out how to get the text to update
     };
   }
   };
 
   // START HERE: Select all classes named ".article" and assign that value to the articles variable
-  let articles = document.querySelectorAll(".article");
+  let articles = document.querySelectorAll('.article');
   
   // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
   articles = Array.from(articles).map(article => new Article(article));
