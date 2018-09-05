9c9dcf1d543eb0d8932b8c987fa6147ff6ac40fa
Colin Dismuke <colin.dismuke@gmail.com>
2018-09-04 21:37:57 -0500

    Fixed issue with Expand only showing on the first article.

--- Article/Article.js
+++ Article/Article.js
@@ -5,16 +5,16 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = 'Expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle);
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.parentNode.classList.toggle('article-open');
+    event.currentTarget.parentNode.classList.toggle('article-open');
   }
 }
 
