639db6d6311c944680cdd10e76231afd6a8fa285
Willie Yturralde <willieino@hotmail.com>
2018-09-04 23:58:11 -0700

    clean up some of the coding

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,50 +1,41 @@
 
 function reader() {
-let menuOpen = true;
+  let menuOpen = true;
 
-
-
-function toggleMenu() {
-
-  // Toggle the "menu--open" class on your menu refence. 
-  if (menuOpen === true) {
-   // menu.classList.toggle("menu--close");
-  menu.classList.toggle("menu--open");  
-  menuOpen = false;
-  } else {
-   // menu.classList.toggle("menu--open");
-   menu.classList.toggle("menu--open"); 
-   menuOpen = true;  
+  function toggleMenu() {
+    // Toggle the "menu--open" class on your menu refence. 
+    if (menuOpen === true) {
+      menu.classList.toggle("menu--open");
+      menuOpen = false;
+    } else {
+      menu.classList.toggle("menu--open");
+      menuOpen = true;
+    }
   }
- }
 
-// Start Here: Create a reference to the ".menu" class
-let menu = document.querySelector(".menu");
+  // Start Here: Create a reference to the ".menu" class
+  let menu = document.querySelector(".menu");
 
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector(".menu-button");
 
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  menuButton.addEventListener("click", toggleMenu);
 
-let anchor = document.createElement('a');
-anchor.className = "link";
-anchor.innerHTML = "Students";
+  let anchor = document.createElement('a');
+  anchor.className = "link";
+  anchor.innerHTML = "Students";
 
-anchor.addEventListener("click", btnClick);
+  anchor.addEventListener("click", btnClick);
 
-let li = document.querySelector("li");
-li.innerHTML = "";
-li.appendChild(anchor);
+  let li = document.querySelector("li");
+  li.innerHTML = "";
+  li.appendChild(anchor);
 
-
-function btnClick() {
-  console.log("click");
-}
-
+  function btnClick() {
+    console.log("click");
+  }
 
 };
-
-
 
 reader();
