4a3b416b3c09e134c897d28dfc05dc0a93a5660a
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-04 23:28:33 -0400

    work on stretch goals

--- Article/Article.js
+++ Article/Article.js
@@ -6,16 +6,30 @@ class Article {
     this.element = element;
     // create a reference to the ".expandButton" class.
     this.expandButton = this.element.querySelector(".expandButton");
+    this.closeButton = this.element.querySelector(".close");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = "expand";
+    this.closeButton.innerHTML = '<i class="far fa-times-circle"></i>';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener("click",  ()   =>  this.expandArticle());
+    this.closeButton.addEventListener("click",  ()  =>  this.closeArticle());
   }
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    this.element.classList.toggle("article-open");
-    
+    if(this.element.style.height === "400px")   {
+        TweenLite.to(this.element, .5, {height: "50px"})
+        this.expandButton.innerHTML = "expand";
+    }   else {
+        TweenLite.to(this.element, .5, {height: "400px"});
+        this.expandButton.innerHTML = "collapse";
+    }
+
+  }
+
+  closeArticle()    {
+      // this.element.style.display = "none";
+      TweenLite.to(this.element, .5, {opacity: 0, display: "none"});
   }
 }
 
