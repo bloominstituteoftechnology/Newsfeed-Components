776f242c2b63116643707ff947123f4afd23cd65
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-04 23:02:27 -0400

    Working on animation stretch goal.

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,27 @@
 class Article {
   constructor(article) {
     // assign this.element to the passed in article element
-    this.element = article;
+    this.element = article ;
     // create a reference to the ".expandButton" class. 
-   let expandButton = Array.from(document.querySelectorAll('.expandButton'));
-    expandButton.innerHTML = 'expand';
-    console.log(expandButton);
+   this.expandButton = this.element.querySelector('.expandButton');
+    this.expandButton.innerHTML='expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    this.element.addEventListener('click', this.expandArticle);
     
   }
-
+ 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
+    event.currentTarget.classList.toggle('article-open');
+
+    let h2Italics = Array.from(document.getElementsByTagName('h2'))
+    
+    h2Italics.map(element => {
+      element.style.fontStyle = 'italic';
+    })
   }
 }
 
