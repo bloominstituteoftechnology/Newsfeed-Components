50ca7003a1a5bb38a79d807032b955db3f09f428
shannonhealy <sahealy525@gmail.com>
2018-09-04 21:51:41 -0400

    Changed Menu.js to a query selector rather than a queryselectorall

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,13 +2,11 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
 
-  menu.forEach((element) => {
-    element.classList.toggle("menu--open");
-  });
+  menu.classList.toggle("menu--open");
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll(".menu");
+const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
