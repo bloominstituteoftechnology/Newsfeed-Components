6df8702b44c9460d5f20cf91aa6843d81e153672
leelichtsinn <lee.lichtsinn@gmail.com>
2018-09-04 21:52:14 -0500

    added references for menu and menuButton in Menu.js added toggleMenu callback for click eventListener

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (e) => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+console.log(menu);
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
