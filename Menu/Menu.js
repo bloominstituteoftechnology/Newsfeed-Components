6b5760c042a171a4b8657329e398a24f62cb5f88
Kyle Teeter <kyleteeter12@gmail.com>
2018-09-04 23:01:26 -0500

    Part 2: The Articles Component (complete)

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,5 @@ const menu = document.querySelector(".menu");
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
-console.log(menuButton)
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu)
