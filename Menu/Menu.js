b9c0beeb32f26fe9e87afda51e3ea84d1d1fa1d4
jalvarez2020 <javieralvarez82@live.com>
2018-09-04 23:51:30 -0700

    complted project working on stretch goals

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,15 @@
-const toggleMenu = () => {
+const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.toggle('menu--open');
+  menu.classList.toggle('menu--open');
 }
 // Start Here: Create a reference to the ".menu" class
+
 const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
+
 const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
-const clickHandler = function (event) {
-  event.target.toggleMenu();
-};
-menuButton.addEventListener('click', clickHandler);
+
+menuButton.addEventListener('click', toggleMenu);
