ca3fdd36c1da4b27c478c1cc3e34c1fcaecbd630
mglittleton <michaelglittleton@gmail.com>
2018-09-05 20:31:26 -0400

    fix: Sidebar menu animation works as intended
    Sidebar animation works as intended with no time lag. Removed the animation instruction from the function that gets called and used a 'paused' tag to keep it from running on page load.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,30 +3,28 @@
 //   menu.classList.toggle("menu--open")
 // };
 
-// This is the function for animation
-let t1 = new TimelineLite();
-t1.reversed(true);
 
-const toggleMenu = () => {
-  const animation = t1.to(menu, 1, {
-    left:0, 
-    ease: Back.easeOut.config(1.7)
-  }); //need nested function?
-  console.log(animation);
 
-  if (t1.reversed()) {
-    animation.reverse();
-    t1.reversed(false);
-    console.log(t1);
-  } else {
-    animation.play();
-    t1.reversed(true);
-    console.log(t1);
-  }
-}
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+
+// This is the function for animation
+let t1 = new TimelineLite({reversed: true, paused: true});
+t1.to(menu, 1, {
+  left:0, 
+  ease: Back.easeOut.config(1.7)
+});
+const toggleMenu = () => {
+  //need nested function?
+  if (t1.reversed()) {
+    t1.reverse();
+    t1.reversed(false);
+  } else {
+    t1.play();
+    t1.reversed(true);
+  }
+}
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
@@ -38,5 +36,3 @@ menuButton.addEventListener("click", toggleMenu);
 // TweenMax.to(menu, 2, {
 //   x: 100
 // })
-
-
