2816fc7caeded8faecb1bd2fb9208660a0f1f730
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-06 14:43:53 -0500

    created menuButton click event  listener

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -7,3 +7,5 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener("click", toggleMenu);
