d6b98d85df9273575114f4f7b58b0ae5f107d173
ShardeDeLane <rhcprocker@yahoo.com>
2018-09-06 10:15:54 -0400

    fix error expandArticle

--- Article/Article.js
+++ Article/Article.js
@@ -15,7 +15,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    articles.classlist.toggle('expand--articles')
+    this.element.classlist.toggle('expand--articles')
   }
 }
 
