a833eb34d342090e36f0a5b082abee3c00746173
paulapivat <paul.apivat@gmail.com>
2018-09-07 09:54:23 +0700

    refactor code for xpand button and article-open, close

--- Article/Article.js
+++ Article/Article.js
@@ -5,39 +5,52 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = expandButton;
+    // this.expandButton = expandButton;
+
+    this.expandButton = element.querySelector('.expandButton');
+    this.closeButton = element.querySelector('.closeBox');
+    this.expandButton.innerText = "expand";
     // Using your expandButton reference, update the text on your expandButton to say "expand"
         // this.expandButton.innerHTML = "expand";
         // console.log(expandButton);
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    element.addEventListener('click', this.expandArticle);
+    this.expandButton.addEventListener('click', this.expandArticle);
+    this.closeButton.addEventListener('click', this.closeBoxHandler);
   }
 
   expandArticle(event) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
+    
+    event.currentTarget.parentNode.classList.toggle('article-open');
+    event.currentTarget.parentNode.classList.toggle('close');
+
     console.log(event.currentTarget);
-    event.currentTarget.classList.toggle('article');
   }
+
+  closeBoxHandler(event) {
+    event.currentTarget.parentNode.style.display = "none";
+  }
+
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
-let expandButton = document.querySelectorAll('.expandButton');
+// let expandButton = document.querySelectorAll('.expandButton');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map((element,index) => new Article(element));
 
-expandButton = Array.from(expandButton).map((element,index) => element.innerHTML = "expand");
+// expandButton = Array.from(expandButton).map((element,index) => element.innerHTML = "expand");
 
 
 //// Close Button
-let closeBox = document.querySelectorAll('.closeBox');
+// let closeBox = document.querySelectorAll('.closeBox');
 
-const closeBoxHandler = function(event) {
-  this.parentNode.style.display = 'none';
-}
+// const closeBoxHandler = function(event) {
+//   this.parentNode.style.display = 'none';
+// }
 
-console.log(closeBox);
+// console.log(closeBox);
 
-closeBox.forEach((e) => e.addEventListener('click', closeBoxHandler));
-closeBox.forEach((e) => e.style.color = "red");
+// closeBox.forEach((e) => e.addEventListener('click', closeBoxHandler));
+// closeBox.forEach((e) => e.style.color = "red");
