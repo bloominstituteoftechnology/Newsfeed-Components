2b7a9637ea40cc5019e8051dc38c1950354b03f7
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-06 22:21:20 -0400

    Add animation to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,21 @@
-
+let visible = false
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
+  if(visible === false)  {
+      TweenLite.to(menu, .2, {opacity: 1})
+      visible = true;
+      console.log(visible)
+  } else {
+      TweenLite.to(menu, .2, {opacity: 0})
+      visible = false;
+  }
+  console.log("clicked")
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+menu.style.display = "block";
+menu.style.opacity = 0;
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
