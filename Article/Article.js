7a627ce831b82ba213284772d8c8d2b8fbc151cd
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-11 17:08:50 -0400

    Got this running as required.

--- Article/Article.js
+++ Article/Article.js
@@ -29,37 +29,32 @@ The end result is that when this.element/article recieves a click event, the eve
 class Article {
   constructor(article) {
     // assign this.element to the passed in article element
-    this.element = article ;
+    this.element = article;
     // create a reference to the ".expandButton" class. 
-   this.expandButton = this.element.querySelectorAll('.expandButton');
-   console.log(this.expandButton);
-    this.expandButton.innerHTML='expand'; 
+    this.expandButton = this.element.querySelector('.expandButton');
+    console.log(this.expandButton);
+    this.expandButton.innerHTML = 'expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.element.addEventListener('click', this.expandArticle);
-    
+    this.expandButton.addEventListener('click', () => {
+
+      this.expandArticle();
+    });
   }
- 
   expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
-    event.currentTarget.classList.toggle('article-open');
-
-    // TweenLite.to(element, 2, {rotation:30, scaleX:0.8});
-
-
-
-    let h2Italics = Array.from(document.getElementsByTagName('h2'))
-    
-    h2Italics.map(element => {
-      element.style.fontStyle = 'italic';
-    })
+    this.element.classList.toggle('article-open')
   }
-}
 
+}
+    
+      
+    
+  
+    
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map(article => new Article (article));
+articles = Array.from(articles).map(article => new Article(article));
