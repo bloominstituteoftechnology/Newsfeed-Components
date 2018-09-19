d1102db257e7a40e1ad6cda65ad65a1ef69a3989
Andre Myrick <amyrick.820@gmail.com>
2018-09-19 17:25:22 -0400

    completed animation portion of stretch

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = "expand";
+    this.expandButton.innerText = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle(event));
   }
@@ -15,6 +15,12 @@ class Article {
   expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+    if (this.domElement.classList.contains("article-open")) {
+      this.expandButton.innerText = "Click to Close";
+    }
+    else {
+      this.expandButton.innerText = "Click to Expand";
+    }
   }
 }
 
