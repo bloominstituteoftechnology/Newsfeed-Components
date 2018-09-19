d8ea4b54b1c17ca438479ebe1858cdcaac2afe03
Sarah Tennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-09-19 17:23:12 -0400

    working on side navigation animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,27 @@
 
+/* MAIN PROJECT SOLUTION
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
 }
+*/
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button')
+
+/* MAIN PROJECT SOLUTION
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+*/
+
+//---------------------------------------------------------------//
+
+const body = document.body;
+
+menuButton.addEventListener('click', () => {
+  body.className = (body.className == 'menu-active') ? '' : 'menu-active';
+});
+
