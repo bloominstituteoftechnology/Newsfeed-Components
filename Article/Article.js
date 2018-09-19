6c6c8da274bf7e09192b2c5f2330970538e8135e
kyranmccann <kyran.mccann@gmail.com>
2018-09-19 13:49:58 -0700

    fixing typo

--- Article/Article.js
+++ Article/Article.js
@@ -15,7 +15,7 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     event.stopPropagation();
-    this.element.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
   }
 }
 
