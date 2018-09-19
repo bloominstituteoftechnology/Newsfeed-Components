af1b288a13f56e52eaaedbb5bc84a0a4af0cef2d
derpwanda <indigoblow@gmail.com>
2018-09-19 16:18:33 -0500

    added menu toggle

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,21 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (event) => {
+  // Toggle the "menu--open" class on your menu refence.
+  if (event.style.display === "none") {
+    event.style.display = "block";
+  } else {
+    event.style.display = "none"
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+const menuButton = document.querySelector('.menu-button');
+// Using your menuButton reference, add a click handler that 
+//calls toggleMenu
+menuButton.addEventListener('click', () => {
+  return toggleMenu(menu);
+});
+
+
