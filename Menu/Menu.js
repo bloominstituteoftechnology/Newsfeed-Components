a471c09800850671ac4a4fb9b8ffa92571ed6fd1
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-19 15:03:32 -0600

    finished MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,12 +5,13 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+let menu = document.querySelector('.menu');
 
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
+let menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', (e) => {
+  e.stopPropagation();
   toggleMenu();
 });
