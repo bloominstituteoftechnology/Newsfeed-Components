20d3abe59b7d8b8722ea119c769a0ac577311a8c
Katie Cleary <katiecleary@airmaster.lan>
2018-09-19 18:20:25 -0400

    got toggle open workin on each article

--- Article/Article.js
+++ Article/Article.js
@@ -5,20 +5,20 @@ class Article {
     // assign this.element to the passed in article element
     this.element = element;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = element.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle(event));}
+    
 
   expandArticle(event) {
-  event.stopPropagation();
-  this.element.classList.toggle('article-open')
-
+    this.element.classList.toggle('article');
+  }
     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
   }
-}
+
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article')
@@ -26,5 +26,6 @@ let articles = document.querySelectorAll('.article')
 // Use .map() to iterate over the articles array and create a new 
 //instance of Article by passing in each article element as a parameter to the constructor.
 articles = Array.from(articles).map(element => {
-  return new Article(element);
+ return new Article(element);
 });
+console.log(articles);
