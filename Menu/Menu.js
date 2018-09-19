3a3881c33b4779f69010a830c88411ff4f995df8
Rahul Desai <rd825@georgetown.edu>
2018-09-19 15:19:05 -0400

    Completed menu animation stretch goal

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,5 @@
 
-const toggleMenu = () => {
+const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu reference. 
   menu.classList.toggle('menu--open');
 }
@@ -11,4 +11,18 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", () => {toggleMenu()});
+menuButton.addEventListener("click", () => {toggleMenu(event)});
+
+// code for menu to slide off screen when anywhere else is clicked.
+document.querySelector('.header').addEventListener('click', () => {dismissMenu(event)})
+
+document.querySelector('.articles').addEventListener('click', () => {dismissMenu(event)})
+
+const dismissMenu = (event) => {
+  let ignore = document.querySelector('.menu-button');
+  let target = event.target;
+  event.stopPropagation();
+  if (target !== ignore) {
+    menu.classList.remove ('menu--open');
+  }
+}
