8f9e996e8853f6f45f36192a2010b51f41e4a295
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-19 16:39:12 -0600

    Added tranistion to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,3 +11,8 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+
+
+TweenMax.from(".menu", 2, {width:"0%",  ease:Power2.easeInOut});
+ 
