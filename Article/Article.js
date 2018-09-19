fbea369ce38f120a08641fcd80edc2187cb3b6df
rytwalker <rytwalker@gmail.com>
2018-09-19 16:02:43 -0400

    Intial commit with menu and article complete

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,30 @@
 // Start at `let articles`
 
 class Article {
-  constructor(element) {
+  constructor(article) {
     // assign this.element to the passed in article element
-    this.element;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.article = article;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = document.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.forEach(button => (button.innerText = 'expand'));
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    
+    this.article.addEventListener('click', e => this.expandArticle(e));
   }
 
-  expandArticle() {
+  expandArticle(e) {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
+    if (e.target.classList.contains('expandButton')) {
+      this.article.classList.toggle('article-open');
+      console.log(e.target);
+    }
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
-
+articles = Array.from(articles).map(article => {
+  return new Article(article);
+});
