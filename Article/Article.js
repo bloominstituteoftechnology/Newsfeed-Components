caaab264c9885efe425e3e913bc365a35ac43f1f
kyranmccann <kyran.mccann@gmail.com>
2018-09-19 15:00:29 -0700

    menu and article animations (just css)

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,21 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-     this.expandButton.innerText = 'expand';
+     this.expandButton.innerText = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
      this.expandButton.addEventListener('click', () => this.expandArticle(event));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log('clicked!')
     event.stopPropagation();
     this.domElement.classList.toggle('article-open');
+    if (this.expandButton.innerHTML == 'Click to Expand'){
+      this.expandButton.innerHTML = 'Click to Close';
+    } else {
+      this.expandButton.innerHTML = 'Click to Expand'; 
+    }
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,3 +10,9 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+//stretch space
+// window.addEventListener('click', function(){
+//   let open = document.querySelector('.menu--open');
+//   open.classList.toggle('menu--open');
+//  })
