06f8cfc9b0a898ec55b87023251f56e483aff7d3
Axolotl <samuelmachat@gmail.com>
2018-09-19 18:26:11 -0400

    added expanding article animation

--- Article/Article.js
+++ Article/Article.js
@@ -1,13 +1,25 @@
 class Article {
   constructor(domElement) {
+    this.expanded = false
     this.domElement = domElement
     this.expandButton = this.domElement.querySelector('.expandButton')
-    this.expandButton.innerText = 'expand'
-    this.expandButton.addEventListener('click', () => this.expandArticle())    
+    this.expandButton.innerText = 'Click to expand'
+    this.expandButton.addEventListener(
+      'click',
+      () => (this.expanded ? this.closeArticle() : this.expandArticle())
+    )
   }
 
   expandArticle() {
-    this.domElement.classList.toggle('article-open')
+    TweenMax.to('.article', 0.7, { height: '400px' })
+    this.expanded = true
+    this.expandButton.innerText = 'Click to close'
+  }
+
+  closeArticle() {
+    TweenMax.to('.article', 0.7, { height: '40px' })
+    this.expanded = false
+    this.expandButton.innerText = 'Click to expand'
   }
 }
 
