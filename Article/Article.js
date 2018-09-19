06ce455f66c8380d3ddacba4d6c4443005d006d9
samsisle <samko9522@gmail.com>
2018-09-19 16:04:31 -0700

    Update Article.js main assignment [2]

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,19 @@
 class Article {
-  constructor(element) {
+  constructor(article) {
     /*-- DOM selectors -----------------------------*/
-    this.element = element;
-    this.expandButton = this.element.querySelector('.expandButton');
+    this.article = article;
+    this.expandButton = this.article.querySelector('.expandButton');
     /*-- DOM manipulations -------------------------*/
     this.expandButton.innerText = 'expand';
     /*-- event listeners ---------------------------*/
-    this.expandButton.AddEventListener('click', this.expandArticle.bind(this));
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('article-open');
   }
 }
 
 let articles = document.querySelectorAll('.article');
-articles = Array.from(articles).map(element => {
-  return new Article(element);
-});
+articles = Array.from(articles).map(article => new Article(article));
 
