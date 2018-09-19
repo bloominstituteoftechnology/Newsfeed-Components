202671767dcb1b686bf5f9b7a402462ea3a593e1
Gannon <gkleblanc@gmail.com>
2018-09-19 17:20:17 -0400

    fixed errors with menu and cleaned up articles

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -15,7 +15,7 @@ class Article {
   expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.    sooooo should this be articles.classList.toggle??
     event.stopPropagation();
-    this.domElement.classList.toggle('article')
+    this.domElement.classList.toggle('article-open')
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu reference. 
-  menu.classList.toggle('.menu--open')
+  menu.classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,6 +10,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menu.addEventListener('click', (event) => {
-  return toggleMenu;
-})
+menuButton.addEventListener('click', (toggleMenu));
