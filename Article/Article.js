4601f870fb164da8f24080169c9582d9b49fb9e1
pedro <pedroluismontesinosrivera@gmail.com>
2018-09-19 17:44:20 -0400

    Used .map() to iterate over the articles array and created a new instance of Article by passing in each article as a parameter to the constructor.

--- Article/Article.js
+++ Article/Article.js
@@ -22,4 +22,4 @@ class Article {
 let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles);
+articles = Array.from(articles).map(article => new Article(article));
