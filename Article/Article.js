067da261f1dd5d4e9b7107f1b299082ef786c881
kyranmccann <kyran.mccann@gmail.com>
2018-09-19 13:48:16 -0700

    component constructor function

--- Article/Article.js
+++ Article/Article.js
@@ -19,6 +19,56 @@ class Article {
   }
 }
 
+
+let articlesToBeAdded = [{
+  header: 'Article One',
+  date: Date(),
+  text: 'Today conceptually blew my mind, tho.'
+},
+{
+  header: 'Article Two',
+  date: Date(),
+  text: 'Adding new articles is haaaaaaaaaard.'
+}];
+
+function convertArticles(){
+  articlesToBeAdded.forEach((article) =>{
+    return articleHtml(article);
+  })
+}
+
+function articleHtml(article){
+  let newArticleHeader = document.createElement('h2');
+  newArticleHeader.innerText = article.header;
+
+  let newArticleDate = document.createElement('p');
+  newArticleDate.classList.add('date');
+  newArticleDate.innerText = article.date;
+
+  let newArticleText = document.createElement('p');
+  newArticleText.innerText = article.text;
+
+  let newButton = document.createElement('span');
+  newButton.classList.add('expandButton');
+
+  let articleContainer = document.querySelector('.articles');
+
+  let addArticles = document.createElement('div');
+  addArticles.classList.add('article')
+  addArticles.appendChild(newArticleHeader);
+  addArticles.appendChild(newArticleDate);
+  addArticles.appendChild(newArticleText);
+  addArticles.appendChild(newButton);
+  articleContainer.appendChild(addArticles);
+}
+
+articlesToBeAdded.push({
+  header: 'Article Three',
+  date: Date(),
+  text: 'Whoaaaa there, buddy does this actually work???'
+})
+convertArticles();
+
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
 
