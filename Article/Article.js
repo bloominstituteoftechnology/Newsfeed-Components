000c30e077f66c466b0e4c7326105829078373c6
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-19 13:47:18 -0700

    fixed expand button

--- Article/Article.js
+++ Article/Article.js
@@ -9,13 +9,14 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.domElement.addEventListener('click', () => this.expandArticle(event));
+    this.expandButton.addEventListener('click', () => this.expandArticle(event));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
   }
+
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
@@ -25,4 +26,3 @@ let articles = document.querySelectorAll('.article');
 articles = Array.from(articles).map(domElement => {
   return new Article(domElement);
 });
-
