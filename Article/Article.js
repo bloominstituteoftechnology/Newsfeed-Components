9bc4e78a88172e6b0b07d0f1b57871d6706eb24a
Troy Amlee <troyamlee19@gmail.com>
2018-09-19 17:58:25 -0500

    Troy Amlee [mvp] did objectives tried to make animation work

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,41 @@
-// Start at `let articles`
+// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(element) {
-    // assign this.element to the passed in article element
-    this.element;
+  constructor(domElement) {
+    // assign this.domElement to the passed in domElement
+    this.domElement = domElement
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton = this.domElement.querySelector('.expandButton');
     
-    // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
+    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.innerHTML = "expand";
+    
+    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', () => {this.expandArticle()})
     
   }
 
   expandArticle() {
-    // Using our reference to the article element, add or remove a class to expand or hide the article.
+    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log("is this working");
+    this.domElement.classList.toggle('article-open');
 
+    TweenLite.to('.article', 1, {
+      ease: Power2.easeIn,
+    })
   }
 }
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
 
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article element as a parameter to the constructor.
-articles = Array.from(articles).map();
+
+
+
+// START HERE: Select all classes named ".article" and assign that value to the articles variable
+let articles = document.querySelectorAll('.article');
+
+// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
+articles = Array.from(articles).map(domElement => {
+  return new Article(domElement);
+});
+
 
