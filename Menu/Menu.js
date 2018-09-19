4569ed0ab91775a213a398092963168363151f1c
Elvis Ibarra <ielvisd@gmail.com>
2018-09-19 15:45:54 -0700

    stuck but making progress

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu reference. 
+//   this.classList.toggle('menu--open');
+// }
 
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelector('.menu');
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector('.menu-button');
+// console.log(menuButton);
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menu.addEventListener('click', () => toggleMenu());
+  
