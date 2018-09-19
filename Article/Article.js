6bacb7eab09f8421ce08f8404a5d37dd5a96ebb0
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-19 15:42:25 -0400

    fixed toggle for article.ls

--- Article/Article.js
+++ Article/Article.js
@@ -2,20 +2,26 @@
 
 class Article {
   constructor(domElement) {
+    // console.log(domElement);
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
+    // console.log('expandButton',this.expandButton);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () =>this.expandArticle(event));
+    this.expandButton.addEventListener('click', () => this.expandArticle(event));
+
   }
+  // this.domElement.addEventListener('click', this.btnClick.bind(this));
 
   expandArticle(event) {
+    // console.log('classlist before',this.domElement.classList)
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     event.stopPropagation();
-    article.classList.toggle('expandButton');
+    this.domElement.classList.toggle('article-open');
+    // console.log('classlist after',this.domElement.classList)
   }
 }
 
