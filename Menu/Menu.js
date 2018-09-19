e9949007332444e0272759e15e11b1872d065cce
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-19 13:43:41 -0600

    first commit

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
 }
-
+//hunter
 // Start Here: Create a reference to the ".menu" class
 const menu;
 // create a reference to the ".menu-button" class
