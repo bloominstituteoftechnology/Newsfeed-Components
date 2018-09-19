060e0511229ff9bdb578a72bd9606f63c6272daf
Axolotl <samuelmachat@gmail.com>
2018-09-19 17:45:23 -0400

    added menu slide in slide out

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,4 +5,19 @@ const toggleMenu = () => {
 const menu = document.querySelector('.menu')
 const menuButton = document.querySelector('.menu-button')
 
-menuButton.addEventListener('click', () => toggleMenu())
+let menuOpen = false
+
+const openMenu = () => {
+  menuOpen = true
+  TweenMax.to('.menu', 0.7, { left: 0 })
+}
+
+const closeMenu = () => {
+  TweenMax.to('.menu', 0.7, { left: '-300px' })
+  menuOpen = false
+}
+
+menuButton.addEventListener(
+  'click',
+  () => (menuOpen === true ? closeMenu() : openMenu())
+)
