e819de32f58be8e3e1ec96e79ff6f873853f89a6
rytwalker <rytwalker@gmail.com>
2018-09-19 19:54:34 -0400

    Refactored some article code

--- Article/Article.js
+++ Article/Article.js
@@ -5,11 +5,11 @@ class Article {
     // assign this.element to the passed in article element
     this.article = article;
     // create a reference to the ".expandButton" class.
-    this.expandButton = document.querySelectorAll('.expandButton');
+    this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.forEach(button => (button.innerText = 'Click to expand'));
+    this.expandButton.innerText = 'Click to expand';
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.article.addEventListener('click', e => this.expandArticle(e));
+    this.expandButton.addEventListener('click', () => this.expandArticle());
     // Close Article
     this.article.addEventListener('click', e => this.closeArticle(e));
   }
@@ -28,25 +28,21 @@ class Article {
     }
   }
 
-  expandArticle(e) {
+  expandArticle() {
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-    if (e.target.classList.contains('expandButton')) {
-      if (!this.article.classList.contains('article-open')) {
-        this.article.classList.add('article-open');
-        TweenMax.to(this.article, 1.25, {
-          height: 400,
-          ease: Bounce.easeOut
-        });
-        setTimeout(() => (e.target.innerText = 'Click to close'), 1250);
-      } else if (this.article.classList.contains('article-open')) {
-        this.article.classList.remove('article-open');
-        TweenMax.to(this.article, 0.5, {
-          height: 50
-        });
-        setTimeout(() => (e.target.innerText = 'Click to expand'), 600);
-      }
-
-      // this.article.classList.toggle('article-open');
+    if (!this.article.classList.contains('article-open')) {
+      this.article.classList.add('article-open');
+      TweenMax.to(this.article, 1.25, {
+        height: 400,
+        ease: Bounce.easeOut
+      });
+      setTimeout(() => (this.expandButton.innerText = 'Click to close'), 1250);
+    } else if (this.article.classList.contains('article-open')) {
+      this.article.classList.remove('article-open');
+      TweenMax.to(this.article, 0.5, {
+        height: 50
+      });
+      setTimeout(() => (this.expandButton.innerText = 'Click to expand'), 600);
     }
   }
 }
