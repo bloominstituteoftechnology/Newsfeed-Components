73e3267420f8de421856597a85c78230bed30d1d
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-09-19 13:46:51 -0700

    Article.js complete

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,33 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement = domElement;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    // this.expandButton.addEventListener("click", this.expandArticle.bind(this));
+    this.expandButton.addEventListener("click", e => this.expandArticle(e));
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-open");
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll(".article");
+// ======= COMPREHENSION CHECK =======
+// articles = array-like object / Nodelist
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles);
-
+articles = Array.from(articles).map(article => new Article(article));
+// ======= COMPREHENSION CHECK =======
+// Array.from converts nodelist into real array, giving us acces to .map
+// .map creates a new array
+// domElement is our parameter, it acts like [i] in a for loop
+// new keyword creates a new 'instance' of our Article class aka component
