e0c14088bc1965835a1fc4becfcf07787115a0d0
Carlos Lantigua <clantigua954@gmail.com>
2018-09-19 16:03:28 -0400

    Carlos Lantigua

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,29 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
+  // Toggle the "menu--open" class on your menu refence.
+}
+
+const cantToggle = () => {
+  return 'cant be clicked';
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+let menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+let menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleMenu();
+
+  //event for clicking outside of the menu button
+  // window.addEventListener('click', (e) => {
+  //   e.stopPropagation();
+  //   if (toggleMenu() !== true) {
+  //     cantToggle();
+  //   } else {
+  //     toggleMenu();
+  //   }
+  // });
+})
