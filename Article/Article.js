8d31814df3af6ef82588b7be4beb7b2859347b74
JacobABrennan <jacobabrennan@gmail.com>
2018-09-19 16:40:36 -0400

    Stretch Goal: Dynamic Articles

--- Article/Article.js
+++ Article/Article.js
@@ -33,3 +33,53 @@ articles = Array.from(articles);
 articles.map((articleElement) => {
   return new Article(articleElement);
 })
+
+
+//== Stretch Goal: Dynamically add new Articles ================================
+
+//-- The addArticle Function ---------------------
+function addArticle (articleData){
+  // Create Basic HTML elements for article
+  const articleBody    = document.createElement('div' );
+  const articleTitle   = document.createElement('h2'  );
+  const articleDate    = document.createElement('p'   );
+  const articleToggle  = document.createElement('span');
+  // Set classes on subelements
+  articleBody  .classList.add('article'     );
+  articleDate  .classList.add('date'        );
+  articleToggle.classList.add('expandButton');
+  // Set text content of basic subelements
+  articleTitle.innerText = articleData.title;
+  articleDate .innerText = articleData.date.toDateString(); // Not exactly the same, but close enough for this demo
+  // Append subelements (up to the article body)
+  articleBody.appendChild(articleTitle);
+  articleBody.appendChild(articleDate);
+  // Create a paragraph element for each line of text in the article, append to the article element
+  articleData.body.split('\n').forEach(line => {
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = line;
+    articleBody.appendChild(newParagraph);
+  });
+  // Append remaining subelements
+  articleBody.appendChild(articleToggle);
+  // Prepend Article to articles list
+  document.querySelector('.articles').insertBefore(articleBody, document.querySelector('.articles .article'));
+  // Create Component from DOM Element
+  new Article(articleBody);
+}
+
+//-- A form for adding articles at runtime -------
+document.querySelector('#publish').addEventListener('submit', function (event){
+  // Construct Article Data from user input
+  const articleData = {};
+  articleData.date  = new Date();
+  articleData.title = this.elements['title'].value;
+  articleData.body  = this.elements['body' ].value;
+  // Clear form, scroll to top of page
+  this.reset();
+  window.scrollTo({top: 0});
+  // Prevent page refresh
+  event.preventDefault();
+  // Create the new Article
+  addArticle(articleData);
+});
