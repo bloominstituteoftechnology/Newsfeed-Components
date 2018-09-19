0ddf5cb504ed6ec1128869d56bff52d7437175fd
rytwalker <rytwalker@gmail.com>
2018-09-19 16:38:59 -0400

    Added animation to the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,27 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open');
+  // menu.classList.toggle('menu--open');
+  TweenMax.to(menu, 1, {
+    width: 350
+  });
+
+  TweenMax.to('.articles', 1, {
+    paddingLeft: 350
+  });
+
+  TweenMax.to('.menu ul li', 1, {
+    opacity: 1
+  });
+
+  TweenMax.staggerFrom(
+    '.menu ul li',
+    1,
+    {
+      x: -500,
+      delay: 0.3
+    },
+    0.2
+  );
 };
 
 // Start Here: Create a reference to the ".menu" class
