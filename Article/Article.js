5e20899f8f3865c2ad8996edf3f62fa70bf1708f
Edward J <edjeong99@gmail.com>
2018-09-19 18:04:16 -0400

    stretch - add close button

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,6 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -12,8 +13,20 @@ class Article {
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());    
+
+    let newElement = document.createElement("span");
+    newElement.className = 'close';
+    newElement.innerText = "Close";
+    this.domElement.append(newElement);
+    
+    this.close  = this.domElement.querySelector('.close');
+    this.close.addEventListener('click', () => this.closeArticle());    
+
   }
 
+  closeArticle() {
+    this.domElement.style.display = "none";
+  }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
