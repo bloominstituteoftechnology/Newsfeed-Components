713c1201e6919c139ba13de394b968c6364dd459
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-19 15:18:55 -0600

    mostly finished. The expand button changes based on wether it is open or closed

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,9 @@ class Article {
     this.expandButton = element.querySelector('.expandButton');
     // create a reference to the ".expandButton" class. 
     //this.expandButton();
-    this.element.addEventListener('click', () => this.expandArticle(element))
+    this.expandButton.addEventListener('click', () => this.expandArticle(element))
+    this.expandButton.innerHTML = 'Expand';
+   // this.buttonText();
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
@@ -16,17 +18,17 @@ class Article {
 
   expandArticle() {
     this.element.classList.toggle('article-open');
-    // console.log('this is the expand article method')
+    if(this.expandButton.innerHTML === 'Expand') {
+      this.expandButton.innerHTML = 'Close';
+    } else {
+      this.expandButton.innerHTML = 'Expand';
+    }
 
 
     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
   }
 
-  expandButton() {
-    // this.element.addEventListener('click', () => this.expandButton(element))
-    console.log('this is the expand button method') 
-  }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
