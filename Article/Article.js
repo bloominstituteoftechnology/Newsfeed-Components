29a4f5f8dc34fd728edbc48120ddbf7aa6d21c0a
Gannon <gkleblanc@gmail.com>
2018-09-19 16:43:25 -0400

    finished the article JS work

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,30 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.domElement.addEventListener('click', () => this.expandArticle(event));
   }
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+  expandArticle(event) {
+    // Using our reference to the domElement, toggle a class to expand or hide the article.    sooooo should this be articles.classList.toggle??
+    event.stopPropagation();
+    this.domElement.classList.toggle('article')
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article')
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles);
+articles = Array.from(articles).map(domElement => {
+  return new Article(domElement);
+});
+
 
