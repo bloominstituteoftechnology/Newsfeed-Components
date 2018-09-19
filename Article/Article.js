5e5413cdf8f0e59b9d25f64cced24048b8f04201
Brad Mortensen <b.mort86@gmail.com>
2018-09-19 16:23:08 -0600

    animated the menu bar in an out

--- Article/Article.js
+++ Article/Article.js
@@ -28,5 +28,7 @@ articles = Array.from(articles).map(domElement =>{
 });
 
 
-TweenMax.to(".tweet", 3, {rotation:1080, scale:0.60});
+// TweenMax.to(".tweet", 2, {rotation:1440, scale:1.0});
 
+TweenMax.from(".tweet", 1, {opacity: 0, scale:.2, y:-200});
+TweenMax.to(".tweet", 2.5, { ease: Bounce.easeOut, y:3});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,11 +2,17 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open') 
 }
-
+const animateMenuOut = () => {
+  TweenMax.to(menu, 1, {x:350}); 
+}
+const animateMenuIn = () => {
+  TweenMax.to(menu, 1, {x:-350});
+}
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener('click', () => toggleMenu());
+menuButton.addEventListener('mouseover', () => animateMenuOut());
+menu.addEventListener('mouseleave', () => animateMenuIn());   
