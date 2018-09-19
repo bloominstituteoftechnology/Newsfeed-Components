7cea715e672161bcddb243770a50fbdca40a7552
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-19 14:18:46 -0600

    expand article is working. Still need to do something with expandButton????

--- Article/Article.js
+++ Article/Article.js
@@ -4,8 +4,10 @@ class Article {
   constructor(element) {
     // assign this.element to the passed in article element
     this.element = element;
+    this.expandButton = element.querySelector('.expandButton');
     // create a reference to the ".expandButton" class. 
-    this.expandButton();
+    //this.expandButton();
+    this.element.addEventListener('click', () => this.expandArticle(element))
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
@@ -13,14 +15,17 @@ class Article {
   }
 
   expandArticle() {
-    console.log('this is the expand article method')
+    this.element.classList.toggle('article-open');
+    // console.log('this is the expand article method')
+
+
     // Using our reference to the article element, add or remove a class to expand or hide the article.
 
   }
 
   expandButton() {
-    console.log('this is the expand button method')
-
+    // this.element.addEventListener('click', () => this.expandButton(element))
+    console.log('this is the expand button method') 
   }
 }
 
