83acacc5786d554df9a7c0ba06b5dc6f2a7ac82e
Katie Cleary <katiecleary@airmaster.lan>
2018-09-19 18:46:59 -0400

    realized the closing was built in, so spent some time trying to get the text to make more sense for usability. Realized time was up so wrapped up the required work. May continue to work on what was apparently strech

--- Article/Article.js
+++ Article/Article.js
@@ -9,16 +9,22 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference (or article element), calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle(event));}
+    this.expandButton.addEventListener('click', () => this.expandArticle(event));
+    //this.expandedButton = element.querySelector('.expandButton');
+    //this.expandedButton.addEventListener('click',() => this.closedButton(event))
+  }
     
 
   expandArticle(event) {
     this.element.classList.toggle('article');
-  }
+   } 
     // Using our reference to the article element, add or remove a class to expand or hide the article.
-
-  }
-
+ //closedButton () {
+   // if (event.currentTarget.classList != ('article')) {
+   //this.expandButton.innerText = "collapse"
+// } else if (event.currentTarget.classList = ('article')) {
+   //this.expandButton.innerText = "expand"
+ }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article')
