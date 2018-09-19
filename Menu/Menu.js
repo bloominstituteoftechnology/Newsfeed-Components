0f23020c59897b1d33cf9c1580eb561fd9e4b441
Adam <ajmboosh@gmail.com>
2018-09-19 17:48:06 -0500

    stretch partially complete: altered .menu in index.css and the toggleMenu function in Menu.js to include sliding animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,11 @@
 const toggleMenu = () => {
   menu.classList.toggle("menu--open");
+
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(".menu", 1, { left: "0px" });
+  } else {
+    TweenMax.to(".menu", 1, { x: "-350px" });
+  }
   // Toggle the "menu--open" class on your menu refence.
 };
 
