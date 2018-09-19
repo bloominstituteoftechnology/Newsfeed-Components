5de941c4b1136e6ee312800736aee2f70089d5cd
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-19 13:44:30 -0600

    the side menu is working, yay

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+// create a reference to the ".menu-button" class:
+// for some reason this started with const when I started it.
+ menuButton.addEventListener('click', e => {
+  menuButton.classList.toggle(toggleMenu());
+  // console.log('this is clicking the menu button')
+
+
+});
 // Using your menuButton reference, add a click handler that calls toggleMenu
