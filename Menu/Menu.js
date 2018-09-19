ef3cc6d151c7245ecb22879c0f66eb21573f2864
Carlos Lantigua <clantigua954@gmail.com>
2018-09-19 17:10:45 -0400

    added sliding navbar

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,20 @@
+let elIsClicked = false;
 
-const toggleMenu = () => {
-  menu.classList.toggle('menu--open');
+const openMenu = () => {
+  return (
+    elIsClicked = true),
+    menu.classList.toggle('menu--open', true),
+    TweenMax.to(".menu", 0.3, { width: "237px", opacity: '1' }),
+    TweenMax.to(".menu ul li", 0.5, { "border-bottom": "1px solid darkolivegreen" })
   // Toggle the "menu--open" class on your menu refence.
 }
 
-const cantToggle = () => {
-  return 'cant be clicked';
+const closeMenu = () => {
+  return (
+    elIsClicked = false),
+    menu.classList.toggle('menu--open', false),
+    TweenMax.to(".menu", 0.2, { width: "0", opacity: '0' }),
+    TweenMax.to(".menu ul li", 0.2, { border: "0" })
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -13,17 +22,21 @@ let menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 let menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', (e) => {
-  e.stopPropagation();
-  toggleMenu();
 
-  //event for clicking outside of the menu button
-  // window.addEventListener('click', (e) => {
-  //   e.stopPropagation();
-  //   if (toggleMenu() !== true) {
-  //     cantToggle();
-  //   } else {
-  //     toggleMenu();
-  //   }
-  // });
+
+menuButton.addEventListener('click', (e) => {
+  // open and close nav
+  if (elIsClicked === false) {
+    e.stopPropagation();
+    openMenu();
+  } else if (elIsClicked === true) {
+    e.stopPropagation();
+    closeMenu();
+  }
+})
+
+window.addEventListener('click', () => {
+  if (elIsClicked === true) {
+    closeMenu();
+  }
 })
