9428c042221d18ac79169a1275d68e9b89eaf133
Adam <ajmboosh@gmail.com>
2018-09-19 16:37:51 -0500

    finished building extra mock articles with same functionality

--- Article/Article.js
+++ Article/Article.js
@@ -9,12 +9,12 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", this.expandArticle);
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
   }
 
   expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.parentNode.classList.toggle("article-open");
+    this.domElement.classList.toggle("article-open");
   }
 }
 
