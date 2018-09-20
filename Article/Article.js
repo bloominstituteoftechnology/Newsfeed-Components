4480e266efeb7e9c5fa19f31535b9fc0f81f7fc7
Carlos Lantigua <clantigua954@gmail.com>
2018-09-19 20:34:31 -0400

    completed sliding article animation

--- Article/Article.js
+++ Article/Article.js
@@ -8,15 +8,30 @@ class Article {
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
+    this.clicked = false;
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => {
-      this.expandArticle();
+      if (this.clicked === false) {
+        console.log(this.clicked);
+        this.expandArticle();
+      } else if (this.clicked === true) {
+        console.log(this.clicked);
+        this.closeArticle();
+      }
     })
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    // this.domElement.classList.toggle('article-open');
+    TweenMax.to(this.domElement, 0.3, { height: "400px" });
+    this.clicked = true;
+  }
+  closeArticle() {
+    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    // this.domElement.classList.toggle('article-open');
+    TweenMax.to(this.domElement, 0.3, { height: "50px" });
+    this.clicked = false;
   }
 }
 
