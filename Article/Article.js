95928ed95c88150d2ea83ae449a92581d9f97d58
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-09-19 23:30:02 -0700

    Stretch: Menu and article animations

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,18 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = "Expand";
+    this.expandButton.innerText = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     // this.expandButton.addEventListener("click", this.expandArticle.bind(this));
     this.expandButton.addEventListener("click", e => this.expandArticle(e));
   }
 
   expandArticle(event) {
+    event.stopPropagation();
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+    TweenMax.fromTo(this.domElement, 4, { height: 50 }, { height: 400 });
+    this.expandButton.innerText = "Click to Close";
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,15 @@
-const toggleMenu = () => {
+const toggleMenu = event => {
   // Toggle the "menu--open" class on your menu reference.
+  event.stopPropagation();
   menu.classList.toggle("menu--open");
+  if (!tweenMenu.isActive()) {
+    tweenMenu.reversed(!tweenMenu.reversed());
+  }
+  // menu.classList.toggle("menu--closed");
 };
+
+let body = document.querySelector("body");
+body.addEventListener("click", toggleMenu);
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
@@ -9,3 +17,16 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
+
+let tweenMenu = TweenMax.to(menu, 0.3, { x: 350 }).reverse();
+
+// window.addEventListener("click", function() {
+//   TweenMax.fromTo(menu, 1, { x: 0 }, { x: -350 });
+// });
+
+// const menuClose = () => {
+//   // Toggle the "menu--open" class on your menu reference.
+//   event.stopPropagation();
+//   window.classList.toggle("menu--open");
+//   TweenMax.fromTo(menu, 1, { x: -350 }, { x: 0 });
+// };
