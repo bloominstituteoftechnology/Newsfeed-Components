d134db3e108fc7a9820636fb9439a04f405cd305
Stublink <chrisrey95@gmail.com>
2018-09-19 23:39:37 -0400

    New Article Form

--- Article/Article.js
+++ Article/Article.js
@@ -7,9 +7,9 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = 'Click to Expand';
+    this.expandButton.innerText = 'Click to Close'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', (e) => this.expandArticle(e))
+    this.expandButton.addEventListener('click', e => this.expandArticle(e))
   }
 
   expandArticle(e) {
@@ -23,11 +23,42 @@ class Article {
   }
 }
 
+class CloseButton {
+  constructor(domElement) {
+    this.domElement = domElement;
+    this.domElement.addEventListener('click', e => this.closeArticle(e));
+  } 
+
+  closeArticle(e) {
+    e.stopPropagation();
+    this.domElement.parentNode.classList.add('hide-article')
+  }
+}
+
+
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles).map(de => {
-  return new Article(de);
-});
+articles = Array.from(articles).map(de => new Article(de));
 
+let closeBtn = document.querySelectorAll('.close-btn');
+closeBtn = Array.from(closeBtn).map(de => new CloseButton(de));
+
+
+let addButton = document.querySelector('.add-btn');
+
+addButton.addEventListener('click', e => {
+  e.currentTarget.parentNode.classList.toggle('show-form');
+  if(e.currentTarget.classList.contains('fa-plus')) {
+    e.currentTarget.className = 'fas fa-minus add-btn';
+  } else {
+    e.currentTarget.className = 'fas fa-plus add-btn';
+  }
+})
+
+let submitButton = document.querySelector('form > button');
+
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+})
