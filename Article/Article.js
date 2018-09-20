c38719c521df53584e902116613378018505907c
Carlos Lantigua <clantigua954@gmail.com>
2018-09-20 09:53:47 -0400

    added delete function with animation

--- Article/Article.js
+++ Article/Article.js
@@ -4,14 +4,17 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
+    this.expandButton.classList.add('fa-angle-down');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = 'Read More';
+    //this.expandButton.innerText = 'Read More';
 
     this.deleteButton = this.domElement.querySelector('.deleteButton');
-    this.deleteButton.innerText = 'delete';
+    // this.deleteButton.innerText = 'delete';
 
-    console.log(this.deleteButton);
     this.clicked = false;
+    this.deleteButton.addEventListener('click', () => {
+      this.deleteArticle();
+    })
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => {
       if (this.clicked === false) {
@@ -26,13 +29,21 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     // this.domElement.classList.toggle('article-open');
     TweenMax.to(this.domElement, 0.3, { height: "400px" });
+    this.expandButton.classList.toggle('fa-angle-down');
+    this.expandButton.classList.toggle('fa-angle-up');
     this.clicked = true;
   }
   closeArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     // this.domElement.classList.toggle('article-open');
     TweenMax.to(this.domElement, 0.3, { height: "50px" });
+    this.expandButton.classList.toggle('fa-angle-up');
+    this.expandButton.classList.toggle('fa-angle-down');
     this.clicked = false;
+  }
+  deleteArticle() {
+    TweenMax.to(this.domElement, 0.3, { height: 0, width: 0 });
+    TweenMax.to(this.domElement, 0.3, { display: "none" });
   }
 }
 
