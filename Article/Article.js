d6fa513ee198fef0bb5ae685a3e857b028140553
SabrinaGear <sabrinagear.ft@gmail.com>
2018-09-20 07:06:37 -0500

    articles. Plus I had to add scroll bars because it was driving me crazy

--- Article/Article.js
+++ Article/Article.js
@@ -7,6 +7,7 @@ class Article {
     constructor(domElement) {
         // assign this.domElement to the passed in domElement
         this.domElement = domElement;
+
         // create a reference to the ".expandButton" class. 
         this.expandButton = this.domElement.querySelector(".expandButton");
         // Using your expandButton reference, update the text on your expandButton to say "expand"
@@ -14,6 +15,7 @@ class Article {
         // Set a click handler on the expandButton reference, calling the expandArticle method.
         this.expandButton.addEventListener("click", (event) => {
             this.expandArticle();
+            domElement.style.overflowY = "scroll";
 
         })
     }
@@ -23,6 +25,7 @@ class Article {
     expandArticle() {
         // Using our reference to the domElement, toggle a class to expand or hide the article.
         this.domElement.classList.toggle('article-open');
+
 
     }
 }
