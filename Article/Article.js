970dcd0c215052c38b0ca119ac4e335e710fba59
mikaelacurrier <mikaela.currier@gmail.com>
2018-09-19 20:10:17 -0400

    fixed task 2

--- Article/Article.js
+++ Article/Article.js
@@ -1,24 +1,24 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerHTML = 'expand';
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    expandButton.addEventListener('click', () => {
-        this.expandArticle()}); 
+    constructor(domElement) {
+        // assign this.domElement to the passed in domElement
+        this.domElement = domElement;
+        // create a reference to the ".expandButton" class. 
+        this.expandButton = domElement.querySelector('.expandButton');
+        // Using your expandButton reference, update the text on your expandButton to say "expand"
+        this.expandButton.innerHTML = 'expand';
+        // Set a click handler on the expandButton reference, calling the expandArticle method.
+        let self = this;
+        this.expandButton.addEventListener('click', function () {
+            self.expandArticle();
+        });
     }
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-  expandArticle(){
+    expandArticle() {
         this.domElement.classList.toggle('article-open')
-       }
-
-  
     }
+}
 
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
