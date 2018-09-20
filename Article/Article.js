f44def44fd799d7cf73d97e3add918c21292d039
kyranmccann <kyran.mccann@gmail.com>
2018-09-19 23:43:54 -0700

    formatting changes/cleaning up and fixing window listener

--- Article/Article.js
+++ Article/Article.js
@@ -5,57 +5,57 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
-    this.expandButton = domElement.querySelector('.expandButton');
+    this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-     this.expandButton.innerHTML = 'Click to Expand';
+    this.expandButton.innerHTML = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-     this.expandButton.addEventListener('click', () => this.expandArticle(event));
+    this.expandButton.addEventListener("click", () =>
+      this.expandArticle(event)
+    );
     // Setting a click handler on the trash reference, calling the removeArticle method.
-     this.trash = domElement.querySelector('.trash');
-     this.trash.addEventListener('click', () => this.removeArticle(event));
-
+    this.trash = domElement.querySelector(".trash");
+    this.trash.addEventListener("click", () => this.removeArticle(event));
   }
-
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article (and change the text depending on state).
     event.stopPropagation();
-    this.domElement.classList.toggle('article-open');
-    if (this.expandButton.innerHTML == 'Click to Expand'){
-      this.expandButton.innerHTML = 'Click to Close';
+    this.domElement.classList.toggle("article-open");
+    if (this.expandButton.innerHTML == "Click to Expand") {
+      this.expandButton.innerHTML = "Click to Close";
     } else {
-      this.expandButton.innerHTML = 'Click to Expand';
+      this.expandButton.innerHTML = "Click to Expand";
     }
   }
   //hides an article when clicking on the trash button.
-  removeArticle(){
+  removeArticle() {
     event.stopPropagation();
-    this.domElement.classList.add('done');
+    this.domElement.classList.add("done");
   }
-}
+}; //end Article class 
 
 //stretch - function to convert data to articles
-function convertArticle(article){
-  let newArticleHeader = document.createElement('h2');
+function convertArticle(article) {
+  let newArticleHeader = document.createElement("h2");
   newArticleHeader.innerText = article.header;
 
-  let newArticleDate = document.createElement('p');
-  newArticleDate.classList.add('date');
+  let newArticleDate = document.createElement("p");
+  newArticleDate.classList.add("date");
   newArticleDate.innerText = article.date;
 
-  let newArticleText = document.createElement('p');
+  let newArticleText = document.createElement("p");
   newArticleText.innerText = article.text;
 
-  let newExpandButton = document.createElement('span');
-  newExpandButton.classList.add('expandButton');
+  let newExpandButton = document.createElement("span");
+  newExpandButton.classList.add("expandButton");
 
-  let newTrash = document.createElement('div');
-  newTrash.classList.add('trash');
-  newTrash.innerHTML='<img src="assets/trash.png" alt="trash can"/>';
+  let newTrash = document.createElement("div");
+  newTrash.classList.add("trash");
+  newTrash.innerHTML = '<img src="assets/trash.png" alt="trash can"/>';
 
-  let articleContainer = document.querySelector('.articles');
+  let articleContainer = document.querySelector(".articles");
 
-  let addArticles = document.createElement('div');
-  addArticles.classList.add('article')
+  let addArticles = document.createElement("div");
+  addArticles.classList.add("article");
   addArticles.appendChild(newTrash);
   addArticles.appendChild(newArticleHeader);
   addArticles.appendChild(newArticleDate);
@@ -66,26 +66,27 @@ function convertArticle(article){
 }
 
 //form input of articles
-let form = document.querySelector('#write-article');
-form.querySelector('.button').addEventListener('click', event => {
+let form = document.querySelector("#write-article");
+form.querySelector(".button").addEventListener("click", event => {
   event.preventDefault();
-  if (form.querySelector('#header').value == '' || form.querySelector('#text').value == '') {
-    alert('Neither field can be empty!')
+  if (
+    form.querySelector("#header").value == "" ||
+    form.querySelector("#text").value == ""
+  ) {
+    alert("Neither field can be empty!");
     return false;
-
   }
   convertArticle({
-    header: form.querySelector('#header').value,
+    header: form.querySelector("#header").value,
     date: new Date().toDateString(),
-    text: form.querySelector('#text').value,
-  })
-  form.querySelector('#header').value = '';
-  form.querySelector('#text').value= '';
-})
-
+    text: form.querySelector("#text").value
+  });
+  form.querySelector("#header").value = "";
+  form.querySelector("#text").value = "";
+});
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles = document.querySelectorAll('.article');
+let articles = document.querySelectorAll(".article");
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles).map(domElement => {
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,12 +2,12 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
  menu.classList.toggle('menu--open');
- //adds an event listener to the window that will close the menu, but it's set to once: true so that it's not continuously hanging out doing nothing. 
+ //adds an event listener to the window that will close the menu
  window.addEventListener('mouseup', (event) => {
    if (event.target != menu && event.target != menuButton && event.target.parentNode.parentNode != menu) {
      menu.classList.remove('menu--open');
    }
- },{once: true})
+ }); 
 }
 
 // Start Here: Create a reference to the ".menu" class
