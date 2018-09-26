2175867fbda72221894552974b264ba9822ecf37
Barbara Hunter <commandandcode@gmail.com>
2018-09-26 17:24:50 -0400

    continue work on Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -5,22 +5,28 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = "expand";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+
+
     
   }
 
   expandArticle() {
+    
     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = querySelectorAll ('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 // articles = Array.from(articles);
+
+articles = Array.from(articles).map(new Article );
 
