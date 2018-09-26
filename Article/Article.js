dcbd8756a29d1ee35ce8c83c738b1e60ecb93ac1
2thumbstech <2thumbstech@gmail.com>
2018-09-26 15:24:19 -0600

    2nd commit

--- Article/Article.js
+++ Article/Article.js
@@ -19,7 +19,8 @@ class Article {
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles= '.article'
+
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
