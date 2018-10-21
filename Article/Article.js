02074ee47be7d9655d176f3cc5a67950ab981116
Holden B <holden.bucher.92@gmail.com>
2018-10-20 21:04:27 -0500

    added DOM JS per instructions

--- Article/Article.js
+++ Article/Article.js
@@ -2,25 +2,22 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.domElement = domElement;
+
+    this.expandButton = domElement.getElementsByClassName("expandButton");
+
+    for (let i = 0; i < this.expandButton.length; i++) {
+      this.expandButton[i].textContent = 'expand';
+      this.expandButton[i].addEventListener('click', this.expandArticle);
+    }
   }
-
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.parentElement.classList.toggle('article-open');
+    if (this.textContent === 'expand') {
+      this.textContent = 'close';
+    } else {this.textContent = 'expand';}
   }
 }
+let articles = document.querySelectorAll('.article');
 
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
-
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles);
-
+articles = Array.from(articles).map(item => new Article(item));
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,12 @@
 
 const toggleMenu = () => {
+  menu.classList.toggle('menu--open')
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", toggleMenu);
 // Using your menuButton reference, add a click handler that calls toggleMenu
