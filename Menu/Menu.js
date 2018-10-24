3dc1e3bd1316d648d58bab36e1b54a5fc4da9a9b
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-10-24 18:11:36 -0500

    added animation to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,20 +1,9 @@
-
-// const toggleMenu = () => {
-//   menu.classList.toggle('menu--open');// Toggle the "menu--open" class on your menu refence. 
-// }
-
-// // Start Here: Create a reference to the ".menu" class
-// const menu = document.querySelectorAll('.menu');
-// // create a reference to the ".menu-button" class
-// const menuButton = document.querySelector('.menu-button');
-// // Using your menuButton reference, add a click handler that calls toggleMenu
-// menuButton.addEventListener('click',toggleMenu);
-
-
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
+  TweenMax.to('.menu', 2, {left:50, opacity: .8, ease:Power4.easeOut});
 }
+
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
