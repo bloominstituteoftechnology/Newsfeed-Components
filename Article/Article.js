77df0b02903db5209d28a90371ef6b166c5099c1
Jasmine <jasmineanderson315@yahoo.com>
2018-10-24 16:16:09 -0400

    fixed Article component. expand button wasn't showing at first

--- Article/Article.js
+++ Article/Article.js
@@ -5,9 +5,9 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector(".domElement span.expandButton");
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand"
+    this.expandButton.innerHTML = "expand"  //why do i have to use inner HTML. why doesnt textContent work
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
