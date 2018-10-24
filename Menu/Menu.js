46773b459ca6c97d8caa14dd7984ca8fbb22a412
Holden B <holden.bucher.92@gmail.com>
2018-10-24 14:33:20 -0500

    fixed issue with document click event where menu would not hide when open and click registers on screen

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,7 +5,8 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", toggleMenu);
 document.addEventListener('click', (event) => {
-  if (event.target !== menuButton && menu.classList.contains('menu--open')) {
+  if (event.target !== menuButton && menu.classList.contains('slide')) {
     menu.classList.toggle('slide');
+    console.log(event.target);
   }
 })
