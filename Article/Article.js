b07601cdf0155044f9c85f0fe784dd1f7be9e27c
Josh Z <jzieger10@gmail.com>
2018-10-24 17:28:56 -0400

    Finished article show/hide functionality

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,24 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.innerText = 'Expand'
+  
+     this.expandButton.addEventListener('click', () => this.expandArticle(event))
+   
     
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     
+      
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
+    }
   }
-}
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
