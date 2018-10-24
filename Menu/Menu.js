fadb28ca13a0482e513c177b07c9bc9b20e7e53a
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-24 12:17:41 -0700

    menu and menuButton now referencing the DOM

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,9 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+console.log(menu);
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
