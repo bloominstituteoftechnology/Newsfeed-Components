e3b1ce503417891077399b034560c42e53596e68
Trai Lynne <trailynne@outlook.com>
2018-10-24 14:04:35 -0600

    Article: Created reference to .article classes using Query Selector All

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,26 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    constructor(domElement) {
+        // assign this.domElement to the passed in domElement
+        this.domElement;
+        // create a reference to the ".expandButton" class. 
+        this.expandButton;
+        // Using your expandButton reference, update the text on your expandButton to say "expand"
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+        // Set a click handler on the expandButton reference, calling the expandArticle method.
 
-  }
+    }
+
+    expandArticle() {
+        // Using our reference to the domElement, toggle a class to expand or hide the article.
+
+    }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
+
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
-
