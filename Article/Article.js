95084b47a0f82cde65f9ba749856c7b5fe8b3bc0
jheinz1999 <jonathan.heinz@aol.com>
2018-10-24 13:32:38 -0700

    implemented article builder

--- Article/Article.js
+++ Article/Article.js
@@ -53,3 +53,46 @@ let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
+
+// Article Builder
+
+function buildArticle(title, date, content) {
+
+  // Create elements
+
+  let newArticle = document.createElement('div');
+  let h2 = document.createElement('h2');
+  let dateTag = document.createElement('p');
+  let contentTag = document.createElement('p');
+  let expand = document.createElement('span');
+  let close = document.createElement('span');
+
+  // Add element classes
+
+  newArticle.classList.add('article');
+  dateTag.classList.add('date');
+  expand.classList.add('expandButton');
+  close.classList.add('close');
+
+  // Populate elements
+
+  h2.textContent = title;
+  dateTag.textContent = date;
+  contentTag.textContent = content;
+  close.textContent = "Read";
+
+  // Nest everything correctly
+
+  newArticle.appendChild(h2);
+  newArticle.appendChild(dateTag);
+  newArticle.appendChild(contentTag);
+  newArticle.appendChild(expand);
+  newArticle.appendChild(close);
+
+  return new Article(newArticle);
+
+}
+
+// Add a new article without touching html
+
+document.querySelector('.articles').appendChild(buildArticle('Dynamically Created Articles Are Now A Thing!', 'Today', 'This is unbelievable. I was made in JavaScript!').domElement);
