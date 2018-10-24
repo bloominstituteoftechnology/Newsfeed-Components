da94ea13c3f6464fedc8fc949d594029516243ef
BrandCam <BrandCam>
2018-10-24 17:07:43 -0500

    added close button

--- Article/Article.js
+++ Article/Article.js
@@ -6,17 +6,23 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton') ;
+    //close button  
+    this.closeButton = this.domElement.querySelector('.close');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
-    
+    //button listener
+    this.closeButton.addEventListener('click', () => this.exitArticle());
   }
-
+  exitArticle(){
+    this.domElement.classList.toggle('article-exit');
+    console.log(this.domElement.classList);
+  }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
-    console.log(this.domElement);
+    
   }
 }
 
