204c9e4026923a445efa084b3601ddc20e3d6422
johnoro <officialjohnorourke@gmail.com>
2018-10-24 18:56:16 -0400

    Add close button and make expand button text more dynamic.

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
@@ -15,11 +15,28 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.expandButton.textContent = this.domElement.classList.contains('article-open')
+      ? 'Click to Minimize'
+      : 'Click to Expand';
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
 let articles = document.querySelectorAll('.article');
+articles.forEach(article => {
+  const closeBtn = document.createElement('span');
+  closeBtn.appendChild(document.createTextNode('Click to Close'));
+  Object.assign(closeBtn.style, {
+    position: 'absolute',
+    right: '10px',
+    top: '5px',
+    fontSize: '12px',
+    color: 'grey',
+    cursor: 'pointer'
+  });
+  closeBtn.addEventListener('click', () => article.style.display = 'none');
+  article.appendChild(closeBtn);
+});
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles).map(article => new Article(article));
