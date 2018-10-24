ae08e1e431cf6ef7abd7e617a9fcfc0a9398cd2c
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-10-24 15:36:25 -0500

    added components

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,25 @@
 
+// const toggleMenu = () => {
+//   menu.classList.toggle('menu--open');// Toggle the "menu--open" class on your menu refence. 
+// }
+
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelectorAll('.menu');
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector('.menu-button');
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener('click',toggleMenu);
+
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+
