002796e6fe58ac2956f0180784bfdd2b750d818e
jheinz1999 <jonathan.heinz@aol.com>
2018-10-24 12:56:17 -0700

    menu is now animated

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,35 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open');
+
+  if (!menuState) {
+
+    menu.classList.toggle('menu--open');
+    TweenMax.to(menu, 1, {css:{left: '0px'}, ease: Power2.easeInOut});
+
+  }
+
+  else {
+
+    TweenMax.to(menu, 1, {css:{left: '-350px'}, ease: Power2.easeInOut});
+    setTimeout(() => menu.classList.toggle('menu--open'), 1000);
+
+  }
+
+  menuState = !menuState;
+
 }
+
+// Holds whether menu is on or off
+let menuState = false;
+
+// Menu off click event listener
+document.addEventListener('click', function(event) {
+
+  if (event.target != menu && event.target != menuButton && menuState)
+    toggleMenu();
+
+});
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
