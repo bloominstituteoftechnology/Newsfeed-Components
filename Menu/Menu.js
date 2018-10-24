4f8012c5d9fdc70791be88471c62b742a1ae1d81
Thomas-Ketterman <32739188+Thomas-Ketterman@users.noreply.github.com>
2018-10-24 16:35:11 -0400

    Created event and functionality for menu opening upon menuButton click.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,9 +1,13 @@
-const toggleMenu = () => {
+const toggleMenu = e => {
   // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle("menu--open");
 };
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector("menu-button");
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", function() {
+  toggleMenu();
+});
