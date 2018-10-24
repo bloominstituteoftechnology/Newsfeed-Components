21f70b3f3e3d38f7cc4436ba3970675f247c13e7
jheinz1999 <jonathan.heinz@aol.com>
2018-10-24 12:26:57 -0700

    menu working

--- Article/Article.js
+++ Article/Article.js
@@ -4,12 +4,12 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+
   }
 
   expandArticle() {
@@ -23,4 +23,3 @@ let articles;
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
-
