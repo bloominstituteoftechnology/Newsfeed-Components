fa7448d6ee07cb78a0529585fc7fee7764cd2a72
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-24 14:32:17 -0500

    add click handler to close menu by clicking anywhere on the page but on the menu itself

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -12,8 +12,11 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
 
+// add click handler to close menu by clicking anywhere on the page but on the menu itself
 const htmlBody = document.querySelector('body');
-
-// htmlBody.addEventListener('click', function(){
-//   menu.classList.remove('menu--open');
-// })
+htmlBody.addEventListener('click', function(){
+  menu.classList.remove('menu--open');
+});
+menu.addEventListener('click', function(e) {
+  e.stopPropagation();
+});
