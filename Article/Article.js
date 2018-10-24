a6094759d26e8b8df2f814def1a9d431c40707e2
jamespagedev <jamespagedev@outlook.com>
2018-10-24 15:41:33 -0700

    added logic to function name

--- Article/Article.js
+++ Article/Article.js
@@ -13,9 +13,9 @@ class Article {
     this.expandButtonCloseText = "[close]";
     this.expandButton.textContent = this.expandButtonOpenText;
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.expandButton.addEventListener('click', this.expandAndCollapseArticle.bind(this));
   }
-  expandArticle() {
+  expandAndCollapseArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     console.log(this.article.classList);
     this.article.classList.toggle('article-open');
