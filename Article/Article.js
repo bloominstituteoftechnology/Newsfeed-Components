45537c5da8cebdf2ea33454d05cac20d9a3b890d
turkworks <cengizbug@Cengizs-iMac.local>
2018-10-24 16:47:27 -0600

    Initial push and pull request

--- Article/Article.js
+++ Article/Article.js
@@ -1,4 +1,4 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
@@ -19,7 +19,7 @@ class Article {
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
