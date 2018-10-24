c93d341cb577414dbc82b6de35dbb5b508732ca4
Trai Lynne <trailynne@outlook.com>
2018-10-24 13:29:59 -0600

    Menu: Successfully toggleMenu callback to menuButton Event Handler Confirmed .menu--open toggles on menuButton click

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,6 +10,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', (event) => {
-    console.log(event.type);
-});
+menuButton.addEventListener('click', toggleMenu);
