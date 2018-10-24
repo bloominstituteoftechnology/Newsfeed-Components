271315d309fde309337e397418adf39f965475d4
Thomas-Ketterman <32739188+Thomas-Ketterman@users.noreply.github.com>
2018-10-24 15:40:37 -0400

    Began project

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,18 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE" Starting
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
   }
 }
 
@@ -23,4 +21,3 @@ let articles;
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
-
