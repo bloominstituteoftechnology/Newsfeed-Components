dae1cd9e7283e03e03fa6e798de780d2f0823c35
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-24 21:47:42 -0400

    Quick fix

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,6 @@
 const toggleMenu = () => {
     // Toggle the "menu--open" class on your menu refence.
     menu.classList.toggle('menu--open');
-    // $(".slide-menu").trigger("open");
-    // $(".slide-menu").trigger("close");
     TweenMax.fromTo(".menu", 0.5, {
         x: -350
     }, {
