1e18331ac052879ef3b0e15b41fe49228d3ae285
wcolts2000 <wcolts2000@gmail.com>
2018-10-24 20:36:22 -0700

    added closing functionality of menu slideout on clicking anywhere outside of it when it is open

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,3 +9,21 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
+
+window.addEventListener("click", (e) => {
+  if(menu.classList.contains("menu--open") && !(e.target.classList.contains("menu-button"))){
+    if(!(e.target.classList.contains("menu--open")) && !(e.target.parentElement.parentElement.classList.contains("menu--open"))){
+
+      console.log(e);
+      menu.classList.toggle("menu--open");
+    }
+
+  }
+ 
+    // e.target.menu.classList.toggle("menu--open");
+    // console.log(e.target.parentElement.parentElement);
+    
+    // console.log(e.target.classList.contains("menu") || e.target.parentElement.parentElement.classList.contains("menu"));
+    
+  })
+  // console.log(menu.classList.contains("menu--open"));
