7c2ccff1e5be1691f38ced517b1b2c7cda5c662b
johnoro <officialjohnorourke@gmail.com>
2018-10-24 22:40:42 -0400

    Make menu close if elsewhere is clicked

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,15 +1,29 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  if (menu.classList.contains('menu--open')) {
+const isDescendant = (parent, child) => {
+  let node = child.parentNode;
+  while (node != null) {
+    if (node === parent) return true;
+    node = node.parentNode;
+  }
+  return false;
+};
+
+const closeMenu = (e) => {
+  if (e.target !== menu && !isDescendant(menu, e.target)) {
     TweenMax.to(menu, 1.25, {
       x: -350,
       onComplete: () => menu.classList.remove('menu--open'),
       ease: Power2.easeIn
     });
-  } else {
-    menu.classList.add('menu--open');
-    TweenMax.fromTo(menu, 1, { x: -350 }, { x: -25, ease: Sine.easeIn });
   }
+};
+
+const toggleMenu = event => {
+  event.stopPropagation();
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.add('menu--open');
+  TweenMax.fromTo(menu, 1, { x: -350 }, { x: -25, ease: Sine.easeIn });
+  // Make the menu close if elsewhere is clicked
+  document.querySelector('body').addEventListener('click', closeMenu);
 }
 
 // Start Here: Create a reference to the ".menu" class
