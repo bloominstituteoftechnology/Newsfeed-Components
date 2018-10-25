e6694611db81a5277a835c39cf4fde7c53303727
wcolts2000 <wcolts2000@gmail.com>
2018-10-24 19:53:07 -0700

    added conditional inside button constructor to update expand and close on bottom of article

--- Article/Article.js
+++ Article/Article.js
@@ -6,22 +6,22 @@ class Article {
     this.article = article;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.article.querySelector(".article span.expandButton");
-    
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand"
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
-
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.article.classList.toggle('article-open');
+    if(this.article.classList.contains("article-open")){
+          this.expandButton.textContent = "Click to Close";
+    } else {
+      this.expandButton.textContent = "Click to Expand";
+    }
   }
 }
-
-// START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles = document.querySelectorAll(".article");
-
-// Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-articles = Array.from(articles).map( article => new Article(article));
-
+      // START HERE: Select all classes named ".article" and assign that value to the articles variable
+      let articles = document.querySelectorAll(".article");
+      // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
+      articles = Array.from(articles).map( article => new Article(article));
