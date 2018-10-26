9a475be8d2d62be96213349c5cbcf980a4769a0e
Alex <alexander.t.carter@gmail.com>
2018-10-26 07:32:08 -0700

    vanilla final

--- Article/Article.js
+++ Article/Article.js
@@ -3,24 +3,31 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
+    
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", () => this.expandArticle(event)); //not sure if even should be passed
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    console.log(this.domElement);
+    this.domElement.classList.toggle('article-open');
+    //menu.classList.toggle('menu--open');
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
 articles = Array.from(articles);
+
+articles = Array.from(articles).map( article => {
+  return new Article(article);
+});
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,20 @@
 
+
+
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open'); //not sure this line of code is correct but we can move on
+  console.log('toggle');
 }
 
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => {
+	console.log('click');
+	//not sure if i should be using this here; since only 1 menu exists and we hard reference it i think not
+	toggleMenu();
+});
