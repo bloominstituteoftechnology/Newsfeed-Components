b1fd661ce5184cdd46fecfa7e83ac5ca63a7abfb
J A <jarata@gmail.com>
2018-10-31 12:34:35 -0700

    tidy up

--- Article/Article.js
+++ Article/Article.js
@@ -14,7 +14,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('.article-open')
+    this.domElement.classList.toggle('article-open')
   }
 }
 
