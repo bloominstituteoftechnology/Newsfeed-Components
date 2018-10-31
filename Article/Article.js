0ce67faa6e35f2170e68b6b7294710bcb356823b
Joseph Eastman <josephryan96@gmail.com>
2018-10-31 14:32:13 -0700

    added gsap animations

--- Article/Article.js
+++ Article/Article.js
@@ -15,6 +15,7 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.domElement.style.backgroundColor = 'brown';
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,9 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open')
+  menu.classList.toggle('menu--open');
+  TweenMax.to(menu, 4, {left:560, opacity: 1, ease:Power4.easeIn});
+  menu.style.width = '800px';
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,3 +12,15 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+const list = document.querySelectorAll('.menu ul li');
+
+list.forEach(item => item.addEventListener('click', () => TweenMax.to(list, 5, {rotation:360, scale:1, ease:Power4.easeOut})));
+
+const body = document.querySelector('body');
+
+document.body.style.backgroundImage = "url('/assets/green-background.jpg')";
+
+const heading = document.querySelector('.header h1');
+
+heading.style.color = 'black';
