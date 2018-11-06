a7e48f8f1e72e2e1a68500ad17dc08143949219c
Jason Cook <the.root.user@gmail.com>
2018-11-06 16:56:21 -0500

    Animate expanse/closure of articles with GSAP

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.article .expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
@@ -15,6 +15,14 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    if (this.domElement.classList.contains('article-open')) {
+      TweenMax.fromTo((this.domElement), .7, {css:{height: "50px"}}, {css:{height: "400px"}});
+      this.expandButton.textContent = 'Click to Close';
+    } else {
+      TweenMax.fromTo((this.domElement), .7, {css:{height: "400px"}}, {css:{height: "50px"}});
+      this.expandButton.textContent = 'Click to Expand';
+
+    }
 
   }
 }
