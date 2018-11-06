57a610b4d4f351cf0c6bac96e70d847cecab3b2e
Paul DeYonghe <p.deyonghe@gmail.com>
2018-11-05 23:20:52 -0600

    fifth commit: now stuck on event listener in Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,13 @@
 
-const toggleMenu = (menu) => {
+ toggleMenu = (menu) => {
   // Toggle the "menu--open" class on your menu refence. 
   this.menu.classList.toggle('.menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = this.menuButton.querySelector('.menu');
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton = this.menuButton.querySelector('.menu-button');
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-this.menuButton.addEventListener('click', this.toggleMenu.bind(this))
+console.log(this.menuButton);
+this.menuButton.addEventListener('click', this.toggleMenu.bind(this));
