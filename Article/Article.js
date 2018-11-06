7435c57bf8d07e71d9870aff885bcb8eb25bfebc
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-06 08:05:06 -0500

    objective #2 - article.js
    - add reference to expandButton class
    - add text to expandButton class
    - add classList toggle to expandArticle callback
    - add addEventListener to expandButton
    - add menu and article less files to main index.less
    - add new article

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -22,8 +22,13 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
+
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+// let articleArray = Array.from(articles);
+articles.forEach((item) => {
+  return new Article(item);
+});
