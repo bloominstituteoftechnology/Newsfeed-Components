e41a92a2bd5eee01d42aff8198688abfe2d18d2d
Trip1701 <bj2336@email.vccs.edu>
2018-11-06 01:35:15 -0500

    Click Event Added to Window to Close Menu if Open

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,8 +5,10 @@ const toggleMenu = () => {
   console.log(menu.classList.contains('menu--open'))
   if(menu.classList.contains('menu--open')) {
     TweenMax.fromTo((menu), .5, {x:-350}, {x:0});
+        event.stopPropagation();
   } else {
     TweenMax.fromTo((menu), .5, {x:0}, {x:-350});
+        event.stopPropagation();
   }
 }
 
@@ -16,3 +18,13 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+window.addEventListener('click', function(event) {
+
+  if(menu.classList.contains('menu--open')) {
+    menu.classList.toggle('menu--open');
+    TweenMax.fromTo((menu), .5, {x:0}, {x:-350});
+  } else {
+    return
+  }
+
+})
