b4d6afa0199241f64582efc6f2d2f059b373175a
CatsAreMLG <joshuadgon@gmail.com>
2018-11-06 00:00:44 -0600

    Added gsap animations to the menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,13 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle("menu--open");
+  if (Array.from(menu.classList).includes("menu--open")) {
+    TweenMax.to(menu, 0, { x: 0 });
+    TweenMax.to(menu, 0.35, { x: 350 });
+  } else {
+    TweenMax.to(menu, 0, { x: 350 });
+    TweenMax.to(menu, 0.25, { x: 0 });
+  }
 };
 
 // Start Here: Create a reference to the ".menu" class
