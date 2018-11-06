b233504762d2aa4bc9fbfad8db82fc4954cd9449
Bao Pham <ryanvuepham@gmail.com>
2018-11-05 23:11:32 -0600

    finished selecting all .article class

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   // using .classList?
-  menu.classList.toggle(".menu--open")
+  menu.classList.toggle("menu--open")
 }
 
 // Start Here: Create a reference to the ".menu" class
