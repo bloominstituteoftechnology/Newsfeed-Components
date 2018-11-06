42bfa91442fa984e952f7e0447b870faf5c9c293
Trip1701 <bj2336@email.vccs.edu>
2018-11-06 01:28:06 -0500

    Animated Menu via GSAP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,12 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
+  console.log(menu.classList.contains('menu--open'))
+  if(menu.classList.contains('menu--open')) {
+    TweenMax.fromTo((menu), .5, {x:-350}, {x:0});
+  } else {
+    TweenMax.fromTo((menu), .5, {x:0}, {x:-350});
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,4 +15,4 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu())
+menuButton.addEventListener('click', toggleMenu);
