6c7fcd15fbffb12608b014d53858f6d78ae460bc
Trip1701 <bj2336@email.vccs.edu>
2018-11-06 12:21:08 -0500

    Component Constructor Function Complete

--- Article/Article.js
+++ Article/Article.js
@@ -30,5 +30,60 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
+let header = 'Some Kind Of Header 2';
+let date = 'Nov 6th, 2018';
+const contentArr = [
+    "Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa.",
 
+    "Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa."];
+
+let header2 = 'This Article Was Made with JS';
+let date2 = 'Nov 10th, 2018';
+const contentArr2 = [
+    "Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa.",
+
+    "Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2 windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba owen jinn tatooine sith organa."];
+
+
+//Article Contructor Function
+function articleConstructor(header, date, contentArr) {
+  //create article container
+  let articleContainer = document.createElement('div')
+  articleContainer.classList.add('article');
+
+  //create header, add to container
+  let head = document.createElement('h2');
+  head.innerText = header;
+  articleContainer.appendChild(head);
+
+  //create date, add to container
+  let articleDate = document.createElement('p');
+  articleDate.classList.add('date');
+  articleDate.innerText = date;
+
+  articleContainer.appendChild(articleDate)
+
+  //create content, add to container
+  for(let i = 0; i < contentArr.length; i++) {
+    let content = document.createElement('p');
+    content.innerText = contentArr[i];
+
+    articleContainer.appendChild(content);
+  }
+
+  //create button, add to container
+  let btn = document.createElement('span');
+  btn.classList.add('expandButton');
+
+  articleContainer.appendChild(btn);
+
+  document.querySelector('.articles').appendChild(articleContainer);
+}
+
+
+//Construct articles
+articleConstructor(header, date, contentArr);
+articleConstructor(header2, date2, contentArr2);
+
+//Assign article class
 let articles = document.querySelectorAll('.article').forEach(article => new Article(article));
