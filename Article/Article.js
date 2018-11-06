670dc5c6dd5f5152cd4f4a3dc3111c69d94ecdc0
Bill Geist <wcgeist@gmail.com>
2018-11-06 00:38:07 -0500

    article js

--- Article/Article.js
+++ Article/Article.js
@@ -1,19 +1,26 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoised you will need to start your code at the bottom of the page.  
+// Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton = this.domElement.querySelectorAll('.expandButton');
+    
+    // Using your expandButton reference, update the text on your expandButton to say 
+    // "expand"
+    this.expandButton.textContent = 'expand';
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
+    this.expandButton.addEventListener('click', expandArticle);
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    domElement.classList.toggle('active'); 
 
   }
 }
@@ -22,8 +29,14 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+- With your selection in place, now chain .forEach() on to the articles variable to 
+  iterate over the articles NodeList and create a new instance of Article by passing 
+  in each article as a parameter to the constructor.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article.');
+
+articles.forEach(function (article) {
+  return new Article(article);
+})
