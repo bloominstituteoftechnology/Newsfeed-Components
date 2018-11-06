b2f9a0c07777bf0d0f3b767f0fa2e9e6177c7fdb
Trip1701 <bj2336@email.vccs.edu>
2018-11-06 17:31:36 -0500

    Form Style Started, Functionality Complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -18,12 +18,3 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
-window.addEventListener('click', function(event) {
-
-  if(menu.classList.contains('menu--open')) {
-    menu.classList.toggle('menu--open');
-    TweenMax.fromTo((menu), .5, {x:0}, {x:-350});
-  } else {
-    return
-  }
-})
