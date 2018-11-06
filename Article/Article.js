8178f284854317a4cf816de001e935de35f1a7bc
Paul DeYonghe <p.deyonghe@gmail.com>
2018-11-05 22:53:50 -0600

    fourth commit: stuck on an uncaught type error in Menu.js, seeking assistance

--- Article/Article.js
+++ Article/Article.js
@@ -3,9 +3,9 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -20,10 +20,14 @@ class Article {
 
 /* START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable. 
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(function (article) {
+  return new Article(article)
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,11 @@
 
-const toggleMenu = () => {
+const toggleMenu = (menu) => {
   // Toggle the "menu--open" class on your menu refence. 
   this.menu.classList.toggle('.menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = this.menuButton  .querySelector('.menu');
+const menu = this.menuButton.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = this.menuButton.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
