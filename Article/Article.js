5eb00b49d84adbdb7978b152412d5f643ab3994f
brettmadrid <brettmadrid9@gmail.com>
2018-11-06 14:12:32 -0800

    Completed building Article Class

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,28 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(domElementArticle) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElementArticle = domElementArticle;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElementArticle.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click',this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    
   }
 }
 
 // START HERE: Select all classes named ".article" and assign that value to the articles variable
-let articles;
+let articles = document.querySelectorAll('.articles .article');
 
 // Use .map() to iterate over the articles array and create a new instance of Article by passing in each article as a parameter to the constructor.
-// articles = Array.from(articles);
+articles = Array.from(articles).map(function(article) {
+  return new Article(article);
+});
 
