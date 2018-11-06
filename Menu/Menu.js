c77d99d905c533af00ea3b99c6c7d6e9e8ee4ae0
Kseniya Platonava <ksu2008@gmail.com>
2018-11-05 23:11:37 -0500

    First commit

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,6 +3,8 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
 }
 
+
+
 // Start Here: Create a reference to the ".menu" class
 const menu;
 // create a reference to the ".menu-button" class
