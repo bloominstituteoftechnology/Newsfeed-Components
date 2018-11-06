cbaffa821bc1227904819976fb746dd3dedb67ea
Paul DeYonghe <p.deyonghe@gmail.com>
2018-11-05 23:49:19 -0600

    sixth commit: Menu.js is done, now working on Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,14 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+   
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
 
- toggleMenu = (menu) => {
+ toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  this.menu.classList.toggle('.menu--open')
+  return menu.classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -9,5 +9,4 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-console.log(this.menuButton);
-this.menuButton.addEventListener('click', this.toggleMenu.bind(this));
+menuButton.addEventListener('click', toggleMenu);
