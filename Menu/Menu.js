77902f63c69da7f2cac617e37825aef41f47de17
brettmadrid <brettmadrid9@gmail.com>
2018-11-06 20:35:03 -0800

    added greensock functionality to menu
    Menu now slides in from the left when the button is clicked.  Next I need to
    add functionality to slide the menu back in.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,11 @@
+// hide the menu
+window.addEventListener('load', () => {
+  TweenMax.set('.menu', {x: -350});
+});
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  //menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -12,6 +16,7 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', function() {
-  TweenMax.to(".header h1", 2, {x:300, ease:Bounce.easeOut, yoyoEase:Power2.easeOut, yoyo:true, repeat:1, repeatDelay:0.1});
-  toggleMenu();
+  // TweenMax.to(".header h1", 2, {x:300, ease:Bounce.easeOut, yoyoEase:Power2.easeOut, yoyo:true, repeat:1, repeatDelay:0.1});
+  TweenMax.to('.menu', 2, {x: 1});
+  //toggleMenu();
 });
