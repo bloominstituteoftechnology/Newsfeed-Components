5fdf8ce5a1c57c75382d1d182a66f00b532175ce
jrizza88 <jetorres88@gmail.com>
2018-11-07 02:52:11 -0500

    fixed syntax issue with event listener of this.expandButton

--- Article/Article.js
+++ Article/Article.js
@@ -14,14 +14,15 @@ class Article {
   
     // Set a click handler on the expandButton reference, calling the 
     //expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle.bind(this));
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     }
-
+ 
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand 
     //or hide the article.
       this.domElement.classList.toggle('.article-open');
+      console.log("article expanded")
   }
 }
 
