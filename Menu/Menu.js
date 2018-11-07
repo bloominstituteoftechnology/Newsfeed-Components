fbe920c40535a85b06ce11e783225db24ddaeb55
Jonathan Dorety <kyullog@gmail.com>
2018-11-06 22:59:25 -0500

    Complete menu animations

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,33 @@
 const toggleMenu = () => {
+  event.stopPropagation();
+
   menu.classList.toggle('menu--open');
-  // Toggle the "menu--open" class on your menu refence.
+  menuButton.classList.toggle('button--open');
+  console.log(menu.classList);
+   // Toggle the "menu--open" class on your menu refence.
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+
+const menuLinks = document.querySelectorAll('.menu ul li')
+console.log(menuLinks);
+
+document.querySelector('body').addEventListener('click', function(event) {
+  console.log(event.target);
+  if (menu.classList.contains('menu--open')) {
+    menu.classList.toggle("menu--open");
+    menuButton.classList.toggle("button--open")
+  };
+})
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(item => item.addEventListener('click', function(event) {
+  event.stopPropagation();
+  if (event.target.style.color) event.target.removeAttribute('style');
+  else event.target.style.color = "red";
+}))
