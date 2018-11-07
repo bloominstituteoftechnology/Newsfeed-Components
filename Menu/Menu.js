5b003205cb316f6e6287f88130bc1d6478865b8e
brettmadrid <brettmadrid9@gmail.com>
2018-11-06 17:50:48 -0800

    added greensock animation to title header

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -12,5 +12,6 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', function() {
+  TweenMax.to(".header h1", 2, {x:300, ease:Bounce.easeOut, yoyoEase:Power2.easeOut, yoyo:true, repeat:1, repeatDelay:0.1});
   toggleMenu();
 });
