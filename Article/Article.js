27e12f4be7a1cfd6757a4e4a550fa3b3a9dddc99
James Wigdor <jpwigdor@gmail.com>
2018-11-06 23:10:45 -0500

    Add Article Functionality

--- Article/Article.js
+++ Article/Article.js
@@ -3,22 +3,26 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.article .expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'Expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
 /* START HERE: 
+
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
@@ -26,4 +30,4 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.articles .article').forEach(x => new Article(x));
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,11 +5,12 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('menu');
+const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button')
+const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => {
-  toggleMenu();
-})
+menuButton.addEventListener('click', toggleMenu);
+
 
