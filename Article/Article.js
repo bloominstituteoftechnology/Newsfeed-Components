3d6576390b1bf6b61aef4bc4bcae1678ebc532d0
dickwinters <coding1965@gmail.com>
2018-11-06 23:26:40 -0600

    completed the article expand and close animation with gsap

--- Article/Article.js
+++ Article/Article.js
@@ -2,6 +2,7 @@
 
 class Article {
   constructor(domElement) {
+    let counter = 1;
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
@@ -9,13 +10,22 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.expandButton.addEventListener('click', () => {
+      counter++;
+      if (counter % 2 == 0) {
+        TweenLite.fromTo(this.domElement, 0.5, {height: 50}, {height: 400});
+        this.expandButton.textContent = 'close';
+      } else {
+        TweenLite.fromTo(this.domElement, 0.5, {height: 400}, {height: 50});
+        this.expandButton.textContent = 'expand';
+      }
+    });
   }
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-  }
+  // expandArticle() {
+  //   // Using our reference to the domElement, toggle a class to expand or hide the article.
+  //   this.domElement.classList.toggle('article-open');
+  // }
 }
 
 //  START HERE:
