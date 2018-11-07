7734ae0354bf2adb6c7db152cde93769db9fec89
Leila <nb.leila10@gmail.com>
2018-11-06 21:47:54 -0600

    Complete Animation goal #2
    Animate the component so that it slides open and slides closed on each click
    Update the text in the expand button to read 'Click to Expand' or 'Click to Close' depending on the state of the article.

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,25 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to expand';
+
+    this.flag = true; //Flag to know if open or closed
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle(event));
+    
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.content = this.domElement.classList.toggle('article-open');
+    if(this.flag) {  // Open the article
+      this.expandButton.textContent = "Click to close";
+      TweenMax.to(this.domElement, 1, {height:400});
+      this.flag = false;
+    } else {  // close the article
+      this.expandButton.textContent = "Click to expend";
+      TweenMax.to(this.domElement, 1, {height:50});
+      this.flag = true;
+    }
   }
 }
 
