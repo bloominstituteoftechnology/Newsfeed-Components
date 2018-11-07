19c76631fc0b8250ea89e9200e987a91905901b2
Melvin Merlos <melvinamerlos@gmail.com>
2018-11-06 23:44:16 -0500

    update 1: part 1 in prog

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,7 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getSelection('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getSelection('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
