aea88321250bed55a40fb3dd9b851c98628cc299
Taylor Belk <belktaylor12@gmail.com>
2018-11-06 21:56:08 -0600

    finished editing the errors in article.js and menu.js

--- Article/Article.js
+++ Article/Article.js
@@ -2,19 +2,24 @@
 
 class Article {
   constructor(domElement) {
+
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand"
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.textContent = "expand";
+
+   // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle)
   }
 
   expandArticle() {
+    
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.expandButton.classList.toggle('active')
+    this.parentNode.classList.toggle('article-open');
   }
 }
 
@@ -26,7 +31,6 @@ class Article {
 
 */
 
-let articles;
-articles = document.querySelectorAll('.article').forEach(function (articles) {
+const articles = document.querySelectorAll('.article').forEach(function (articles) {
   return new Article (articles)
 });
