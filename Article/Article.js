8e07a96cf8b78e2c5afc7437a0832682742dd6f6
Andrew <andrewryanogle@gmail.com>
2018-11-06 18:24:18 -0800

    I am stuck

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,20 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement= domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelector('.expandButton');
+    this.expandButton = document.querySelector('.article .expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerHTML = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click',this.expandArticle());
+    this.expandButton.addEventListener('click', (event) => {
+      this.expandArticle();
+    });
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement,classList.toggle('.article-open');
+    this.domElement.classList.toggle('.article-open');
   }
 }
 
@@ -27,8 +29,8 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.article');
-articles.forEach((article,index)=>{
-
+articles.forEach((article, index) => {
+  console.log(article);
   return new Article(article);
 
 })
