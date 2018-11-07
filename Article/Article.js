5c9e49f9f778f0ba2a1ae9bd2337ce092004ed08
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-06 21:21:10 -0700

    completed stretch goals - animation goals 1 and 2

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,25 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle)
     // console.log(this.domElement.classList.toggle('article-open'))
+    // this.closeButton = this.domElement.querySelector('.closeButton');
+    // this.closeButton.addEventListener('click', this.closeArticle)
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.parentNode.classList.toggle('article-open');
+    if ($('.article').hasClass('article-open')) {
+      this.textContent = 'Click to Close';
+    } else {
+      this.textContent = 'Click to Expand';
+    }
   }
+  // closeArticle() {
+  //   this.parentNode.style.display = 'none';
+  // }
 }
 
 /* START HERE: 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -26,3 +26,4 @@ class Menu {
 }
 
 new Menu(menu, menuButton);
+
