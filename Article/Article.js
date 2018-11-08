e050ff5dc07cdc8fa5d4f68f95c48ace99420ca5
jrizza88 <jetorres88@gmail.com>
2018-11-07 20:22:34 -0500

    added Article.less file to the index.less file and successfully expanded all articles of the constructor

--- Article/Article.js
+++ Article/Article.js
@@ -4,25 +4,30 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
+    // console.log(domElement) shows all panels from the dom
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
         // was originally this.expandButton
-    this.expandButton = this.domElement.querySelector('.expandButton');
+    this.expandButton = this.domElement.querySelector('.article .expandButton');
+    // this grabs the expandButton!
+    console.log(this.expandButton)
     // Using your expandButton reference, update the text on your 
     //expandButton to say "expand"
     this.expandButton.textContent = 'expand';
   
     // Set a click handler on the expandButton reference, calling the 
     //expandArticle method.
+    // this needs to pass two event handlers (event and a callback function)
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     }
  
-
+// method
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand 
     //or hide the article.
-      this.domElement.classList.toggle('.article-open');
+      this.domElement.classList.toggle('article-open');
       console.log("article expanded")
+      // console.log(this.expandButton.nextSibling)
   }
 }
 
@@ -40,7 +45,7 @@ console.log(articles)
 
 //articles = Array.from(articles);
 
-articles = Array.from(articles).map((article) => {
+articles = Array.from(articles).map( article => {
   return new Article(article);
 });
 
