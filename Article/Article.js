4cdacbf258897500f91ff727c0c4d7406961509a
mustafa inaya <moose.inaya@icloud.com>
2018-11-08 14:46:11 -0800

    TOGGLE, Toggeled The Menu and The Articles.

--- Article/Article.js
+++ Article/Article.js
@@ -3,16 +3,21 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = "expand";
     
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    // this.expandButton.addEventListener('click',  this.expandArticle.bind(this));
+     
   }
 
   expandArticle() {
+    this.domElement.classList.toggle('article-open');
     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
   }
@@ -26,4 +31,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach( function(item)  {
+  new Article(item);
+});
