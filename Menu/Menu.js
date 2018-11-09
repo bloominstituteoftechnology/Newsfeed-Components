083b9ef8f1390c9ce6249aa01908594e73589911
karlie0 <kamabrapa88@gmail.com>
2018-11-08 21:21:47 -0500

    Corrected MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,14 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('.menu--open');
+  // menu[0].classList.toggle('menu--open');
+  menu.classList.toggle('menu--open')
   // menu.forEach(item => console.log(item.classList));
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+// const menu = document.querySelector('.menu'); 
+const menu = document.querySelector('.menu'); 
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
