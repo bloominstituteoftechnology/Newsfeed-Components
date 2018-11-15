8e4f6e0c997cc7badd24e46fc75cf1de2fce1b2a
Aquariax <43307745+Aquariax@users.noreply.github.com>
2018-11-15 00:21:08 -0500

    project complete/ Less files imported properly/ articles open

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement = domElement;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = "open";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click',() => {this.expandArticle()})
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open')
+    if(this.expandButton.innerText === "open"){
+      this.expandButton.innerText = "close"
+    } else {
+      this.expandButton.innerText = "open"
+    }
   }
 }
 
@@ -26,4 +31,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(article => {
+  new Article(article)
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,11 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  menu[0].classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getElementsByClassName('menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getElementsByClassName('menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton[0].addEventListener('click', toggleMenu);
