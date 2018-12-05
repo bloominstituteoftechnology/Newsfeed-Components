52c8affb521f04ba5a9b1d6ec521da1994e8b8c2
mcferrenm <mcferrenmax@gmail.com>
2018-12-05 18:48:07 -0500

    Remove article button added to Article Component

--- Article/Article.js
+++ Article/Article.js
@@ -4,6 +4,18 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
+    // Create new button and give it text and a class
+    this.readButton = document.createElement('span');
+    this.readButton.textContent = '[x]';
+    this.readButton.className = 'readButton';
+
+    // Append new button to domElement
+    this.domElement.appendChild(this.readButton);
+
+    // Add event listener to button and use removeArticle() in the call back
+    this.readButton.addEventListener('click', () => this.removeArticle());
+
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
@@ -14,22 +26,24 @@ class Article {
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     // this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     this.expandButton.addEventListener('click', () => this.toggleArticle());
-    this.expandButton.addEventListener('click', () => this.closeArticle());
-    
+
   }
 
   toggleArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-      if(!this.isExpanded){
-        this.domElement.classList.add('article-open');
-        this.expandButton.textContent = 'Click to Close';
-        this.isExpanded = true;
-      } else {
-        this.domElement.classList.remove('article-open');
-        this.expandButton.textContent = 'Click to Expand';
-        this.isExpanded = false;
-      }
+    if(!this.isExpanded){
+      this.domElement.classList.add('article-open');
+      this.expandButton.textContent = 'Click to Close';
+      this.isExpanded = true;
+    } else {
+      this.domElement.classList.remove('article-open');
+      this.expandButton.textContent = 'Click to Expand';
+      this.isExpanded = false;
     }
+  }
+  removeArticle() {
+    articleDiv.removeChild(this.domElement);
+  }
 }
 
 /* START HERE: 
@@ -40,7 +54,8 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
+const articleDiv = document.querySelector('.articles');
+const articles = document.querySelectorAll('.article');
 
 articles.forEach(article => {
   return new Article(article);
