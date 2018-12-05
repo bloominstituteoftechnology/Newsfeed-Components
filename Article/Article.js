1b28c6f5855fa15c0000fa9e6c7bd1d508a1673a
Nate Boyette <nateboyette@icloud.com>
2018-12-05 13:13:27 -0800

    Got article expand button working to display the article content.

--- Article/Article.js
+++ Article/Article.js
@@ -4,6 +4,7 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+    console.log(this.domElement);
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
@@ -12,11 +13,19 @@ class Article {
     })
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        this.expandArticle(e);
+      })
+    })
+    
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log('test worked');
+    this.domElement.classList.toggle('article-open');
 
   }
 }
