b0b52723caa9d7ce9e115e136f0720f527cd6099
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-05 13:55:59 -0800

    You can now hide articles with the hide button that was added to each one

--- Article/Article.js
+++ Article/Article.js
@@ -1,15 +1,19 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+//Store all articles in array when they are instantiated
+let articleObjects = [];
 class Article {
   constructor(domElement) {
+    articleObjects.push(this);
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    this.hideButton = this.domElement.querySelector('.hideButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.hideButton.addEventListener('click', () => this.hide());
   }
 
   expandArticle() {
@@ -27,6 +31,15 @@ class Article {
         this.domElement.classList.toggle('article-open');
       }, 200)
     }
+  }
+
+  hide() {
+    TweenMax.to(this.domElement, .5, {opacity: 0});
+    //setTimeout to allow animation to finish before removing element from display.
+    setTimeout(() => {
+      this.domElement.style.display = 'none';
+    }, 500);
+    
   }
 }
 
