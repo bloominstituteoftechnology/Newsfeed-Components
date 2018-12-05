0c42735a8a10d8ae6b77e80e76a8e2fb62a0107d
Imon Ovbude <imonovbude@gmail.com>
2018-12-05 13:40:54 -0800

    created reference for expandButton

--- Article/Article.js
+++ Article/Article.js
@@ -1,11 +1,11 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-	constructor(elem) {
+	constructor(domElement) {
 		// assign this.domElement to the passed in domElement
-		this.domElement = elem;
+		this.domElement = domElement;
 		// create a reference to the ".expandButton" class.
-		this.expandButton;
+		this.expandButton = document.querySelectorAll('.expandButton');
 		// Using your expandButton reference, update the text on your expandButton to say "expand"
 
 		// Set a click handler on the expandButton reference, calling the expandArticle method.
