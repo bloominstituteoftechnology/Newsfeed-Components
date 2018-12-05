767d63dd6bc3162b0abf555a53d081290e76cb0d
Scott Vojik <scott.vojik@gmail.com>
2018-12-05 12:48:47 -0800

    Part 2, articles component compolete

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,20 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +26,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => {
+  return new Article (article);
+});
