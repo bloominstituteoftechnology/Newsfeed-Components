9361e6ff5fd5bfdd82fa266e3d2648ea0324a8c9
kjohna <43828763+kjohna@users.noreply.github.com>
2018-12-05 15:38:34 -0700

    Stretch: bonus goal complete, menu close on click anywhere but menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,11 @@
 
-const toggleMenu = () => {
+const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence.
   // console.log("here");
+  e.stopPropagation();
   if(menu.classList.contains("menu--open")) {
     console.log("start wait")
-    let slideTimeout = setTimeout(function() {
-      slideTimeout = null;
+    setTimeout(function() {
       menu.classList.toggle("menu--open");
       console.log("wait done")
      // wait for animation to stop to toggle off display: block
@@ -27,15 +27,27 @@ const toggleMenu = () => {
       opacity:1
     });
   }
-  
+}
+
+const closeMenu = (e) => {
+
+  if(menu.classList.contains("menu--open")) {
+    toggleMenu(e);
+  }
+}
+
+const catchMenuClick = (e) => {
+  // console.log("here");
+  e.stopPropagation();
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
-console.log(menu);
+// console.log(menu);
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", () => toggleMenu());
-
-console.log(menuButton);
+menuButton.addEventListener("click", (e) => toggleMenu(e));
+// Add listeners to close menu when click on anywhere but menu
+window.addEventListener("click", (e) => closeMenu(e)); // only passing event to closeMenu since closeMenu calls toggleMenu for sake of DRYness
+menu.addEventListener("click", (e) => catchMenuClick(e));
