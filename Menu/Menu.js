6586548a9923936ed1a65aa07f99e36c7a55b8e3
leiannel <leilouis112@gmail.com>
2018-12-05 17:25:27 -0500

    added gsap

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,13 @@
 const toggleMenu = () => {
   menu.classList.toggle('menu--open')
 }
+const menuAmimate = () => {
+  if(menu.classList.contains('menu--open')){
+    TweenMax.to('.menu-button', 1, {scaleX: 1.5})
+  } else {
+    TweenMax.to('.menu-button', 1, {scaleX: 1})
+  }
+}
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
@@ -9,6 +16,5 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 
-menuButton.addEventListener('click', (e) => toggleMenu(e, menu))
-
+menuButton.addEventListener('click', () => {toggleMenu(); menuAmimate();})
 // Using your menuButton reference, add a click handler that calls toggleMenu
