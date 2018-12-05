505c85ad54fbb8a7883704022d52d3c4671a4711
Daniel Weinman <namniewd@gmail.com>
2018-12-05 16:49:11 -0500

    menu closes from click anywhere on screen

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,6 @@
 
 let isMenuOpen = false;
+const html = document.querySelector('html');
 
 const openMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
@@ -17,10 +18,18 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', function() {
+menuButton.addEventListener('click', function(e) {
+  e.stopPropagation();
   if (!isMenuOpen) {
     openMenu();
   } else {
     closeMenu();
   }
 });
+
+window.addEventListener('click', function() {
+  if(isMenuOpen) {
+    closeMenu();
+  }
+})
+
