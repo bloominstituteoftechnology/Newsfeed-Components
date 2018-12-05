977cfe2dd19b7b2130a3e24d4d6cdd255d34edef
Matt Basile <matthewbasile2@gmail.com>
2018-12-05 18:33:28 -0500

    Added smooth opening for expand Article, just need to close it! LINES: Articles.js 18-19.

--- Article/Article.js
+++ Article/Article.js
@@ -15,8 +15,9 @@ class Article {
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.article.classList.toggle("article-open");
     
+    // this.article.classList.toggle("article-open");
+    TweenMax.to(this.article, 2, {className: "+=article-open" });
   }
 
 }
