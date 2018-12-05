60e3a0637fe93978e2a3c4af5e729e2963ebd3e6
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-12-05 14:44:34 -0600

    imported the article.less file to the index.less file. Completed adding the expanding and closing function for the articles element

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-open");
   }
 }
 
@@ -26,4 +26,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article").forEach(article => {
+  return new Article(article);
+});
