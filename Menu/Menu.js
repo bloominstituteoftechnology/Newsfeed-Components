20216c840004def6b475efdace442b5c37e8fc9f
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2018-12-05 18:18:44 -0500

    Stretch: basic animation using GSAP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
 
-  menuButton.classList.toggle('menu--open');
+  menuButton.classList.toggle('menu-open');
 }
 
 
@@ -19,6 +19,8 @@ const toggleMenu = () => {
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button')
-menuButton.addEventListener('click', () => toggleMenu());
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+
+
