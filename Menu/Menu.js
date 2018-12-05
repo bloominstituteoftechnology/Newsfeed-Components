b83f001f5ea7916dfff54923dce31d503f81a5c2
Jaouad Sarouti <jadsarouti@outlook.com>
2018-12-05 16:51:01 -0500

    slide animation of the list left to right/Stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,16 +1,31 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  menu[0].classList.toggle("menu--open");
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+//   menu[0].classList.toggle("menu--open");
 
-}
+// }
 
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll(".menu");
-console.log(menu);
-console.log(menu[0]);
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
-console.log(menuButton);
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click",toggleMenu)
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelectorAll(".menu");
+// console.log(menu);
+// console.log(menu[0]);
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector(".menu-button");
+// console.log(menuButton);
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener("click",toggleMenu)
+
+
+// this is to slide the menu using jQuery
+let  duration = 'slow';
+
+$('.menu-button').click(function () {
+  console.log("Iam being clicked");
+    $('.menu-button').hide(duration, function() {
+        $('.menu').show('slide', {direction: 'left'}, duration);});
+});
+
+$('.menu').click(function () {
+  $('.menu').hide('slide', {direction: 'left'});
+  $(".menu-button").show("slide",{direction:"left"},duration);
+});
