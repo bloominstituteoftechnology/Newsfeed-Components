a18a0fb6a3f3f77411165a4a2481674dfd0fc118
Imon Ovbude <imonovbude@gmail.com>
2018-12-05 13:37:01 -0800

    created a reference for article, and used forEach method to iterate through the node list of the variable articles

--- Article/Article.js
+++ Article/Article.js
@@ -1,21 +1,19 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+	constructor(elem) {
+		// assign this.domElement to the passed in domElement
+		this.domElement = elem;
+		// create a reference to the ".expandButton" class.
+		this.expandButton;
+		// Using your expandButton reference, update the text on your expandButton to say "expand"
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+		// Set a click handler on the expandButton reference, calling the expandArticle method.
+	}
 
-  }
+	expandArticle() {
+		// Using our reference to the domElement, toggle a class to expand or hide the article.
+	}
 }
 
 /* START HERE: 
@@ -26,4 +24,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach((elem) => {
+	return new Article(elem);
+});
