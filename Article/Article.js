f2ec81b33f1689b5692ac9360bbe94081bd5e240
Scott Vojik <scott.vojik@gmail.com>
2018-12-05 13:26:04 -0800

    added a close button which makes the article disappear when clicked

--- Article/Article.js
+++ Article/Article.js
@@ -6,15 +6,22 @@ class Article {
     this.article = article;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.article.querySelector('.expandButton');
+    this.closeButton = this.article.querySelector('.closeButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
+    this.closeButton.textContent = "close";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.closeButton.addEventListener('click', () => this.byeArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.article.classList.toggle('article-open');
+  }
+
+  byeArticle () {
+    this.article.classList.toggle('read-article');
   }
 }
 
