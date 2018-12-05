e8496ae9c93c5948a7901d4bc41ecff2a2b1fa58
McNultyPT <mcnultypt@gmail.com>
2018-12-05 12:56:58 -0800

    Article.js complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -15,3 +15,4 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', () => toggleMenu());
+
