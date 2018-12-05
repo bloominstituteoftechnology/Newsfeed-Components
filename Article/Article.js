39bd99b4a4ddcdcaea3151281f2d821ae6b39e1b
Imon Ovbude <imonovbude@gmail.com>
2018-12-05 15:38:36 -0800

    expandButton text changes when button is pressed

--- Article/Article.js
+++ Article/Article.js
@@ -18,15 +18,12 @@ class Article {
 	expandArticle(e) {
 		// Using our reference to the domElement, toggle a class to expand or hide the article.
 		console.log('clicked');
-		let paragraph = this.domElement.querySelector('p');
-		console.log(paragraph);
 		if (!this.domElement.classList.contains('article-open')) {
 			this.domElement.classList.toggle('article-open');
-			paragraph.style.display = 'inherit';
+			this.expandButton.textContent = 'Click to Close';
 		} else {
 			this.domElement.classList.toggle('article-open');
-
-			paragraph.style.display = 'none';
+			this.expandButton.textContent = 'Click to Expand';
 		}
 	}
 }
