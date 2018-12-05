b520115d26539e576804330ec5bf3fdcec179426
mcferrenm <mcferrenmax@gmail.com>
2018-12-05 18:18:01 -0500

    Expand button textContent dynamic in Article Component

--- Article/Article.js
+++ Article/Article.js
@@ -7,19 +7,29 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
     
+    this.isExpanded = false;
+    this.expandButton.textContent = 'Click to Expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     // this.expandButton.addEventListener('click', this.expandArticle.bind(this));
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener('click', () => this.toggleArticle());
+    this.expandButton.addEventListener('click', () => this.closeArticle());
     
   }
 
-  expandArticle() {
+  toggleArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-
-  }
+      if(!this.isExpanded){
+        this.domElement.classList.add('article-open');
+        this.expandButton.textContent = 'Click to Close';
+        this.isExpanded = true;
+      } else {
+        this.domElement.classList.remove('article-open');
+        this.expandButton.textContent = 'Click to Expand';
+        this.isExpanded = false;
+      }
+    }
 }
 
 /* START HERE: 
