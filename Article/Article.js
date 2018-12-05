3c51fd7937c3e8b27fd72156878833c12c7b7b33
stellarluminant <stellarluminant@gmail.com>
2018-12-05 11:03:53 -0800

    Add article expand functionality
    Styles are added to index.less
    Click the 'expand' text at the bottom of each article to expand it.

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,26 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', _ => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
-/* START HERE: 
+// Select all nodes with class 'article' and assigning the NodeList to articles variable.
+let articles = document.querySelectorAll('.article');
 
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
-
-let articles;
+// Iterate through articles and create a new Article object using the dom objects in artcles.
+articles.forEach(article => new Article(article));
