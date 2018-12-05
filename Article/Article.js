e278b8d7050f94034102860de5b85f4cc4fe62d5
Kingaa6 <Kingaa6@gmail.com>
2018-12-05 13:59:49 -0800

    Refactored news content to remove fixed height styling

--- Article/Article.js
+++ Article/Article.js
@@ -6,6 +6,7 @@ class Article {
     this.article = article;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.article.querySelector('.expandButton');
+    this.content = this.article.querySelector('.news-content');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -14,7 +15,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.article.classList.toggle('article-open');
+    this.content.classList.toggle('news-content');
   }
 }
 
