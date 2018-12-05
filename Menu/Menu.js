aae292c3a90d391ff5ee5810cf395998f997d0c8
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-05 13:07:09 -0800

    Menu now animates onto the screen and off the screen when activated/deactivated with the menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,18 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  if(!menu.classList.contains('menu--open')) {
+    menu.classList.toggle('menu--open');
+    TweenMax.to(menu, 0, {x: -500});
+    TweenMax.to(menu, .25, {x: 0});
+  } else {
+    TweenMax.to(menu, .25, {x: -500});
+    TweenMax.to(menu, 0, {x: 0});
+    //We setTimeout here to allow the animation of the menu going off screen before it's style is set to display: none
+    setTimeout(() =>{
+      menu.classList.toggle('menu--open');
+    }, 250);
+    
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
