3d9cb1e0b2d3c00024287e87f8379b1ca2e929e2
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-05 13:24:46 -0800

    Article expand button text changes depending on if they are open or closed

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
@@ -16,11 +16,13 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     if(!this.domElement.classList.contains('article-open')) {
       TweenMax.to(this.domElement, .20, {height: '400px'});
+      this.expandButton.textContent = 'Click to Close';
       setTimeout(() => {
         this.domElement.classList.toggle('article-open');
       }, 200)
     } else {
       TweenMax.to(this.domElement, .20, {height: '50px'});
+      this.expandButton.textContent = 'Click to Expand';
       setTimeout(() => {
         this.domElement.classList.toggle('article-open');
       }, 200)
