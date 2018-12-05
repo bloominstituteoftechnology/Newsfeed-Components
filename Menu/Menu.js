ccdb7f86cdb33a7729eb6bbd8044fb03cf78a293
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-05 18:42:02 -0500

    Stretch: Added slide back in window webpage for menu.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,20 @@
 
-const toggleMenu = () => {
+const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence.
+  e.stopPropagation();
   menu.classList.toggle('menu--open');
-    if (menu.classList.contains("menu--open")) {
-      TweenMax.to(menu, 1, {left: 0})
-    } else {
-      TweenMax.to(menu, 1, {left: -350})
-    }
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(menu, 1, {left: 0})
+  } else {
+    TweenMax.to(menu, 1, {left: -350})
+  };
+}
+
+const slideBack = () => {
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(menu, 1, {left: -350})
+    menu.classList.toggle('menu--open');
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -15,3 +23,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+window.addEventListener('click', slideBack);
