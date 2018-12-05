323dcf732bace8688375a0317972a60ae4ab9497
Kingaa6 <Kingaa6@gmail.com>
2018-12-05 14:30:36 -0800

    Minor Article.js refactor

--- Article/Article.js
+++ Article/Article.js
@@ -2,14 +2,12 @@
 
 class Article {
   constructor(article) {
-    // assign this.domElement to the passed in domElement
     this.article = article;
-    // create a reference to the ".expandButton" class. 
     this.expandButton = this.article.querySelector('.expandButton');
     this.content = this.article.querySelector('.news-content');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+
     this.expandButton.textContent = 'expand';
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
@@ -19,45 +17,8 @@ class Article {
   }
 }
 
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
-
 let articles = document.querySelectorAll('.article');
 
 articles.forEach(article => {
   return new Article(article);
 })
-
-
-
-
-/*
-// class takes in parameter of static html panels, allows `this` keyword
-class Panel {
-  constructor(panel){
-    this.panel = panel; // `this` binds panel x to block
-    console.log(this.panel); // returns full panel as DOM element
-    this.heading = this.panel.querySelector('panel-content h3');
-    this.content = this.panel.querySelector('panel-content p');
-    console.log(this.heading); // returns each H3 from panels
-
-    // non-arrow function binding for method access
-    this.heading.addEventListener('click', this.togglePanel.bind(this)); // use this keyword to access object methods
-
-    // arrow function binding for method access
-    this.heading.addEventListener('click', () => this.togglePanel()); // arrow function implicitly binds `this` to method!!!
-
-  }
-  // methods
-  togglePanel(){
-    this.content.classList.toggle('toggle-on'); // can't access object scope without binding (see above)
-  }
-}
-
-
-*/
