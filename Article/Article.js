d860d300b06194a3438f7f6d64197dffc535c549
Mike Landers <landers.mike@gmail.com>
2018-12-05 15:16:16 -0800

    * side menu slide in and out using GreenSock

--- Article/Article.js
+++ Article/Article.js
@@ -9,17 +9,24 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-
     this.expandButton.addEventListener('click', () => this.expandArticle());    
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    // console.log(this.domElement)
-    articles.forEach((e)=>{
-      e.classList.remove('article-open')
-    })
-    this.domElement.classList.toggle('article-open')
+  
+
+    if(this.domElement.classList.contains('article-open')){
+      articles.forEach((e)=>{
+        e.classList.remove('article-open');
+      });  
+    }else{
+      articles.forEach((e)=>{
+        e.classList.remove('article-open');
+      });  
+      this.domElement.classList.add('article-open');
+
+    }
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,20 @@
 
-const toggleMenu = () => {
+const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open')
+  e.stopPropagation();
+  menu.classList.toggle("menu--open");
+  if (menu.classList.contains("menu--open")) {
+    TweenMax.to(menu, 0.5, {
+      left: 0,
+      ease: Power4.easeOut
+    });
+   } else {
+    TweenMax.to(menu, 3, {
+      left: -350,
+      ease: Power4.easeOut
+    })
+  }
+
 }
 
 // // Start Here: Create a reference to the ".menu" class
