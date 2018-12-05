0ddb7203e51268243e1bbe43269be2f0bd5b21c3
joshua-disney <joshua.a.disney@gmail.com>
2018-12-05 15:59:05 -0700

    Built article class constructor and toggle.

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,21 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
+
 
 /* START HERE: 
 
@@ -28,7 +29,6 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.article');
-articles.forEach(function(element) {
-  return new Article;
-  console.log(element);
+articles.forEach(domElement => {
+  return new Article(domElement);
 });
