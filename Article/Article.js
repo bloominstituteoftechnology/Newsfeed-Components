41cffeb8a8de03807ba74cfa0d46d11691ef36ec
matt0418 <mfeldma7@gmail.com>
2018-12-05 18:20:26 -0500

    Finished the sliding menu animation

--- Article/Article.js
+++ Article/Article.js
@@ -16,6 +16,7 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
   }
+
 }
 
 /* START HERE: 
@@ -26,7 +27,26 @@ class Article {
 
 */
 
+
+
 let articles = document.querySelectorAll('.article');
-articles.forEach( (article) => {
+articles.forEach( article => {
   return new Article(article);
 });
+
+
+
+// const toggleMenu = (e) => {
+//   // Toggle the "menu--open" class on your menu refence. 
+//   e.stopPropagation()
+//   if(menu.classList.contains('menu--open')){
+//     setTimeout(function(){
+//       menu.classList.toggle('menu--open');
+//     }, 150);
+//     TweenMax.fromTo(menu, .3, {x:0}, {x:-350});
+//   } else {
+//     menu.classList.toggle('menu--open');
+//     TweenMax.fromTo(menu, .3, {x:-350}, {x:0});
+//   }
+     
+// }
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,37 @@
 
-const toggleMenu = () => {
+const toggleMenu = (e) => {
   // Toggle the "menu--open" class on your menu refence. 
-  console.log("it fired")
-  menu.classList.toggle('menu--open');
+  e.stopPropagation()
+  if(menu.classList.contains('menu--open')){
+    setTimeout(function(){
+      menu.classList.toggle('menu--open');
+    }, 150);
+    TweenMax.fromTo(menu, .3, {x:0}, {x:-350});
+  } else {
+    menu.classList.toggle('menu--open');
+    TweenMax.fromTo(menu, .3, {x:-350}, {x:0});
+  }
+     
 }
+
+const closeMenu = (e) => {
+  if(menu.classList.contains('menu--open')) {
+    toggleMenu(e)
+  }
+}
+
+const openMenuClick = (e) => {
+  e.stopPropagation();
+}
+
+// const myBody = document.querySelector('body');
+// myBody.addEventListener('click', () => closeMenu());
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => toggleMenu());
+menuButton.addEventListener('click', (e) => toggleMenu(e));
+window.addEventListener('click', (e) => closeMenu(e));
+menu.addEventListener('click', (e) => openMenuClick(e));
