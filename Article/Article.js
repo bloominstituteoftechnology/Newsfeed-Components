0d6b01c11229d5fa33596880d5a25ee21772a7d1
rosilioroman <rosilioroman@gmail.com>
2018-12-05 14:13:26 -0700

    **All commits going forward are stretch goals** Added changes to the expand/collapse button for articles

--- Article/Article.js
+++ Article/Article.js
@@ -9,10 +9,17 @@ class Article {
     this.expandButton = domElement.querySelector(".expandButton");
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
-    
+    this.expandButton.textContent = "Click to Expand";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", () => this.expandArticle());
+    this.expandButton.addEventListener("click", () => {
+      if(this.expandButton.textContent === "Click to Expand") {
+        this.expandButton.textContent = "Click to Collapse";
+      } else {
+        this.expandButton.textContent = "Click to Expand";
+      }
+      return this.expandArticle();
+    });
   }
 
   expandArticle() {
