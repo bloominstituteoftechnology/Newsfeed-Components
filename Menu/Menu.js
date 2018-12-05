784ed61fa864b3a57b0e0b46a59d984e57ed66b8
Lenna <lenna@lennamau.com>
2018-12-05 15:26:19 -0700

    Newsfeed
    task 3 completed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,7 +3,6 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
 }
-
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
