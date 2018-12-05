851175e1efefabf74a2ac09e06ea3b00b23ebae9
bhumip214 <bhumip214@gmail.com>
2018-12-05 18:57:46 -0500

    Updated text in the expand button

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
@@ -15,6 +15,13 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+
+    //Changes open and close text
+    if (this.domElement.classList.contains("article-open")) {
+      this.expandButton.textContent = "Click to Close";
+    } else {
+      this.expandButton.textContent = "Click to Expand";
+    }
   }
 }
 
