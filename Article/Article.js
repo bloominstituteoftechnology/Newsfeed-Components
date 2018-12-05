12911165062a36bd00028aa5d3c4b815a7e8cb55
Lenna <lenna@lennamau.com>
2018-12-05 14:49:06 -0700

    Newsfeed-Components
    part 2

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,28 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.domElement = domElement;
     
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    // create a reference to the ".expandButton" class. 
+    this.expandButton = this.domElement.querySelector('.expandButton');
+    // Using your expandButton reference, update the text on your 
+    //expandButton to say "expand"
+    this.expandButton.textContent = 'Expand';
+    
+    // Set a click handler on the expandButton reference, calling the 
+    //expandArticle method.
+    this.expandButton.addEventListener('click', () => this.expandArticle());
     
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    if (this.expandButton.textContent =="Expand") {
+      this.expandButton.textContent = "Close";
 
+    } else {
+      this.expandButton.textContent = "Expand";
+    }
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -22,8 +32,17 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+- With your selection in place, now chain .forEach() on to the articles variable to 
+iterate over the articles NodeList and create a new instance of Article by passing 
+in each article as a parameter to the constructor.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(article => {
+  new Article(article)
+});
+
+/*articles.forEach((domElement) => {
+  let article = new Article(domElement);
+  console.log(domElement);
+});*/
