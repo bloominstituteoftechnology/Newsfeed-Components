a14f7d1a1b696f32049f6c9e74be51f358bb18df
Jeff <jeff@Jeffs-MacBook-Pro.local>
2018-12-05 17:12:17 -0500

    Added articles constructor function and articles object q

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', event => this.expandArticle(event));
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -27,3 +27,5 @@ class Article {
 */
 
 let articles;
+articles = document.querySelectorAll(".article");
+articles.forEach((article) => new Article(article));
