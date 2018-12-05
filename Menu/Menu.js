5d66a9e07880bd49a24296f116b59fedd9227e48
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2018-12-05 16:30:59 -0500

    Newsfeed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,30 @@
 
+
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
 }
 
+
+
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+class MenuOpen{
+  constructor(menuList){
+    this.menuList = menuList;
+    this.menuItems = this.menuList.querySelector('.menu')
+  }
+}
+this.menuItems.addEventListener('click', this.toggleMenuOpen.bind(this));
+// const menu = doc
+
+toggleMenu() {
+  console.log('is this working?');
+  this.menuItems.vlassList>toggleMenu('toggle-on');
+}
+
+
+
 // create a reference to the ".menu-button" class
 const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
