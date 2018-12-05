0b76437303ee427cda5f721921599057793b8fb0
spinon187 <spinon187@gmail.com>
2018-12-05 15:19:45 -0600

    Components I Part 2

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,19 @@
-
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+class moveMenu {
+  constructor(moveMenu){
+    this.moveMenu = moveMenu;
+    this.all = this.moveMenu.querySelector('.menu');
+    
+  }
 }
 
+console.log(moveMenu.all);
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+
+// }
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
+// const menu;
+// // create a reference to the ".menu-button" class
+// const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
