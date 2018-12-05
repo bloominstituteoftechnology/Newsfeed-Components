b8ca0e4f97ae324031067d6050a2e932964e96c0
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-12-05 16:13:33 -0500

    Cleaned up .js files and added more articles to HTML

--- Article/Article.js
+++ Article/Article.js
@@ -2,39 +2,19 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
-    
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-
     this.expandButton.textContent = "expand";
     this.expandButton.addEventListener("click", () => this.expandArticle());
-    
-    // this.expandButton.forEach(button => {
-    //   button.textContent = "expand";
-    //   button.addEventListener("click", () => this.expandArticle());
-    // })
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+
     
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-console.log(this.domElement.classList);
-this.domElement.classList.toggle("article-open");
+    this.domElement.classList.toggle("article-open");
     
   }
 }
-
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
 
 let articles = document.querySelectorAll(".article");
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,10 @@
 
 const toggleMenu = () => {
- // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle("menu--open");
 }
 
-// Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 
-// create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 
 menuButton.addEventListener('click', toggleMenu);
