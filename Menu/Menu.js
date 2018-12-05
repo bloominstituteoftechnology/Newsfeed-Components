ccf40103ea01270abb7d1549cfa41e08d1544fd5
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2018-12-05 16:53:02 -0500

    refactored menu.js, now working on article.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -18,13 +18,13 @@ class MenuOpen{
 this.menuItems.addEventListener('click', this.toggleMenuOpen.bind(this));
 // const menu = doc
 
-toggleMenu() {
-  console.log('is this working?');
-  this.menuItems.vlassList>toggleMenu('toggle-on');
-}
+// toggleMenu() {
+//   console.log('is this working?');
+//   this.menuItems.classList>toggleMenu('toggle-on');
+// }
 
 
 
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
