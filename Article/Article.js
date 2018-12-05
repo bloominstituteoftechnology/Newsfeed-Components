c22a6afc67172e6fb1d43f839a6a4caeb15187cb
Jacob Bryan <jtb36688@gmail.com>
2018-12-05 18:12:00 -0500

    main goals done

--- Article/Article.js
+++ Article/Article.js
@@ -4,7 +4,7 @@ class Article {
   constructor(domElement) {
     this.domElement = domElement;
     this.Button = this.domElement.querySelector('.expandButton'); // <------------- Fix this reference to the expandButton class
-  
+    this.pElements = this.domElement.querySelectorAll('p');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.Button.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -12,6 +12,7 @@ class Article {
   }
 
   expandArticle(e) {
+    // this.pElements.forEach(element => element.classList.toggle('showhide'));
     this.domElement.classList.toggle('article-open');
     // Using our reference to the domElement, toggle a class to expand or hide the article.
   }
