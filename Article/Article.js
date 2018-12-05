cd5d64d978c7feba7f738d691615775c880c3225
Raine Wallace <rainebeaudubs@gmail.com>
2018-12-05 17:40:37 -0500

    Got the Article class and structure finished, onto making new articles.

--- Article/Article.js
+++ Article/Article.js
@@ -2,19 +2,15 @@
 
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.domElement = domElement;
+    this.expandButton = 
+    this.domElement.querySelector('.expandButton');
+    this.expandButton.textContent = "expand";
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
+   expandArticle() {
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-open");
   }
 }
 
@@ -26,4 +22,10 @@ class Article {
 
 */
 
-let articles;
+
+let articles =
+document.querySelectorAll('.article');
+
+articles.forEach(article => {
+  return new Article(article);
+});
