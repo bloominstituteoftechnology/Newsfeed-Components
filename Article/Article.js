a58311d028e4be8da476b91887dabf008d67bfb2
Brandon Lent <blent1050@yahoo.com>
2018-12-05 15:24:12 -0500

    article functionality completed

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,20 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
+    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener('click', () =>{
+      this.expandArticle();
+    });
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('.article-open');
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -27,7 +29,7 @@ class Article {
 
 */
 let articles = document.querySelectorAll('.article');
+
 articles.forEach(article => {
-  
   return new Article(article);
 });
