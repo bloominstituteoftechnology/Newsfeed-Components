77b033f276a0178bc0fc1944e3947bd250f1f45f
Daniel Weinman <namniewd@gmail.com>
2018-12-05 17:21:43 -0500

    article opening and closing is animated

--- Article/Article.js
+++ Article/Article.js
@@ -7,17 +7,31 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click To Expand';
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+
+
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    
+
+    
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    // this.domElement.classList.toggle('article-open');
+    TweenMax.to(this.domElement, 1, {height:400});
+    this.expandButton.textContent = 'Click To Close';
+    this.expandButton.addEventListener('click', () => this.closeArticle());
 
+  }
+
+  closeArticle() {
+    TweenMax.to(this.domElement, 1, {height:50});
+    this.expandButton.textContent = 'Click To Expand';
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 }
 
@@ -30,4 +44,5 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.article');
-articles.forEach( article => new Article(article) )
+articles.forEach( article => new Article(article) );
+
