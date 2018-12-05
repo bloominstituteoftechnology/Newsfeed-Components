624aa7c1c9a015d649374231da0eb9859b274e46
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-05 18:24:05 -0500

    Stretch: Added sline in and out menu on click.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,11 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
+    if (menu.classList.contains("menu--open")) {
+      TweenMax.to(menu, 1, {left: 0})
+    } else {
+      TweenMax.to(menu, 1, {left: -350})
+    }
 }
 
 // Start Here: Create a reference to the ".menu" class
