ded85a9a2c1a3a948a3b8bb85d231db772b5d699
Nathan Thomas <nwthomas@me.com>
2018-12-05 13:44:58 -0800

    Add other articles close on each open

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,5 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+let allPanels = [];
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -13,11 +13,25 @@ class Article {
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
+
+    // Pushes to allPenels array for alter use
+    allPanels.push(this);
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open");
+    if (this.domElement.classList.contains("article-open")) {
+      this.domElement.classList.toggle("article-open");
+    } else {
+      allPanels.forEach(article => {
+        article.closeArticles();
+      });
+      this.domElement.classList.toggle("article-open");
+    }
+  }
+
+  closeArticles() {
+    this.domElement.classList.remove("article-open");
   }
 }
 
