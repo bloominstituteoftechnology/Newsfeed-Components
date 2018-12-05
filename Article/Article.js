69b81d7db688495da4377be95795c8354998ec8b
Samsoucy <newf930@outlook.com>
2018-12-05 17:50:23 -0500

    finished part 3

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,35 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent ="expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open')
   }
 }
 
 /* START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable.
+    
 
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
 
-let articles;
+const articles = document.querySelectorAll(".article");
+
+articles.forEach(articles => {
+  return new Article(articles);
+});
+
+
+// let articles;
