f6e941f110f6d0608e8bfe0c468fe31ff1665a26
bhumip214 <bhumip214@gmail.com>
2018-12-05 18:03:08 -0500

    Animation goal #1 stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,9 @@
-const toggleMenu = () => {
+const toggleMenu = event => {
   // Toggle the "menu--open" class on your menu refence.
   console.log("clicked on menu");
+
+  // stopping propagation to document so menu bar can open when clicked on menu
+  event.stopPropagation();
   menu.classList.toggle("menu--open");
 };
 
@@ -12,3 +15,16 @@ const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
+
+//////// For Stretch Task
+// Add event listener on document for allowing menu bar to close
+document.addEventListener("click", event => {
+  menu.classList.remove("menu--open");
+  console.log("Document is clicked");
+});
+
+menu.addEventListener("click", event => {
+  // stopping propagation to document so that menu doesnt close when user clicks on it
+  event.stopPropagation();
+  console.log("menu list clicked");
+});
