777f3b298ff33a106a3ac5f13551eaa1e09cee19
Nathan Thomas <nwthomas@me.com>
2018-12-05 13:32:06 -0800

    Create menu slid-in css animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,9 +1,13 @@
 const toggleMenu = () => {
   menu.classList.toggle("menu--open");
 };
+const toggleLinks = () => {
+  menuItems.classList.toggle("menu__list--show");
+};
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+const menuItems = document.querySelector("ul");
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
@@ -11,4 +15,5 @@ const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", _ => {
   toggleMenu();
+  toggleLinks();
 });
