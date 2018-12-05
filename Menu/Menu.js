40530e57b314c5d11bcc20d6c01cd6639b65cd08
linkel <ironcarbide@gmail.com>
2018-12-05 10:24:31 -0800

    Stretch goal animation #1: Menu comes out from the left.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,9 +4,20 @@ const toggleMenu = () => {
   menu.classList.toggle("menu--open");
 }
 
+const offMenu = () => {
+  if (menu.classList.contains("menu--open")) {
+    menu.classList.remove("menu--open");
+  }
+}
+
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu, false);
+menuButton.addEventListener("click", toggleMenu);
+
+const articles = document.querySelector(".articles");
+
+// if an area in the articles is clicked while the menu is open, close the menu
+articles.addEventListener("click", offMenu);
