328e707dbdb9ffff418d0d9718ebe0abff81b9ad
rosilioroman <rosilioroman@gmail.com>
2018-12-05 13:57:33 -0700

    Added two new articles to index.html

--- Article/Article.js
+++ Article/Article.js
@@ -4,10 +4,13 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector(".expandButton");
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
+    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
