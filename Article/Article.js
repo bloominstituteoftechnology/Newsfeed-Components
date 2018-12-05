bc93b431413cf027a7e3010e0de922b6533c1ded
Nathan Thomas <nwthomas@me.com>
2018-12-05 14:34:56 -0800

    Implement delete article button with GSAP fade

--- Article/Article.js
+++ Article/Article.js
@@ -2,24 +2,18 @@
 let allPanels = [];
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-
-    // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
-
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.closeButton = this.domElement.querySelector(".close");
     this.expandButton.textContent = "expand";
-
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
 
-    // Pushes to allPenels array for alter use
+    this.closeButton.addEventListener("click", () => this.deleteArticle());
+
     allPanels.push(this);
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
     if (this.domElement.classList.contains("article-open")) {
       this.domElement.classList.toggle("article-open");
     } else {
@@ -32,6 +26,10 @@ class Article {
 
   closeArticles() {
     this.domElement.classList.remove("article-open");
+  }
+
+  deleteArticle() {
+    TweenMax.to(this.domElement, 2, { opacity: 0, display: "none" });
   }
 }
 
