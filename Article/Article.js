5aa0a0c200f83cc16ab55e50c69c8fd99e4c1f3d
bhumip214 <bhumip214@gmail.com>
2018-12-05 16:48:15 -0500

    Created forEach for articles

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,15 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
   }
 }
 
@@ -26,4 +24,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article");
+
+articles.forEach(article => {
+  return new Article(article);
+});
