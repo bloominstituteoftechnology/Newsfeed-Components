619673229c6a07cdbd6b0323872931e457d48ab9
atbenedict <atbenedict@gmail.com>
2018-12-05 18:44:29 -0500

    working on submission form

--- Article/Article.js
+++ Article/Article.js
@@ -4,11 +4,11 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    console.log(this.domElement);
+
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "click to expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
@@ -17,10 +17,10 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
 
-    if (this.expandButton.textContent === "expand") {
-      this.expandButton.textContent = "close";
+    if (this.expandButton.textContent === "click to expand") {
+      this.expandButton.textContent = "click to close";
     } else {
-      this.expandButton.textContent = "expand";
+      this.expandButton.textContent = "click to expand";
     }
   }
 }
---
+++ composer.js
@@ -0,0 +1,13 @@
+// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+
+/* START HERE: 
+
+- Select all classes named ".article" and assign that value to the articles variable.  
+
+- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+
+*/
+
+function submitStory() {
+  console.log(document.getElementById("newStory").submit());
+}
