a98d2ba8ab5e904db3f335a5c3be3748b39c555d
Jaouad Sarouti <jadsarouti@outlook.com>
2018-12-05 15:50:25 -0500

    MVP finished

--- Article/Article.js
+++ Article/Article.js
@@ -8,15 +8,13 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click",() => {this.expandArticle()})
+    this.expandButton.addEventListener("click", () => { this.expandArticle() })
   }
-   expandArticle() {
+  expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-     this.domElement.classList.toggle("article-open")
+    this.domElement.classList.toggle("article-open")
   }
 }
-
-
 /* START HERE: 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
