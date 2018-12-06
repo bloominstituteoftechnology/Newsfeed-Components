2bad5b87d605304bf636dc5570706b0f8c8fc98b
Jeff <jeff@Jeffs-MacBook-Pro.local>
2018-12-05 19:06:00 -0500

    added jquery

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open')
+  //menu.classList.toggle('menu--open')
+  $('.menu--open').slideToggle();
 }
 
 // Start Here: Create a reference to the ".menu" class
