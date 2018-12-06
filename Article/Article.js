75548f35de9a9e02ead99c90de32fd45b9b5a1f6
EJaona <ejaona@gmail.com>
2018-12-06 06:12:31 -0600

    added refrence to the menu button, and added classList toggle function in menu.js

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,18 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement;
-    // create a reference to the ".expandButton" class. 
+
+    // create a reference to the ".expandButton" class.
     this.expandButton;
+    
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+   
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    ;
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
   }
 }
 
@@ -26,4 +27,5 @@ class Article {
 
 */
 
-let articles;
+const articles;
+
