63dea528be4f8090bb007b0629b4d53a82345ead
Jeff <jeff@Jeffs-MacBook-Pro.local>
2018-12-06 09:59:29 -0500

    added animated toggle to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,16 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  //menu.classList.toggle('menu--open')
-  $('.menu--open').slideToggle();
-}
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence. 
+//   menu.classList.toggle('menu--open')
+// }
 
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu)
+// // Start Here: Create a reference to the ".menu" class
+// const menu = document.querySelector('.menu');
+// // create a reference to the ".menu-button" class
+// const menuButton = document.querySelector('.menu-button');
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener('click', toggleMenu);
+
+$('.menu-button').click(function (e) { 
+  $('.menu').toggle("slide");
+});
+
