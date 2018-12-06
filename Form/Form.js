f5708af6986611f98500d3461cf5e12e371be151
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-06 09:09:56 -0500

    fixed a bug

--- Form/Form.js
+++ Form/Form.js
@@ -28,30 +28,43 @@ class NewArticle {
   }
 
   appendArticle(article, articleSection) {
-    const articleDiv = document.createElement("div");
-    articleDiv.className = "article";
-    const closeBtn = document.createElement("button");
-    closeBtn.className = "close";
-    const x = document.createTextNode("X");
-    closeBtn.appendChild(x);
-    closeBtn.addEventListener("click", e => this.handleClose(e));
-    const h2Title = document.createElement("h2");
-    const h2TitleText = document.createTextNode(article.title);
-    h2Title.appendChild(h2TitleText);
-    const date = document.createElement("p");
-    date.className = "date";
-    const dateEntered = document.createTextNode(article.date);
-    date.appendChild(dateEntered);
-    const text = document.createElement("p");
-    const textEntered = document.createTextNode(article.text);
-    const span = document.createElement("span");
-    span.className = "expandButton";
-    const spanText = document.createTextNode("expand");
-    span.appendChild(spanText);
-    span.addEventListener("click", e => this.expandArticle(e));
-    articleDiv.append(closeBtn, h2Title, date, text, span);
-    text.appendChild(textEntered);
-    articleSection.appendChild(articleDiv);
+    let newElement = articleSection.querySelector(".article").cloneNode(true);
+
+    let newH2 = document.querySelector("#title").value;
+    let newContent = document.querySelector("#article").value;
+    newElement.querySelector("h2").textContent = newH2;
+    newElement.querySelector(".content").textContent = newContent;
+    newElement.querySelector(".date").textContent = document.querySelector(
+      "#date"
+    ).value;
+    newElement = new Article(newElement);
+    articleSection.appendChild(newElement.domElement);
+    document.querySelector("#title").value = "";
+    document.querySelector("#article").value = "";
+    // const articleDiv = document.createElement("div");
+    // articleDiv.className = "article";
+    // const closeBtn = document.createElement("button");
+    // closeBtn.className = "close";
+    // const x = document.createTextNode("X");
+    // closeBtn.appendChild(x);
+    // closeBtn.addEventListener("click", e => this.handleClose(e));
+    // const h2Title = document.createElement("h2");
+    // const h2TitleText = document.createTextNode(article.title);
+    // h2Title.appendChild(h2TitleText);
+    // const date = document.createElement("p");
+    // date.className = "date";
+    // const dateEntered = document.createTextNode(article.date);
+    // date.appendChild(dateEntered);
+    // const text = document.createElement("p");
+    // const textEntered = document.createTextNode(article.text);
+    // const span = document.createElement("span");
+    // span.className = "expandButton";
+    // const spanText = document.createTextNode("expand");
+    // span.appendChild(spanText);
+    // span.addEventListener("click", e => this.expandArticle(e));
+    // articleDiv.append(closeBtn, h2Title, date, text, span);
+    // text.appendChild(textEntered);
+    // articleSection.appendChild(articleDiv);
     //console.log(articleSection);
   }
   expandArticle(e) {
