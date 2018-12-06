1a33b4226b1e3591b032afab44ee01e554e060e6
BrannanC <brannanconrad@gmail.com>
2018-12-06 13:11:34 -0600

    Added expand functionality

--- Article/Article.js
+++ Article/Article.js
@@ -1,7 +1,7 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+    constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
@@ -14,21 +14,20 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-    if(this.expandButton.textContent === "Expand"){
+    let isArticleOpen = this.domElement.classList.contains('article-open');
+    document.querySelectorAll('.expandButton').forEach(x => x.textContent = 'Expand');
+    articles.forEach(x => x.classList.remove('article-open'));
+    
+    if(!isArticleOpen){
+      this.domElement.classList.toggle('article-open');
       this.expandButton.textContent = "^";
-    } else {
-      this.expandButton.textContent = "Expand";
     }
   }
 }
 
 /* START HERE: 
-
 - Select all classes named ".article" and assign that value to the articles variable.  
-
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
 */
 
 let articles = document.querySelectorAll('.article');
