0f3633ca3c96098b358f7c230da823d78fc4ab3c
EJaona <ejaona@gmail.com>
2018-12-06 06:20:57 -0600

    added text content of `expand` to expandButton

--- Article/Article.js
+++ Article/Article.js
@@ -9,7 +9,7 @@ class Article {
     this.expandButton = this.domElement.querySelector(".expandButton");
     console.log(this.expandButton);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
   }
 
