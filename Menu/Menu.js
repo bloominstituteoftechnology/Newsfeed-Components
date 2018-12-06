254e894cb44008d8c70d29afa506a74d705e171c
josharmantrout91 <josh.armantrout@gmail.com>
2018-12-06 11:44:50 -0500

    added ease in and ease out to menu click event listener in menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,14 @@
 const toggleMenu = () => {
   //Test Function: console.log('Get toggled, son!'); 
   menu.classList.toggle('menu--open');
+  if (menu.classList.contains('menu--open')){
+    TweenMax.to(menu, 1, {left: 0});
+  }
+  else {
+    TweenMax.to(menu, 1, {left: -350});
+  }
 }
+
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
