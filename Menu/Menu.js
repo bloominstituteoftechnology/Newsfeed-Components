437989b7edda8c3ce6aeb97dbdf4c4008564b920
Raine Wallace <rainebeaudubs@gmail.com>
2018-12-05 19:04:36 -0500

    As far as I got on the stretch, which is not far.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,8 +8,12 @@ document.querySelector('.menu');
 const menuButton = 
 document.querySelector('.menu-button');
 
-
+function getRandomColor() {
+  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+};
 
 menuButton.addEventListener('click', function(e) {
   e.target = toggleMenu();
+  e.target.style.backgroundColor = getRandomColor();
+  e.target.style.borderColor = getRandomColor();
 });
