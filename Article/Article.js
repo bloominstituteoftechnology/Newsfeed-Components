1b2761f9b9aa62587cd19c4e0a6b8397146a1d5a
byronholmes2018 <lshopeful2018@mgmail.com>
2018-12-06 05:06:37 -0600

    fixed a couple of errors in Article.js. Everything works according to MVP specs now.

--- Article/Article.js
+++ Article/Article.js
@@ -5,27 +5,24 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
-    this.expandButton = this.domElement.querySelector(".expandButton");
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', ()=>{this.expandArticle()});
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open")
+    this.domElement.classList.toggle('article-open');
   }
 }
 
 /* START HERE:
-
 - Select all classes named ".article" and assign that value to the articles variable.
-
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
 */
 
-let articles = document.querySelectorAll(".article);
-articles.forEach((article)=>{return new Article(article)});
-console.log(articles);
+let articles = document.querySelectorAll('.article').forEach((article) =>{
+  return new Article(article);
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,26 +1,12 @@
-class Article {
-  constructor(domElement) {
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class.
-    this.expandButton = this.domElement.querySelector(".expandButton");
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.innerText = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", () => { this.expandArticle() })
-  }
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open")
-  }
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open');
 }
-/* START HERE:
-- Select all classes named ".article" and assign that value to the articles variable.
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the
-articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-*/
-let articles = document.querySelectorAll(".article");
-articles.forEach(article => {
-  return new Article(article);
-})
-console.log(articles);
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector('.menu');
+// create a reference to the ".menu-button" class
+
+const menuButton = document.querySelector('.menu-button');;
+// Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click',toggleMenu);
