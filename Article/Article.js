702c66c9a62be88627f3dceecb9339d4de02ae95
KROHNICK <business.kanekperez@gmail.com>
2018-12-05 16:17:33 -0800

    Nav link fixes

--- Article/Article.js
+++ Article/Article.js
@@ -16,6 +16,7 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
   }
+
 }
 
 /* START HERE: 
