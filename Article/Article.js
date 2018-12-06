9e2b2323e2771f6886125e093d4cbea02e7510ec
Mike Landers <landers.mike@gmail.com>
2018-12-05 16:55:29 -0800

    * added remove button * remove fades out the article and then removes it

--- Article/Article.js
+++ Article/Article.js
@@ -4,41 +4,49 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+    
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     this.content = this.domElement.querySelector('.news-content');
+    this.remove = this.domElement.querySelector('.remove');
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());    
+    this.expandButton.addEventListener('click', (e) => this.expandArticle(e));    
 
+    // Setting click handler to remove the article
+    this.remove.addEventListener('click', (e) => this.removeArticle(e));    
     
     
-  }
+  }//End of constructor
 
-  expandArticle() {
+  removeArticle(e) {
+    setTimeout(() => {this.domElement.style.display = 'none'},400);
+    TweenMax.to(this.domElement, 0.5, {
+      opacity:0,
+    });
+  }//End of remove method
+
+  expandArticle(e) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    
+    console.log(e.target)
     // $(this.content).toggle( "slow")
 
-    
-
-
     if(this.domElement.classList.contains('article-open')){
-      this.content.classList.remove('news-content');
       articles.forEach((e)=>{
         e.classList.remove('article-open');
       });  
     }else{
-      this.content.classList.add('news-content');
       articles.forEach((e)=>{
         e.classList.remove('article-open');
       });  
       this.domElement.classList.add('article-open');
 
     }
-  }
-}
+  }//End of expandArticle method
+}// End of Class
 
 /* START HERE: 
 
