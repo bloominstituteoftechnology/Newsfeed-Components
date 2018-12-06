16d480e098a3b4d103b621dd69d0eed88033b149
JamesB <jblazer1@yahoo.com>
2018-12-06 08:37:29 -0800

    Add two articles to bottom

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,20 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', (e) => this.expandArticle(e));
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    // this.?.classList.toggle('article');
+     this.domElement.classList.toggle('article-open');
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,22 @@
-
-const toggleMenu = (e) => {
+const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
-  e.addEventListener.toggle('menu-open');
+  menu.classList.toggle('menu--open');
 }
+
+// what I had: did not work
+// const toggleMenu = (e) => {
+//   // Toggle the "menu--open" class on your menu refence.
+//   e.addEventListener.toggle('menu--open');
+// }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+ menuButton.addEventListener('click', toggleMenu);
+
+// what I had: worked
+// menuButton.addEventListener( 'click', function() {
+//   toggleMenu();
+// });
