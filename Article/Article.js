fb9fce037ca5b62b626028e0b649afe87e07a38e
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-05 19:06:50 -0500

    Stretch: Added slide animation to article cards with open and close intructions.

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.forEach((item) => item.textContent = 'expand');
+    this.expandButton.forEach((item) => item.textContent = 'Click to Expand');
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.forEach((item) => item.addEventListener('click', () => this.expandArticle()));
   }
@@ -15,6 +15,14 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    if (this.domElement.classList.contains('article-open')) {
+      TweenMax.to(this.domElement, 1, {height: 400});
+      console.log(this.expandButton);
+      this.expandButton[0].textContent = 'Click to Close';
+    } else {
+      TweenMax.to(this.domElement, 1, {height: 50});
+      this.expandButton[0].textContent = 'Click to Expand';
+    };
   }
 }
 
