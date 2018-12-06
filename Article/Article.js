543900ca49f9a8f132d02f2feebf9c973d1d65db
Nathan Thomas <nwthomas@me.com>
2018-12-06 08:47:13 -0800

    Add open/close article GSAP animation

--- Article/Article.js
+++ Article/Article.js
@@ -7,40 +7,26 @@ class Article {
     this.closeButton = this.domElement.querySelector(".close");
     this.expandButton.textContent = "expand";
     this.expandButton.addEventListener("click", () => this.expandArticle());
-
     this.closeButton.addEventListener("click", () => this.deleteArticle());
-
     allPanels.push(this);
   }
-
   expandArticle() {
     if (this.domElement.classList.contains("article-open")) {
-      this.domElement.classList.toggle("article-open");
+      TweenMax.to(this.domElement, 1, { className: "-=article-open" });
     } else {
       allPanels.forEach(article => {
         article.closeArticles();
       });
-      this.domElement.classList.toggle("article-open");
+      TweenMax.to(this.domElement, 1, { className: "+=article-open" });
     }
   }
-
   closeArticles() {
-    this.domElement.classList.remove("article-open");
+    TweenMax.to(this.domElement, 1, { className: "-=article-open" });
   }
-
   deleteArticle() {
     TweenMax.to(this.domElement, 0.5, { opacity: 0, display: "none" });
   }
 }
-
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
-
 let articles = document.querySelectorAll(".article");
 articles.forEach(article => {
   return new Article(article);
