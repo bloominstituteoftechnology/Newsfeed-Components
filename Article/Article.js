8d9f72df20ac7602b8c415126aa9d57f100cd7cb
Matt Basile <matthewbasile2@gmail.com>
2018-12-06 12:02:41 -0500

    Added close funciton when other tab is opened. LINES: article.js 25-35.

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,38 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+let allPanels =[];
 class Article {
   constructor(article) {
     // assign this.domElement to the passed in domElement
     this.article = article;
     // create a reference to the ".expandButton" class. 
     this.expandButton = article.querySelector('.expandButton');
-
-
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent="Expand";
     // // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener('click', () => this.handlePanelClick());
+    allPanels.push(this);
+  }
+  handlePanelClick(){
+    this.expandArticle(event.currentTarget);
+    console.log(event.currentTarget);
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     if(this.article.classList.contains("article-open")){
-      TweenMax.to(this.article, 2, {className: "-=article-open" })
+      TweenMax.to(this.article, 2, {className: "-=article-open" });
       this.expandButton.textContent="Expand"
     } else{
-    TweenMax.to(this.article, 1, {className: "+=article-open" })
-    this.expandButton.textContent="Close"
+      allPanels.forEach(article=>{
+        article.CloseArticles();
+      })
+      TweenMax.to(this.article, 1, {className: "+=article-open" });
+      this.expandButton.textContent="Close"
     }
   }
-
+  CloseArticles(){
+    TweenMax.to(this.article, 2, {className: "-=article-open" });
+    this.expandButton.textContent="Expand"
+  }
 }
 
 /* START HERE: 
