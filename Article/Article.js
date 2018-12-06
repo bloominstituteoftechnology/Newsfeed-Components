972a492b3744cf9a11b2d8fb448f6031d1637396
cecil john tantay <cjbtantay@gmail.com>
2018-12-05 21:38:39 -0800

    added delete feature and an animation when you click the x button

--- Article/Article.js
+++ Article/Article.js
@@ -8,6 +8,7 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
+    this.deleteX = this.domElement.querySelector(".deleteX");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
 
     this.expandButton.textContent = "Click to Expand";
@@ -16,18 +17,31 @@ class Article {
       this.updateText();
     });
 
+    this.deleteX.addEventListener("click", e => this.deleteThis(e));
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
 
-    this.expandButton.addEventListener("click", e => {
-      this.expandArticle(e);
+    this.expandButton.addEventListener("click", e => this.expandArticle(e));
+  }
+
+  deleteThis() {
+    TweenMax.to(this.domElement, 2, {
+      ease: Power4.easeOut,
+      x: -5500,
+      onComplete: ()=>{
+        this.domElement.style.display = "none";
+      }
     });
+    console.log("hello");
+
+    
   }
 
   updateText() {
     this.expandButton.textContent = "Click to Close";
     if (this.domElement.style.height === "400px") {
       this.expandButton.textContent = "Click to Expand";
-    } 
+    }
   }
 
   expandArticle(e) {
@@ -36,9 +50,9 @@ class Article {
     articleCounter++;
     console.log(articleCounter);
     //odd
-    
+
     if (Math.abs(articleCounter % 2) === 1) {
-      console.log(Math.abs(articleCounter % 2) === 1);
+      console.log(articleCounter % 2 === 1);
       TweenMax.to(this.domElement, 0.2, { height: "50px" });
       //even
     } else {
@@ -54,6 +68,8 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 
 */
+
+// new element for the x
 
 let articles = document.querySelectorAll(".article");
 
