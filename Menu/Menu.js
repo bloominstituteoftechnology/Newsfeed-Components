a85fa2badf2cbe8edd3b538ffca4db4d9152cd38
EJaona <ejaona@gmail.com>
2018-12-06 06:14:37 -0600

    added event listener on menuButton to toggle menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,3 +10,4 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
