f87cc9e218457c864637e011e0b2bc8c7e794a30
Randy Wilson <phoenixrr2113@gmail.com>
2018-12-05 19:18:25 -0500

    finished and got some animations done

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,31 +1,17 @@
-
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
-}
-const togglePage = () => {
-  page.classList.toggle('change');
-  // page.classList.toggle('.move-left')
-  // console.log(page)
+  page.classList.toggle('move-right');
+  navButton.classList.toggle('change');
 }
 
-// Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
-// create a reference to the ".menu-button" class
+
 const menuButton = document.querySelector('.menu-button');
-// Using your menuButton reference, add a click handler that calls toggleMenu
+
+const navButton = document.querySelector('.header-container');
+
 const page = document.querySelector('.container');
 
 menuButton.addEventListener('click', () => {
   toggleMenu();
 })
-
-page.addEventListener('click', () => {
-  togglePage();
-  // page.style.marginLeft = "250px";
-})
-
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-//   document.getElementById("main").style.marginLeft = "0";
-// }
