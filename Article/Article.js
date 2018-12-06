47da0e36a926753286ffb7283f4ca43b164a057d
Jacob Bryan <jtb36688@gmail.com>
2018-12-05 19:17:12 -0500

    Some stretch done

--- Article/Article.js
+++ Article/Article.js
@@ -1,30 +1,24 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     this.domElement = domElement;
-    this.Button = this.domElement.querySelector('.expandButton'); // <------------- Fix this reference to the expandButton class
+    this.Button = this.domElement.querySelector('.expandButton');
+    this.Button2 = this.domElement.querySelector('.closeButton');
     this.pElements = this.domElement.querySelectorAll('p');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.Button.textContent = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.Button.addEventListener('click', (e) => this.expandArticle(e));
+    this.Button.textContent = "Expand";
+    this.Button2.textContent = "Discard Article";
+    this.Button.addEventListener('click', () => this.expandArticle());
+    this.Button2.addEventListener('click', () => this.closeArticle());
   }
 
-  expandArticle(e) {
-    // this.pElements.forEach(element => element.classList.toggle('showhide'));
-    this.domElement.classList.toggle('article-open');
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+  expandArticle() {
+    this.pElements.forEach(element => element.classList.toggle('showhide'));
+  }
+  closeArticle() {
+    this.domElement.style.display = 'none';
   }
 }
 
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
 
 let articles = document.querySelectorAll('.article')
 
