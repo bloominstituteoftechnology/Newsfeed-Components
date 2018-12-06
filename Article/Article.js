2fade57cc4bf2d7e08ea40c2ebaebda47b01491f
cecil john tantay <cjbtantay@gmail.com>
2018-12-05 20:17:20 -0800

    added animation to article opening with button text updates

--- Article/Article.js
+++ Article/Article.js
@@ -1,24 +1,49 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Animate the article opening. Change the css. animate the component so that it slids open and slides closed on each click. Update the text "Click to Expand or "Click to Close
 
+// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+let articleCounter = 1;
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    this.expandButton.textContent = "expand";
-    
+
+    this.expandButton.textContent = "Click to Expand";
+
+    this.expandButton.addEventListener("click", e => {
+      this.updateText();
+    });
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
 
-    this.expandButton.addEventListener("click", ()=>this.expandArticle())
-
+    this.expandButton.addEventListener("click", e => {
+      this.expandArticle(e);
+    });
   }
 
-  expandArticle() {
+  updateText() {
+    this.expandButton.textContent = "Click to Close";
+    if (this.domElement.style.height === "400px") {
+      this.expandButton.textContent = "Click to Expand";
+    } 
+  }
+
+  expandArticle(e) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open");
+    e.stopPropagation();
+    articleCounter++;
+    console.log(articleCounter);
+    //odd
+    
+    if (Math.abs(articleCounter % 2) === 1) {
+      console.log(Math.abs(articleCounter % 2) === 1);
+      TweenMax.to(this.domElement, 0.2, { height: "50px" });
+      //even
+    } else {
+      TweenMax.to(this.domElement, 0.2, { height: "400px" });
+    }
   }
 }
 
@@ -32,6 +57,6 @@ class Article {
 
 let articles = document.querySelectorAll(".article");
 
-articles.forEach(item=>{
+articles.forEach(item => {
   return new Article(item);
 });
