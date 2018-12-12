11ea5d0cb8f7b76913e57dac8b8f41d031781bd1
nickkruger <ngkruger@gmail.com>
2018-12-12 14:22:39 -0700

    Menu system drops down

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,13 @@
 
 const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
 }
 
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector ('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector ('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
