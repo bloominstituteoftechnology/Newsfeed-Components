a8ee21a33b5f1557e8f5fda89de3434c317d306a
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-12 17:46:36 -0600

    watching camerons video and it is helping me make more moves in the project

--- Article/Article.js
+++ Article/Article.js
@@ -3,17 +3,22 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    // this.expandButton;
+    this.expandButton = this.domElement.querySelector("");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.textContent = "expand";
+    // Set a click handler on the expandButton reference, 
+    //calling the expandArticle method.
+    // this.expandButton.addEventListener("click", this.toggle)
     
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    // Using our reference to the domElement, 
+    //toggle a class to expand or hide the article.
+    // console.log("");
 
   }
 }
@@ -22,8 +27,15 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+- With your selection in place, now chain .forEach() on to the articles variable to 
+iterate over the articles NodeList and create a new instance of Article 
+by passing in each article as a parameter to the constructor.
 
 */
 
-let articles;
+// let articles;
+
+let articles = document.querySelectorAll(".article")
+
+articles.forEach(anything => {return new Article(anything)
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,53 @@
 
 const toggleMenu = () => {
+  
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
+
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
+
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener("click", () => menuButton.classList.togggle.toggleMenu());
+console.log();
+
+// const nav = document.querySelector(".nav");
+
+// nav.addEventListener('click', function(event) {
+//     console.log("Link was clicked!");
+// })
+
+
+// class Panel {
+//   constructor(panel){
+//     this.panel = panel;
+//     this.heading = this.panel.querySelector('.panel-content h3');
+//     this.content = this.panel.querySelector('.panel-content p');
+//     console.log(this.heading);
+//     // Non-arrow function 
+//     //this.heading.addEventListener('click', this.togglePanel.bind(this));
+//     this.heading.addEventListener('click', (e) => this.togglePanel(e));
+//   }
+//   // Methods
+//   togglePanel(e) {
+//     // only need the event object if we are using it***
+//     console.log(e.target);
+//     this.content.classList.toggle('toggle-on');
+//   }
+// }
+
+// const panels = document.querySelectorAll('.panel');
+
+// panels.forEach( panel => {
+//   return new Panel(panel);  
+// });
+
+// panels = Array.from(panels).map( panel => {
+//   return new Panel(panel);
+// })
