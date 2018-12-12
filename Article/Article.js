efbcad107c2df0318bc7856042ab1dd48c3da00c
Preprocessing - I <karenly1961@gmail.com>
2018-12-12 16:58:46 -0500

    @VivaCode - Making Changes

--- Article/Article.js
+++ Article/Article.js
@@ -2,10 +2,11 @@
 
 class Article {
   constructor(domElement) {
+    constructor(domElement)
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -14,7 +15,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,15 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  this.content.classList.toggle("toggle-on");
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = documentSelector(".menu");
+
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = documentSelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener("click", ( menuButton.classList.toggle("toggle-on"));)
