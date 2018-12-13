5c85cc04bfaf3b971e6561fea2094df2b4c60161
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-12 22:13:43 -0600

    my compiler was messed up all day. deleted my project and started new. using the code I worked on today turns out I do have an idea about what is going on

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,6 @@
 
 const toggleMenu = () => {
-  
+  menu.classList.toggle("menu--open");
   // Toggle the "menu--open" class on your menu refence. 
 }
 
@@ -14,40 +14,7 @@ const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener("click", () => menuButton.classList.togggle.toggleMenu());
-console.log();
-
-// const nav = document.querySelector(".nav");
-
-// nav.addEventListener('click', function(event) {
-//     console.log("Link was clicked!");
-// })
+menuButton.addEventListener("click", () => toggleMenu()
+);
 
 
-// class Panel {
-//   constructor(panel){
-//     this.panel = panel;
-//     this.heading = this.panel.querySelector('.panel-content h3');
-//     this.content = this.panel.querySelector('.panel-content p');
-//     console.log(this.heading);
-//     // Non-arrow function 
-//     //this.heading.addEventListener('click', this.togglePanel.bind(this));
-//     this.heading.addEventListener('click', (e) => this.togglePanel(e));
-//   }
-//   // Methods
-//   togglePanel(e) {
-//     // only need the event object if we are using it***
-//     console.log(e.target);
-//     this.content.classList.toggle('toggle-on');
-//   }
-// }
-
-// const panels = document.querySelectorAll('.panel');
-
-// panels.forEach( panel => {
-//   return new Panel(panel);  
-// });
-
-// panels = Array.from(panels).map( panel => {
-//   return new Panel(panel);
-// })
