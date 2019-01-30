10b010c5be98ab3792711a17d0ce74c4f021d70d
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-30 13:15:19 -0800

    Finished up the function for expanding and collapsing articles moving on.

--- Article/Article.js
+++ Article/Article.js
@@ -9,12 +9,13 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventLister('click', () => expandArticle());
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
+    this.expandButton.textContent = "Collapse"
   }
 }
 
