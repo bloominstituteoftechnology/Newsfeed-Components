6e9c1ee53835285980957448c05e9c7e4b493aec
joepak <josephpak2014@gmail.com>
2019-01-30 13:57:51 -0800

    Close Button!

--- Article/Article.js
+++ Article/Article.js
@@ -6,15 +6,20 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
+    this.closeButton = this.domElement.querySelector(".closeButton"); 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "Expand";
+    this.closeButton.textContent = "Close";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle())
+    this.closeButton.addEventListener("click", () => this.expandArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open")
+    this.domElement.classList.toggle("article-open");
+    this.expandButton.classList.toggle("hide-btn");
+    this.closeButton.classList.toggle("hide-btn");
   }
 }
 
