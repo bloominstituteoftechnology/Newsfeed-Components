90d5e1bb97e3ed12164784d6807734e3af432b70
Javontay McElroy <dinomightnetwork@gmail.com>
2019-01-30 15:37:13 -0600

    Working on getting the articles to show.

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,20 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
+class ArticleElement {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.articleContent = article.querySelector('.article-content');
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click'() => this.expandArticle());
   }
 
   expandArticle() {
+    this.expandButton.classList.toggle('close');
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
   }
 }
 
@@ -26,4 +26,5 @@ class Article {
 
 */
 
-let articles;
+const article = document.querySelectorAll('.article');
+articles.forEach( article => new ArticleElement(article));
