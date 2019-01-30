3c97e1c38eb66349ce1f1f28af05e7206c84e654
Kim Duclos <kimduclos@gmail.com>
2019-01-30 16:35:07 -0700

    added close button - just needed to figure out what happened withe expand button - was working before

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,40 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoisted you will need to start your code at the bottom of the page.  
+//Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement.expBtn = this.domElement.querySelector('.expandButton');
+    //const expBtn = document.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.domElement.expBtn.innerHTML = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.domElement.expBtn.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.article.classList.toggle('article-open');
+    this.domElement.expBtn.innerHTML = 'close';
+    //this.domElement.closeBtn.classList.toggle('.close');
+  }
 
+  closeArticle() {
+    this.domElement.article.classList.toggle('close');
   }
 }
 
 /* START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable.  */
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+let articles = document.querySelectorAll('.article');
 
+/*- With your selection in place, now chain .forEach() on to the articles variable to iterate over the 
+articles NodeList and create a new instance of Article by passing in each article as a parameter to the 
+constructor.
 */
 
-let articles;
+articles.forEach(article => new Article(article));
