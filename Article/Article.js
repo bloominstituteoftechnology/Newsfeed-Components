cb7e61cf52de1a3591c5fc9bed91bd2772ece921
Marc Thiongo <marcthuo@gmail.com>
2019-02-06 17:47:24 -0500

    article updates

--- Article/Article.js
+++ Article/Article.js
@@ -8,7 +8,8 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = article.querySelectorAll(".expandButton"); 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand"; 
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());   
   }
