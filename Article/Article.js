0393e66bb1ce2b63fdbf64b25f28fe1609a7298d
[taliashivers] <[talia.trembley@gmail.com]>
2019-02-05 21:19:11 -0700

    MVP COMPLETE AND A STRETCH GOAL!

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,47 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
+    //Read button
+    this.readButton = this.domElement.querySelector('.readButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle())
+    //Read button
+    this.readButton.addEventListener('click', () => {this.domElement.style.display = 'none'})
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    if(this.expandButton.innerText === 'Click to Expand'){
+      this.expandButton.innerText = 'Click to Close';
+      this.domElement.classList.toggle('article-open')
+      TweenMax.to(".article-open", .5, {
+        height:400,
+      })
+    }else if(this.expandButton.innerText === 'Click to Close'){
+      this.expandButton.innerText = 'Click to Expand';
+      TweenMax.to(".article-open", .5, {
+        height:50,
+      })
+      setTimeout(()=>{
+        this.domElement.classList.toggle('article-open')
+      }, 500)
+    }
   }
 }
 
 /* START HERE: 
-
 - Select all classes named ".article" and assign that value to the articles variable.  
-
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach( article => {
+  return new Article(article);
+  // article.querySelector('.expandButton').innerText = 'expand';
+})
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,30 @@
+let menuActive = false;
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  if(!menuActive){
+    menu.classList.toggle('menu--open');
+    TweenMax.to(".menu", .5, {
+      left:0,
+    })
+    menuActive = true;
+  } else if(menuActive){
+    TweenMax.to(".menu", .2, {
+      left:-350,
+    })
+    menuActive = false;
+    setTimeout(()=>{
+      menu.classList.toggle('menu--open');
+    }, 200)
+  }
 }
 
+
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => toggleMenu())
