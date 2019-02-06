e5dd115c033d78be2a5e409285746cc55bb8e346
shayaanakbar <shayaanakbar1106@gmail.com>
2019-02-05 20:37:36 -0600

    updated menu.js article.js & HTML code added

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.forEach((item) => item.textContent = 'expand');
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +26,4 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('article').forEach(article => new Artcile(article));
