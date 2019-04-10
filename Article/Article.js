af1b14c08d6319d368b3f7be6af0bd45a7da121a
EoPlex <you@example.com>
2019-04-10 15:02:09 -0700

    article js mvp complete

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,36 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+
+    this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.addEventListener("click",  
+    this.expandArticle.bind(this)
+    );
+  };
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
-  }
+    this.domElement.classList.toggle("article-open");
+  };
 }
 
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+- With your selection in place, now chain .forEach() on to the articles 
+  variable to iterate over the articles NodeList and create a new instance of 
+  Article by passing in each article as a parameter to the Article class.
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article"); 
+
+articles.forEach(function(element) {
+  return new Article(element);
+});
