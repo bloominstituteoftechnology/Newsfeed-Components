606ba615d91aea547647115f312d9d7ef60a1820
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-10 14:35:25 -0700

    Update Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -7,13 +7,14 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle("article-open");
 
   }
 }
