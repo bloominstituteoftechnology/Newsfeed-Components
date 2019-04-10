6a682dc809b9e33e8d740fc3046b1b33ad670c33
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-10 14:59:55 -0700

    Update Article.js
    had to make function implicit

--- Article/Article.js
+++ Article/Article.js
@@ -9,7 +9,7 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", expandArticle());
+    this.expandButton.addEventListener("click", this.expandArticle());
   }
 
   expandArticle() {
