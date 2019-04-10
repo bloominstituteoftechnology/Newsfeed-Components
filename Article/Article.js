ef468e8a64ca9b2aae6b1ce32865379b5d64b9f2
joerlop <jj.erlich155@gmail.com>
2019-04-10 16:39:41 -0500

    Added close button to articles

--- Article/Article.js
+++ Article/Article.js
@@ -7,8 +7,10 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
+    this.closeButton = this.domElement.querySelector(".closeButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "Click to expand";
+    this.closeButton.textContent = "Close";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => {
       this.expandArticle();
@@ -18,11 +20,19 @@ class Article {
         this.expandButton.textContent = "Click to expand"
       }
     })
+
+    this.closeButton.addEventListener("click", () => {
+      this.closeArticle();
+    })
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+  }
+  closeArticle() {
+    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle('article-close');
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,13 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  if (menu.classList.contains("slide-right")) {
+    menu.classList.replace("slide-right", "slide-left");
+  } else if (menu.classList.contains("slide-left")) {
+    menu.classList.replace("slide-left", "slide-right");
+  } else {
     menu.classList.toggle("slide-right");
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
