ccc71e417edbbdfbbdcb3e3db75719dd392e23f1
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-10 14:30:22 -0700

    Update Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -3,9 +3,9 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
