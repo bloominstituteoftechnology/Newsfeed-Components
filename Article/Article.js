853e2553ebd031e1d659d13a5a8e7b3acedd7b75
Manju KC <Manju KC>
2019-04-10 21:02:31 -0400

    Updated article js

--- Article/Article.js
+++ Article/Article.js
@@ -34,9 +34,27 @@ NodeList and create a new instance of Article by passing in each article as a pa
 
 */
 
+const paragraph = document.createElement("p")
+paragraph.textContent = "News sites come and go but there are several thebecome extremely popular. For instance, The Verge was founded less than 3 yearago and has become a dominant tech news player with millions of monthly visitors. Thereare several other large news sites using WordPress such as TechCrunch, Engadget, CNN, Forbes, Reuters, The New York Times and others. They are hosted on WordPress VIP hosting, which iscreated to handle millions of monthly visitors. It is a Nginx-powered web hosting, which."
+
+const seconaryContent = document.querySelector(".articles");
+
+const articleDiv = document.createElement('div')
+articleDiv.classList.add('article')
+
+const header2 = document.createElement('h2')
+header2.textContent = 'I just create a header'
+
+const explandSpan = document.createElement('span')
+explandSpan.classList.add('expandButton')
+
+articleDiv.append(header2)
+articleDiv.append(paragraph)
+articleDiv.append(explandSpan)
+
+seconaryContent.append(articleDiv);
 
 let articles = document.querySelectorAll(".article");
 articles.forEach(function(article){
   return  new Article(article)
 })
-
