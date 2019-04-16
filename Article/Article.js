e302e306f60af1ef5a61c99b7713645fe5444288
RyanSWest <west56309@gmail.com>
2019-04-16 17:30:30 -0500

    ryan-west 1stCommit

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,46 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton =  
+   domElement.querySelector('.expandButton');
+     
+     
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerHTML = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this)) 
+    // this.expandButton.addEventListener('click', () => this.expandArticle)
+    }
+ 
 
-  expandArticle() {
+   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+  this.domElement.classList.toggle('article-open');
   }
 }
 
+// pee = document.getElementsByTagName('p')[0];
+// pee.style.backgroundColor ='orange'
+// newButton = document.createElement('button');
+// newButton.textContent ='Kalima'
+// pee.append(newButton)
+// newButton.addEventListener('click',function(){
+//   pee.append('OMMM')
+// })
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+- With your selection in place, now chain .forEach() on to the articles
+ variable to iterate over the articles NodeList 
+ and create a new instance of Article by passing in each
+  article as a parameter to the Article class.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article') ;
+articles.forEach(article => new Article (article));
+console.log(articles)
+ 
