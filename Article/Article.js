955d444d033cd1a1e591d126ff473972336b7fae
Jason Barr <jason@jasonsbarr.com>
2019-04-16 21:47:27 -0500

    Add article component - use factory function

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,30 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+const createArticle = function(articleElement) {
+  const article = {
+    __proto__: articleProto,
+    articleElement: articleElement,
+    expandButton: articleElement.querySelector('.expandButton'),
+  };
+
+  article.expandButton.textContent = 'Expand';
+  article.registerEventHandlers();
+
+  return article;
+};
+
+const articleProto = {
+  registerEventHandlers: function() {
+    this.articleElement.addEventListener('click', this.toggleExpandArticle);
+  },
+
+  toggleExpandArticle: (event) => {
+    if (event.target.classList.contains('expandButton')) {
+      event.currentTarget.classList.toggle('article-open');
+    }
   }
+};
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
-  }
-}
-
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
-
-let articles;
+const articles = [...document.querySelectorAll('.article')].map(elem => {
+  return createArticle(elem);
+});
