21f8cfd35f267af2d07e82c47db2502aab0b15da
Jason Barr <jason@jasonsbarr.com>
2019-04-16 20:38:17 -0500

    Add JS for Menu component

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,21 @@
+const Menu = (function(menu) {
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector('.menu-button');
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
+  function registerEventHandlers() {
+    // Using your menuButton reference, add a click handler that calls toggleMenu
+    menuButton.addEventListener('click', event => toggleMenu());
+  }
 
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+  function toggleMenu() {
+    return menu.classList.toggle('menu--open');
+  }
+
+  function init() {
+    registerEventHandlers();
+  }
+  return {
+    init,
+  };
+  // Start Here: Create a reference to the ".menu" class
+})(document.querySelector('.menu')).init();
