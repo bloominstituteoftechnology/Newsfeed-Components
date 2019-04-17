e3c648a533645471ce9859d66d57078ffd693c0d
JordanG <jmg236@yahoo.com>
2019-04-17 17:08:47 -0500

    menu

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,19 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.expandButton.querySelector(".expandButton")
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+  expandArticle = () => {
+    // Using our reference to the domElement, toggle a class to 
+    // expand or hide the article.
+    this.expandArticle.classList.toggle(".expandbutton")
   }
 }
 
@@ -22,8 +23,41 @@ class Article {
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+- With your selection in place, now chain .forEach() on to the articles 
+variable to iterate over the articles NodeList and create a new instance 
+of Article by passing in each article as a parameter to the Article class.
 
 */
 
-let articles;
+let articles; document.querySelectorAll(".article")
+
+articles = Array.from(articles).map(articles => (
+  new Article(articles)
+));
+
+// class Panel {
+//   constructor(panel) {
+//     this.panel = panel;
+//     this.panelButtons = this.panel.querySelector(".panel-buttons");
+//     this.panelBtnOpen = this.panel.querySelector(".panel-btn-open");
+//     this.panelBtnClose = this.panel.querySelector(".panel-btn-close");
+//     this.panelContent = this.panel.querySelector(".panel-content")
+
+//     this.panelButtons.addEventListener("click", this.togglePanel)
+//   }
+//   togglePanel = () => {
+//     this.panelBtnOpen.classList.toggle("hide-btn");
+//     this.panelBtnClose.classList.toggle("hide-btn");
+//     this.panelContent.classList.toggle("toggle-on")
+//   }
+// }
+
+
+// let panels = document.querySelectorAll(".panel");
+
+// panels = Array.from(panels).map(panel => (
+//   new Panel(panel)
+// ));
+
+// const buttonClose = document.querySelector(".secondary .accordion .panel-btn-close")
+// console.log(buttonClose)
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,59 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle("menu--open")
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu");
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
+
+
+
+
+// class Panel {
+//   constructor(panel) {
+//     this.panel = panel;
+//     this.panelButtons = this.panel.querySelector(".panel-buttons");
+//     this.panelBtnOpen = this.panel.querySelector(".panel-btn-open");
+//     this.panelBtnClose = this.panel.querySelector(".panel-btn-close");
+//     this.panelContent = this.panel.querySelector(".panel-content")
+    
+//     // old binding of the this
+//     // this.panelButtons.addEventListener("click", this.togglePanel.bind(this))
+    
+//     // new: Using Arrow functions
+//     this.panelButtons.addEventListener("click", this.togglePanel)
+//   }
+//   // methods
+//   togglePanel = () => {
+//     // debugger;
+//     this.panelBtnOpen.classList.toggle("hide-btn");
+//     this.panelBtnClose.classList.toggle("hide-btn");
+//     this.panelContent.classList.toggle("toggle-on")
+//   }
+// }
+
+
+// let panels = document.querySelectorAll(".panel");
+// // console.log(panels)
+
+// // panels.forEach(panel => {
+// //   new Panel(panel);
+// // })
+
+// // Converting to an array
+
+// // impplicit vs explicit
+// // map, filter, reduce DOES NOT APPLY to forEach
+// panels = Array.from(panels).map(panel => (
+//   new Panel(panel)
+// ));
+
+// const buttonClose = document.querySelector(".secondary .accordion .panel-btn-close")
+// console.log(buttonClose)
