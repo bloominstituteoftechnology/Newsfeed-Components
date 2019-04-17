1ca8dea2650bc3cf4931a3a1c7c1e7099e79d480
JordanG <jmg236@yahoo.com>
2019-04-17 17:41:31 -0500

    article incomplete

--- Article/Article.js
+++ Article/Article.js
@@ -3,19 +3,25 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
+
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+
+    // create a reference to the ".expandButton" class.
+
     this.expandButton = this.expandButton.querySelector(".expandButton")
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+
+    // Using your expandButton reference, update the text on your 
+    // expandButton to say "expand"
+  
+    // Set a click handler on the expandButton reference,
+    // calling the expandArticle method.
+    this.expandButton.addEventListener('click', () => this.expandArticle)
   }
 
-  expandArticle = () => {
+  expandArticle() {
     // Using our reference to the domElement, toggle a class to 
     // expand or hide the article.
-    this.expandArticle.classList.toggle(".expandbutton")
+    console.log('EXPAND!', this.article)
   }
 }
 
@@ -29,11 +35,11 @@ of Article by passing in each article as a parameter to the Article class.
 
 */
 
-let articles; document.querySelectorAll(".article")
+let articles = document.querySelectorAll('.article')
+articles.forEach(function(article) {
+  const fancyArticle = new Article(article)
+});
 
-articles = Array.from(articles).map(articles => (
-  new Article(articles)
-));
 
 // class Panel {
 //   constructor(panel) {
