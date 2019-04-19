650874315578de94e3b8302c6eade84081b8e511
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-18 18:41:08 -0700

    expand and contract

--- Article/Article.js
+++ Article/Article.js
@@ -3,9 +3,9 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement.querySelector('.article');
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
