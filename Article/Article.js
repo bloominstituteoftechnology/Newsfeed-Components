afc76c3093c81b60b978dd0091bb9673a0deff11
Lambda_School_Loaner_96 <lambda_school_loaner_96@Lambda-School-Loaner-96s-MacBook-Pro.local>
2019-04-24 19:20:35 -0400

    News Feed

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,35 @@
+
+
+
+
+
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.article.classList.toggle('article-open');
 
   }
 }
 
 /* START HERE: 
-
 - Select all classes named ".article" and assign that value to the articles variable.  
-
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+// console.log(article);
+articles.forEach(article => new Article(article));
+//console.log(articles);
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,10 @@
 
-const toggleMenu = () => {
+//const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-}
+//}
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+//const menu;
 // create a reference to the ".menu-button" class
-const menuButton;
+//const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
