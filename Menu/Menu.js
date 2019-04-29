45ec9cd5e4c30882f811038b9ee83ebd547d4b7d
ckopecky <kopecky12112@gmail.com>
2019-04-28 17:16:07 -0700

    Toggle menu logic in place

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,15 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+//   // Toggle the "menu--open" class on your menu 
+    menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+console.log("menu", menu );
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
+console.log("menuBtn", menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener('click', toggleMenu)
