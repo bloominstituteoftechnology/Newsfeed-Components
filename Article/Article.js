486d372d277cb949054e0d835c287c50f9b9246f
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-30 20:50:26 -0700

    good moves

--- Article/Article.js
+++ Article/Article.js
@@ -14,7 +14,8 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.
+    this.domElement.classList.toggle('expandButton')
+    this.
   }
 }
 
