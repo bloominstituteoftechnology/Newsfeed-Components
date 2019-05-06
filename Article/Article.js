6d51b76883141135e2c624d424149546dfccfa41
Tara Sherman <taradelin@gmail.com>
2019-05-06 14:49:35 -0400

    fixed typo, articles expand correctly now

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector('expandButton');
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -16,7 +16,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.document.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
   };
 };
 
