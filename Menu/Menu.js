0e6451a48303dd5a91124fe738a79721d3ba1091
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-05-15 14:23:43 -0700

    WIP: working on article functionality

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,7 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
