64149c2809a54626def53a24ca026640a233b0d0
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-06-17 22:58:24 -0400

    errors fixed, working, MVP met

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,22 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement; //referencing article
+    
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton')
     this.expandButton.innerText = 'expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     //ES 6
+        // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => { this.expandArticle() });
-
-
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
   }
+    //ES 5
+    // this.expandButton.addEventListener("click", this.expandArticle.bind(this));
+
+
+    
+  
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
@@ -35,3 +39,7 @@ class Article {
 let articles = document.querySelectorAll('.article').forEach(article => {
   new Article(article)
 })
+
+
+
+
