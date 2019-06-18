158d2cb6a803f7a813fd2c308288246328458df2
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-06-17 20:31:30 -0400

    fixed errors, is working

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,24 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement; //referencing article
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton')
+    this.expandButton.innerText = 'expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
+    //ES 6
+    this.expandButton.addEventListener('click', () => { this.expandArticle() });
+
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    //need classList for toggle functionality 
+      this.domElement.classList.toggle('article-open')
   }
 }
 
@@ -26,4 +32,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(article => {
+  new Article(article)
+})
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,22 @@
-
+//WORKING 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  //.classList
+// classList will return an array-like object of all the classes on the element. There are a number of useful methods available on classList.
+// classList is a DOMTokenList.
+// A DOMTokenList is an array-like object with a numerical zero-based index, a length property, also the .contains() and .forEach() methods.
+// Most notably the methods .add() .remove() and .toggle() exist. All three take a single string representing the class.
+// .add('className') and .remove('className') do as their names indicate.
+// .toggle('className') will add the class if it does not exist and remove it if if does.
+  menu.classList.toggle('menu--open')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+//reference menuButton variable above which references btn from html 
+menuButton.addEventListener('click', () => {
+  toggleMenu(); //call tagglemenu function
+})
