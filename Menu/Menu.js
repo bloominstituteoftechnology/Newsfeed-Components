46f638feab343bbcbc8c64c57a16d998e57e9563
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-19 18:06:01 -0400

    done menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,7 +4,9 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll(".menu");
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click',toggleMenu);
+
