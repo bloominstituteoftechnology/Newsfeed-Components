f0a865b3ee91b383d7119a5e5ffbca1afde89294
Greg Johnston <johng3269@gmail.com>
2019-06-19 15:40:24 -0400

    Half done with Menu and Article. Very lost.

--- Article/Article.js
+++ Article/Article.js
@@ -3,13 +3,13 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement = domElement;
+    // create a reference to the ".expandButton" class.
+    this.expandButton = expandButton;
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    let expandButton = document.querySelectorAll(".expandButton").innerText = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+
   }
 
   expandArticle() {
@@ -18,12 +18,14 @@ class Article {
   }
 }
 
-/* START HERE: 
+/* START HERE:
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable.
 
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => new Article(article));
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const toggleMenu = (element) => {
+    menuButton.classList.toggle('menu--open')
+  // Toggle the "menu--open" class on your menu refence.
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getElementsByClassName('menu')
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getElementsByClassName('menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', event => {
+
+})
