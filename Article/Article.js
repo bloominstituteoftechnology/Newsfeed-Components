85523901f6895f15c47400a2c476b169bdd772d2
unknown <kenneth.austin93@gmail.com>
2019-06-19 19:17:55 -0400

    commit

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,34 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".expandButton");
+    this.closeButton = this.domElement.querySelector(".closeButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Click to expand";
+    this.closeButton.textContent = "Close";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", () => {
+      this.expandArticle();
+      if (this.expandButton.textContent == "Click to expand") {
+        this.expandButton.textContent = "Click to close"
+      } else {
+        this.expandButton.textContent = "Click to expand"
+      }
+    })
+    this.closeButton.addEventListener("click", () => {
+      this.closeArticle();
+    })
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
-}
+  }
+closeArticle();
+  this.domElement.classList.toggle('article-close');
 
 /* START HERE: 
 
@@ -26,4 +40,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article");
+
+articles.forEach(article => {
+  new Article(article);
+})
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-}
-
+  if (menu.classList.contains("slide-right")) {
+    menu.classList.replace("slide-right", "slide-left");
+  } else if (menu.classList.contains("slide-left")) {
+    menu.classList.replace("slide-left", "slide-right");
+  } else {
+    menu.classList.toggle("slide-right");
+  }
+}	
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector(".menu")
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu); 
