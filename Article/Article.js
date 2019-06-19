95b058051586ca4b9a45c23c8072f4710f0b3b2c
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-19 19:41:04 -0400

    the codes work now

--- Article/Article.js
+++ Article/Article.js
@@ -3,13 +3,12 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement = domElement.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     
-    this.articleContent = domElement.querySelector(".article-content")
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click',()=>{
       this.expandArticle()
@@ -18,7 +17,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.articleContent.classList.toggle("toggle-on");
+    this.domElement.classList.toggle("article-open");
   }
 }
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -7,7 +7,8 @@ const toggleMenu = () => {
 console.log(toggleMenu);
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll(".menu");
+const menu = document.querySelector(".menu");
+console.log(menu);
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
