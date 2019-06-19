95d0d3f4a0f30103f8e38373bb7a4a46800f0fa5
dangolbeeker <scottb305@outlook.com>
2019-06-19 16:33:22 -0400

    added articles to test component/fix article.js error

--- Article/Article.js
+++ Article/Article.js
@@ -5,7 +5,7 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-   this.expandButton = this.domElement.querySelector('expandButton');
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
    
@@ -16,7 +16,7 @@ class Article {
 }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-this.domElement.classList.toggle('article-open');
+    this.domElement.classList.toggle('article-open');
   }
 }
 
