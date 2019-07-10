c9ba9c993bc8b66f469a3072f8d3899830bae0cb
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-10 14:36:30 -0700

    Still working on getting paragraph styles

--- Article/Article.js
+++ Article/Article.js
@@ -1,32 +1,3 @@
-// // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
-// class Article {
-//   constructor(domElement) {
-//     // assign this.domElement to the passed in domElement
-//     this.domElement = domElement;
-//     // create a reference to the ".expandButton" class. 
-//     this.expandButton = this.domElement.querySelector('.expandButton');
-//     // Using your expandButton reference, update the text on your expandButton to say "expand"
-//     this.expandButton.textContent = 'expand'
-//     // Set a click handler on the expandButton reference, calling the expandArticle method.
-//     this.expandButton.addEventListener('click', () => this.expandArticle())
-//   }
-//   expandArticle() {
-//     // Using our reference to the domElement, toggle a class to expand or hide the article.
-//     console.log("click")
-//     return this.domElement.classList.toggle('article-open')
-//   }
-// }
-
-// //  START HERE: 
-// // - Select all classes named ".article" and assign that value to the articles variable.  
-// // - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-// let articles = document.querySelectorAll('.article');
-
-// articles.forEach(articles => {
-//   new Article(articles)
-// })
-
 const panelData = [{
     title: `Lambda School Students: "We're the best!"`,
     content: `<p>Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
@@ -48,7 +19,7 @@ const panelData = [{
         darth. Moff
         solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto.
         Yavin jawa sebulba
-        owen jinn tatooine sith organa.</p>
+        owen jinn tatooine sith organa.
 
       <p>Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
@@ -148,60 +119,6 @@ const panelData = [{
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.</p>`
   }
 ]
-
-// Remove panel from HTML
-// create new function that will create a panel component exactly as you see it in the HTML
-// give the component functionality when the open or closed button is clicked, the content is toggled on/off
-// create components for each of the items in the above array.
-
-// write a function 
-// use createElement 
-// returns a new DOM element
-
-// <div class="articles">
-//     <div class="article">
-//       <h2>Lambda School Students: "We're the best!"</h2>
-//       <p class="date">Nov 5th, 2017</p>
-//       <p></p>
-//       <p></p>
-//       <p</p>
-//       <span class='expandButton'></span>
-//     </div>
-//     <div class="article">
-//       <h2>Javascript and You, ES6</h2>
-//       <p class="date">Nov 7th, 2017</p>
-//       <p></p>
-//       <p></p>
-//       <span class='expandButton'></span>
-//     </div>
-//     <div class="article">
-//       <h2>React vs Angular vs Vue</h2>
-//       <p class="date">Nov 7th, 2017</p>
-//       <p></p>
-//       <p></p>
-//       <span class='expandButton'></span>
-//     </div>
-//     <div class="article">
-//       <h2>Professional Software Development in 2018</h2>
-//       <p class="date">Nov 7th, 2017</p>
-//       <p></p>
-//       <p></p>
-//       <p></p>
-//       <p></p>
-//       <span class='expandButton'></span>
-//     </div>
-//     <div class="article">
-//       <h2>Lambda School Students: "We're the best!"</h2>
-//       <p class="date">Nov 5th, 2017</p>
-//       <p></p>
-//       <p></p>
-//       <p</p>
-//       <span class='expandButton'></span>
-//     </div>
-//   </div>
-
-
-
 
 
 const accordion = document.querySelector('.accordion')
