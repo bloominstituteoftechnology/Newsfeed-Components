0130682da4b957c800f81012bc59262069a3c55e
Greg Johnston <johng3269@gmail.com>
2019-07-10 17:14:29 -0400

    Working on nav bar

--- articleComponents.js
+++ articleComponents.js
@@ -1,8 +1,8 @@
 const articlesC = document.querySelector('.articles');
-console.log(articlesC);
+// console.log(articlesC);
 
 function createNewArticle(title, date, content) {
-  console.log(title, date, content);
+  // console.log(title, date, content);
   const articleC = document.createElement('div');
   const titleC = document.createElement('h2');
   const dateC = document.createElement('p');
@@ -35,7 +35,7 @@ const articleData = [
   {
     title: "Lambda School Students: 'We're the best!'",
     date: "Nov 5th, 2017" ,
-    content: "Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth baba skywalker watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau darth calamari. Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine skywalker. Qui-gonn jar twi'lek jinn leia jango skywalker mon."
+    content: "Lucas ipsum dolor sit amet ben twi\'lek padm\u00E9 darth darth darth moff hutt organa twi\'lek. Ben amidala secura skywalker lando\r\nmoff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth baba skywalker\r\nwatto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau darth calamari.\r\nHutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine skywalker. Qui-gonn\r\njar twi\'lek jinn leia jango skywalker mon. \r\n\r\nGrievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2\r\nwindu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine\r\nmace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff\r\nk-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff\r\nsolo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba\r\nowen jinn tatooine sith organa.\r\n\r\nDagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious\r\nnaboo ackbar tatooine. Hutt lars padm\u00E9 darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket\r\nhan. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi\'lek padm\u00E9 wookiee. Leia\r\nmoff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon."
   },
   {
     title: "Javascript and You, ES6",
---
+++ headerComponent.js
@@ -0,0 +1,18 @@
+
+
+
+function navItems () {
+
+
+
+}
+
+
+const navList = [
+    'Students',
+    'Faculty',
+    'What\'s New',
+    'Tech Trends',
+    'Music',
+    'Log Out',
+]
