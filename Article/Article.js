18af7fd442ba0d97a73c307cb0a9bbdb0d512b3c
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-10 15:52:36 -0700

    Done

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,6 @@
 const panelData = [{
     title: `Lambda School Students: "We're the best!"`,
-    content: `<p>Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
+    content: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
         skywalker lando
         moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth
         baba skywalker
@@ -8,9 +8,9 @@ const panelData = [{
         darth calamari.
         Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine
         skywalker. Qui-gonn
-        jar twi'lek jinn leia jango skywalker mon. </p>
+        jar twi'lek jinn leia jango skywalker mon.
 
-      <p>Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
+      Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
         windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff
         chewbacca palpatine
         mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando
@@ -21,46 +21,16 @@ const panelData = [{
         Yavin jawa sebulba
         owen jinn tatooine sith organa.
 
-      <p>Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+      Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
         baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé
         wookiee. Leia
-        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.</p>`
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
     title: `Javascript and You, ES6`,
-    content: `<p>Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
-            skywalker lando
-            moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth
-            baba skywalker
-            watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau
-            darth calamari.
-            Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine
-            skywalker. Qui-gonn
-            jar twi'lek jinn leia jango skywalker mon. </p>
-
-          <p>Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
-            windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff
-            chewbacca palpatine
-             mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando
-             dantooine moff
-             k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth
-             darth. Moff
-             solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto.
-            Yavin jawa sebulba
-             owen jinn tatooine sith organa.</p>
-
-           <p>Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
-             naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
-             baba wicket
-             han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé
-             wookiee. Leia
-             moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.</p>`
-  },
-  {
-    title: "React vs Angular vs Vue",
-    content: `<p>Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
+    content: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
         skywalker lando
         moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth
         baba skywalker
@@ -68,9 +38,9 @@ const panelData = [{
         darth calamari.
         Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine
         skywalker. Qui-gonn
-        jar twi'lek jinn leia jango skywalker mon. </p>
+        jar twi'lek jinn leia jango skywalker mon.
 
-      <p>Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
+      Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
         windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff
         chewbacca palpatine
         mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando
@@ -79,18 +49,18 @@ const panelData = [{
         darth. Moff
         solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto.
         Yavin jawa sebulba
-        owen jinn tatooine sith organa.</p>
+        owen jinn tatooine sith organa.
 
-      <p>Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+      Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
         baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé
         wookiee. Leia
-        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.</p>`
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: "Professional Software Development in 2018",
-    content: `<p>Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
+    title: `React vs Angular vs Vue`,
+    content: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala      secura
         skywalker lando
         moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth
         baba skywalker
@@ -98,9 +68,9 @@ const panelData = [{
         darth calamari.
         Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine
         skywalker. Qui-gonn
-        jar twi'lek jinn leia jango skywalker mon. </p>
+        jar twi'lek jinn leia jango skywalker mon.
 
-      <p>Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
+      Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
         windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff
         chewbacca palpatine
         mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando
@@ -109,71 +79,99 @@ const panelData = [{
         darth. Moff
         solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto.
         Yavin jawa sebulba
-        owen jinn tatooine sith organa.</p>
+        owen jinn tatooine sith organa.
 
-      <p>Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+      Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
         baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé
         wookiee. Leia
-        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.</p>`
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+  },
+  {
+    title: `Professional Software Development in 2018`,
+    content: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura
+        skywalker lando
+        moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth
+        baba skywalker
+        watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau
+        darth calamari.
+        Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine
+        skywalker. Qui-gonn
+        jar twi'lek jinn leia jango skywalker mon. 
+        
+        Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
+        windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff
+        chewbacca palpatine
+        mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando
+        dantooine moff
+        k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth
+        darth. Moff
+        solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto.
+        Yavin jawa sebulba
+        owen jinn tatooine sith organa.
+
+        Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+        naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff
+        baba wicket
+        han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé
+        wookiee. Leia
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   }
-]
+];
 
-
-const accordion = document.querySelector('.accordion')
+const accordion = document.querySelector(".accordion");
 
 panelData.forEach(data => {
-  console.log('creating', data.title)
-  accordion.appendChild(createPanel(data.title, data.content))
-})
+  console.log("creating", data.title);
+  accordion.appendChild(createPanel(data.title, data.content));
+});
 // accordion.appendChild(createPanel('Test Title','Test Content'))
 
 function createPanel(title, content) {
   // define new elements
-  const panel = document.createElement('div');
-  const panelBar = document.createElement('div');
-  const panelTitle = document.createElement('h3');
-  const buttonPanel = document.createElement('div');
-  const buttonOpen = document.createElement('button');
-  const buttonClose = document.createElement('button');
-  const panelContent = document.createElement('div');
+  const panel = document.createElement("div");
+  const panelBar = document.createElement("div");
+  const panelTitle = document.createElement("h3");
+  const buttonPanel = document.createElement("div");
+  const buttonOpen = document.createElement("button");
+  const buttonClose = document.createElement("button");
+  const panelContent = document.createElement("p");
 
   // Setup structure of elements
-  panel.appendChild(panelBar)
-  panel.appendChild(panelContent)
-  panelBar.appendChild(panelTitle)
-  panelBar.appendChild(buttonPanel)
-  buttonPanel.appendChild(buttonOpen)
-  buttonPanel.appendChild(buttonClose)
+  panel.appendChild(panelBar);
+  panel.appendChild(panelContent);
+  panelBar.appendChild(panelTitle);
+  panelBar.appendChild(buttonPanel);
+  buttonPanel.appendChild(buttonOpen);
+  buttonPanel.appendChild(buttonClose);
 
   // set class names
-  panel.classList.add('panel')
-  panelBar.classList.add('panel-bar')
-  buttonPanel.classList.add('panel-buttons')
-  buttonOpen.classList.add('panel-btn-open')
-  buttonClose.classList.add('panel-btn-close', 'hide-btn')
-  panelContent.classList.add('panel-content')
+  panel.classList.add("panel");
+  panelBar.classList.add("panel-bar");
+  buttonPanel.classList.add("panel-buttons");
+  buttonOpen.classList.add("panel-btn-open");
+  buttonClose.classList.add("panel-btn-close", "hide-btn");
+  panelContent.classList.add("panel-content");
 
   // set text content
-  buttonOpen.textContent = 'Open'
-  buttonClose.textContent = 'Close'
-  panelContent.textContent = content
-  panelTitle.textContent = title
+  buttonOpen.textContent = "Open";
+  buttonClose.textContent = "Close";
+  panelContent.textContent = content;
+  panelTitle.textContent = title;
 
   // button events
-  buttonPanel.addEventListener('click', event => {
-    console.log('button clicked', event.target)
+  buttonPanel.addEventListener("click", event => {
+    console.log("button clicked", event.target);
     // 1. toggle hide-btn on BOTH buttons
-    buttonOpen.classList.toggle('hide-btn')
-    buttonClose.classList.toggle('hide-btn')
+    buttonOpen.classList.toggle("hide-btn");
+    buttonClose.classList.toggle("hide-btn");
     // 2. Change visibility of the content w/ 'toggle-on'
-    panelContent.classList.toggle('toggle-on')
-  })
-
+    panelContent.classList.toggle("toggle-on");
+  });
 
   // (DO THIS) buttonPanel.addEventListener('click', toggleHandler)
   // (DO NOT DO)buttonPanel.querySelectorAll('button').forEach(btn => btn.addEventListener('click', clickHandler))
 
-  return panel
+  return panel;
 }
