289ad9a19a89bddb5c7e2999c1ed0eaf77f712f8
dangolbeeker <scottb305@outlook.com>
2019-07-10 15:57:56 -0400

    refactoring code

--- Article/Article.js
+++ Article/Article.js
@@ -10,13 +10,13 @@ class Article {
     this.expandButton.textContent = 'expand';
    
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () =>{
+    expandButton.addEventListener('click', () =>{
       this.expandArticle();
     });
 }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    domElement.classList.toggle('article-open');
   }
 }
 
