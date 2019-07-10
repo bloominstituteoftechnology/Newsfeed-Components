64402f26b2a496b42c0a9c92312efcba6531c0ac
johnschneider1 <johnlawschneider@gmail.com>
2019-07-10 17:40:52 -0500

    finishing up project

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,9 @@
+// const toggleMenu = () => {
+//   // Toggle the "menu--open" class on your menu refence.
+// }
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
-
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+// // Start Here: Create a reference to the ".menu" class
+// const menu;
+// // create a reference to the ".menu-button" class
+// const menuButton;
+// // Using your menuButton reference, add a click handler that calls toggleMenu
