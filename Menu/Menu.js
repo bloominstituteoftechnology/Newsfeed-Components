3aa0fb494c392a9dd7c326559684836443b2f2a3
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-07 22:51:56 -0500

    menu functional

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,4 +1,5 @@
-/* This is the data we will be using, study it but don't change anything, yet. */
+window.addEventListener('load', (e)=> {
+//  This is the data we will be using, study it but don't change anything, yet. 
 
 let menuItems = [
   'Students',
@@ -9,27 +10,51 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+  // Step 1: Write a function that will create a menu component as seen below:
 
-  Step 1: Write a function that will create a menu component as seen below:
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as a list item}
+  //   </ul>
+  // </div>
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+  // Pass the function an array as it's only argument.
+ 
+const header = document.querySelector('.header');
 
-  Pass the function an array as it's only argument.
+function createMenu(array){
+  const menu = document.createElement('div');
+  const ul = document.createElement('ul');
+  menu.appendChild(ul);
+  menu.classList.add('menu');
+  for(i = 0; i < array.length; i ++){
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = (`${array[i]}`);
+  }
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+  let menuButton = document.querySelector('.menu-button');
+  console.log(menuButton)
+  menuButton.addEventListener('click', (e)=> {
+    console.log('button clicked');
+    menu.classList.toggle('menu--open');
+  })
 
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+  console.log(header);
+  return menu;
+}
+header.appendChild(createMenu(menuItems));
+
+  // Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  // Add those items to the <ul>
+
+  // Step 3: Using a DOM selector, select the menu button currently on the DOM.
   
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
-  Step 5: return the menu component.
+  // Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
-  Step 6: add the menu component to the DOM.
+  // Step 5: return the menu component.
+
+  // Step 6: add the menu component to the DOM.
   
-*/
+})
--- README.md
+++ README.md
@@ -40,7 +40,7 @@ x Import `Article.less` from the correct path into index.less (follow the exampl
 
 ### Part 2: Add Articles
 
-* Now that we have our components built, add a few articles of your own to the data array. Notice how our JavaScript code automatically creates the new articles and adds the styling and functionality we just built to them. This is the power of components. Write the code once and add as many components as you want all while maintaining the functionality we built!
+x Now that we have our components built, add a few articles of your own to the data array. Notice how our JavaScript code automatically creates the new articles and adds the styling and functionality we just built to them. This is the power of components. Write the code once and add as many components as you want all while maintaining the functionality we built!
 
 ### Part 3: The Menu
 
