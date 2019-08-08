dab31d2b247fadeb4a29284679cf96b0088ff0d2
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-07 21:37:22 -0500

    added new component and new article

--- Article/Article.js
+++ Article/Article.js
@@ -1,8 +1,6 @@
 /* This is the data we will be using to create our article components */
 /* Look over this data, then proceed to line 91*/
 window.addEventListener('load', (e)=> {
-
-
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
@@ -88,10 +86,24 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Who Is Spider-Man?',
+    date: 'December 25, 2019',
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   }
 ];
-
-
 
 //  Step 1: Create a function that creates a component. You will want your component to look like the template below: 
   
@@ -143,7 +155,6 @@ buttonExpand.classList.add('expandButton');
 
 const open = '\u25bc';
 
-
 //set text content
 title.textContent = t;
 date.textContent = d;
@@ -152,28 +163,33 @@ paragraph2.textContent = p2;
 paragraph3.textContent = p3;
 buttonExpand.textContent = open;
 
-
-
-
-
-
   // Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
   buttonExpand.addEventListener('click', (e) => {
     article.classList.toggle('article-open');
   })
 
+  // Step 3: return the entire component.
 //return article
 
 return article; 
-  // Step 3: return the entire component.
-// console.log(createArticle);
+}
   // Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
+function createAuthor(author){
+  const auth = document.createElement('h6');
+  auth.textContent = author;
+  return auth;
+}
 
 
-  // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
+let arts = Array.from(document.querySelectorAll(".article"));
+console.log(arts)
+
+for(i=0; i < arts.length; i++){
+  arts[i].prepend(createAuthor("M.J. Watson"));
+}
+
+  // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article
 
  
-}//function
-
-})
+})// Window load event
