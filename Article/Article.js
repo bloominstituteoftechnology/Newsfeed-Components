ebcd16740420e917680b948884772feaa51a533f
spettigrew <sara.pettigrew5@gmail.com>
2019-08-14 13:43:17 -0600

    2 paragraphs and 1 menu item added

--- Article/Article.js
+++ Article/Article.js
@@ -85,13 +85,22 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
-
-  title: 'Sara Pettigrew - Lambda student in webPT8',
+  },
+  {
+  title: 'Lambda student - webPT8',
   date: '13 August 2019',
   firstParagraph: 'Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space vSpace Space Space Space Space Space Space Space Space Space Space Space vvSpace Space vSpace Space vvvvvvSpace Space Space vSpace vvvvSpace Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space Space',
   secondParagraph: 'Second Space Second SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond SpaceSecond Space',
   thirdParagraph: 'The End.'
+  },
+  {
+    title: 'To <div>, or not to </div>, that is the question?',
+    date: '6 June 2019',
+    firstParagraph: 'Yes. Keep it to where you and future web developers can understand your encrypted code.',
+    secondParagraph: 'Seriously, don\'t get carried away. People beside yourself have to read this stuff. Also, check your spelling. Super annoying to have to spell check while trying to read your encrypted code!',
+    thirdParagraph: 'Thank you.'
+  
+  }
 ];
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -6,6 +6,7 @@ let menuItems = [
   "What's New",
   'Tech Trends',
   'Music',
+  'Job Board',
   'Log Out'
 ];
 
