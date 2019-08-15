4a183b865df960f380d9df7407389593b5d788de
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-15 17:28:21 +0200

    change comments a bit to make instructions clearer

--- Article/Article.js
+++ Article/Article.js
@@ -101,7 +101,7 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each peice of the data object above.
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
 
   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -19,14 +19,14 @@ let menuItems = [
     </ul>
   </div>
 
-  Pass the function an array as it's only argument.
+  The function takes an array as its only argument.
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
 
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
-  
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+
+  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
   Step 5: return the menu component.
 
