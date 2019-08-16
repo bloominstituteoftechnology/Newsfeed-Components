ddffc3c0a630ba22ebc42199e08d68a3d4eedc49
JoeLorenzo <Joselmedrano@gmail.com>
2019-08-15 21:42:55 -0500

    -Finsihed article componets.

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,6 @@
 /* This is the data we will be using to create our article components */
 /* Look over this data, then proceed to line 91*/
-const data = [
+ const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
     date: 'Nov 5th, 2018',
@@ -37,9 +37,9 @@ const data = [
         mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma
         and Parvati Sorting Hat Minister of Magic blue turban remember my last.`,
 
-    thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
-        Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
-        roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
+    thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights
+        Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven
+        roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
@@ -66,8 +66,8 @@ const data = [
         consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
 
-    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
-        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
+    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel
+        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
@@ -85,11 +85,73 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+  },
+  {
+  title: 'Lambda School Students: "We\'re the best!"',
+  date: 'Nov 5th, 2018',
+  firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+      moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+      watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+      Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+      jar twi'lek jinn leia jango skywalker mon.`,
+}
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
-  
+
+function articleCreater(article){
+
+  const articleDiv = document.createElement('div');
+  const h2Tag = document.createElement('h2');
+  const pTag = document.createElement('p');
+  const p1Tag = document.createElement('p');
+  const p2Tag = document.createElement('p');
+  const p3Tag = document.createElement('p');
+  const spanTag = document.createElement('span');
+
+  articleDiv.classList.add('article');
+  pTag.classList.add('date');
+  spanTag.classList.add('expandButton');
+  h2Tag.textContent = article.title;
+  pTag.textContent = article.date;
+  spanTag.textContent = 'Expand';
+  p1Tag.textContent = article.firstParagraph;
+  p2Tag.textContent = article.secondParagraph;
+  p3Tag.textContent = article.thirdParagraph;
+  articleDiv.appendChild(h2Tag);
+  articleDiv.appendChild(pTag);
+  articleDiv.appendChild(p1Tag);
+  articleDiv.appendChild(p2Tag);
+  articleDiv.appendChild(p3Tag);
+  articleDiv.appendChild(spanTag);
+  spanTag.addEventListener('click', () => {
+  articleDiv.classList.toggle('article-open');
+  });
+
+   return articleDiv
+ }
+
+
+function articeOutput(array){
+  newArr = array.map(article => {
+    return articleCreater(article)
+  })
+}
+
+articeOutput(data)
+console.log(newArr)
+
+const articleDivClass = document.querySelector('.articles');
+function appendComponents(componentArr){
+  componentArr.forEach(component => articleDivClass.appendChild(component)
+)
+}
+
+appendComponents(newArr);
+
+
+
+/* Step 1: Create a function that creates a component. You will want your component to look like the template below:
+
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
