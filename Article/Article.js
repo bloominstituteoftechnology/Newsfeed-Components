5abe7046adb8592c7e36b44b61300354d911dfb7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-04 14:06:01 +0100

    create function componentBuilder

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,20 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function componentBuilder() {
+  const article = document.createElement('div');
+  article.classList.add('article')
+
+  const title = document.createElement('h2');
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+
+  const paragraph1 = document.createElement('p');
+  const paragraph2 = document.createElement('p');
+  const paragraph3 = document.createElement('p');
+
+  const button = document.createElement('button');
+  button.classList.add('expandButton');
+}
