0f0290cea61b1efaaf450987d4d94d8636c0dd8b
ChiragThesia <harithesia@gmail.com>
2019-10-08 10:40:17 -0500

    Created a CSS file and created basic function for menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -18,18 +18,32 @@ let menuItems = [
       {each menu item as a list item}
     </ul>
   </div>
-
-  The function takes an array as its only argument.
-
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
-  
 */
+//The function takes an array as its only argument.
+  function createNav(){
+    const navLi = document.createElement('li');
+    const navUl = document.createElement('ul');
+
+    navUl.classList.add('.')
+
+
+
+  }
+  
+  const navUl = document.querySelector('')
+  menuItems.forEach((data) => {
+
+    
+  });
+
+// Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+// Add those items to the <ul>
+
+// Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+
+// Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+
+// Step 5: return the menu component.
+
+// Step 6: add the menu component to the DOM.
+
