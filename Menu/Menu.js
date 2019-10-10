019201b1052e12004dc5d3c6aef56e0109e5ab29
Samuel Kraus <S_read@aol.com>
2019-10-10 17:50:30 -0500

    Update Menu.js
    copmleted menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -34,6 +34,41 @@ let menuItems = [
   
 */
 
-function menuCreator(array)
 
 
+
+function menuCreator(array){
+  const newMenu = document.createElement('div')
+  newMenu.classList.add('menu')
+
+  const menuList = document.createElement('ul')
+  newMenu.appendChild(menuList);
+
+
+  for(i = 0; i < array.length; i++){
+    var listItem = document.createElement('li')
+    listItem.textContent = array[i]
+    menuList.appendChild(listItem);
+  };
+  
+
+
+  const button = document.querySelector('.menu-button');
+
+  function toggleMenu(){
+    newMenu.classList.toggle('menu--open')
+  }
+  
+  button.addEventListener('click', toggleMenu);
+ 
+
+  return newMenu;
+
+};
+
+
+const header = document.querySelector('.header')
+header.appendChild(menuCreator(menuItems));
+
+
+console.log(menuCreator(menuItems));
