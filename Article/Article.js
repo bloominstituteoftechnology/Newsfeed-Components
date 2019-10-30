d2db9b75335e895a9279efed025e6bd43c5bc9ce
Jessica Novak <jessnovak2@gmail.com>
2019-10-30 14:56:17 -0700

    final commit

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "New Article",
+    date: "Nov 1st, 2019",
+    firstParagraph: `First Paragraph filled with all sorts of useful information `,
+
+    secondParagraph: `Second paragraph filled with all sorts of useful information `,
+
+    thirdParagraph: `Even more useful information`
   }
 ];
 
@@ -93,70 +102,60 @@ const data = [
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
-
     {three separate paragraph elements}
-
     <span class='expandButton'></span>
   </div>
-
   Hint: You will need to use createElement more than once here!
-
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
   Step 3: return the entire component.
-
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
 */
-function newsfeedCreator(articleTitle, articleDate, paragraphOne, paragraphTwo, paragraphThree) {
-  const article = document.createElement('div');
-  const title = document.createElement('h2');
-  const date = document.createElement('p');
-  const firstParagraph = document.createElement('p');
-  const secondParagraph = document.createElement('p');
-  const thirdParagraph = document.createElement('p');
-  const buttonHolder = document.createElement('span');
-  const buttonOpen = document.createElement('button');
-  const buttonClose = document.createElement('button');
 
-  article.classList.add('article');
-  date.classList.add('date');
-  buttonHolder.classList.add('expandButton')
-  buttonOpen.classList.add('close');
-  buttonClose.classList.add('close');
+// Component creator function
 
-  const open = '\u25bc';
-  const close = '\u25b2';
+const article = document.querySelector(".articles");
 
-  buttonOpen.textContent = open;
-  buttonClose.textContent = close;
-  title.textContent = articleTitle;
-  date.textContent = articleDate;
-  firstParagraph.textContent = paragraphOne;
-  secondParagraph.textContent = paragraphTwo;
-  thirdParagraph.textContent = paragraphThree;
+function articleCreator(info) {
+  const mainDiv = document.createElement("div");
+  const titleArticle = document.createElement("h2");
+  const dateArticle = document.createElement("p");
+  const paragraph1 = document.createElement("p");
+  const paragraph2 = document.createElement("p");
+  const paragraph3 = document.createElement("p");
+  const expandButton = document.createElement("span");
 
-  buttonOpen.addEventListener('click', () => {
-    article.classList.toggle('article-open');
+  // Adding content
+
+  titleArticle.textContent = info.title;
+  dateArticle.textContent = info.date;
+  paragraph1.textContent = info.firstParagraph;
+  paragraph2.textContent = info.secondParagraph;
+  paragraph3.textContent = info.thirdParagraph;
+  expandButton.textContent = "Read More";
+
+  // Add event listener for button
+
+  expandButton.addEventListener("click", e => {
+    mainDiv.classList.toggle("article-open");
   });
 
+  mainDiv.appendChild(titleArticle);
+  mainDiv.appendChild(dateArticle);
+  mainDiv.appendChild(paragraph1);
+  mainDiv.appendChild(paragraph2);
+  mainDiv.appendChild(paragraph3);
+  mainDiv.appendChild(expandButton);
 
-  article.appendChild(title);
-  article.appendChild(date);
-  //article.appendChild(buttonHolder);
-  article.appendChild(buttonOpen);
-  article.appendChild(firstParagraph);
-  article.appendChild(secondParagraph);
-  article.appendChild(thirdParagraph);
-  return article;
+  mainDiv.classList.add("article");
+  titleArticle.classList.add("h2");
+  dateArticle.classList.add("date");
+  expandButton.classList.add("expandButton");
+
+  return mainDiv;
 }
 
-const articleHolder = document.querySelector('.articles');
-
-data.forEach((data) => {
-  articleHolder.appendChild(newsfeedCreator(data.title, data.date, data.firstParagraph, data.secondParagraph, data.thirdParagraph));
-})
+data.forEach(cv => {
+  article.appendChild(articleCreator(cv));
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,58 +1,53 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
-
   Step 1: Write a function that will create a menu component as seen below:
-
   <div class="menu">
     <ul>
       {each menu item as a list item}
     </ul>
   </div>
-
   The function takes an array as its only argument.
-
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
-
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
   Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
   Step 5: return the menu component.
-
   Step 6: add the menu component to the DOM.
   
 */
-function menuCreator(arr) {
-  const navigation = document.createElement('div');
-  const navItems = document.createElement('ul');
 
-  navigation.classList.add('menu');
+function menuFunction(newData) {
+  const newMenu = document.createElement("div");
+  const uL = document.createElement("ul");
+  const hamburg = document.querySelector(".menu-button");
 
-  arr.forEach(link => {
-    let menuItem = document.createElement('li');
-    menuItem.textContent = link;
-    navItems.appendChild(menuItem);
+  newMenu.appendChild(uL);
+
+  newData.forEach(element => {
+    const listItem = document.createElement("li");
+    listItem.textContent = element;
+    uL.appendChild(listItem);
   });
 
-  let menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', () => {
-  navigation.classList.toggle('menu--open');
+  newMenu.classList.add("menu");
 
-})
-  navigation.appendChild(navItems);
-  
-  return navigation;
+  hamburg.addEventListener("click", () => {
+    newMenu.classList.toggle("menu--open");
+  });
+
+  return newMenu;
 }
 
-document.querySelector('.header').appendChild(menuCreator(menuItems));
+const myHeader = document.querySelector(".header");
+
+myHeader.appendChild(menuFunction(menuItems));
