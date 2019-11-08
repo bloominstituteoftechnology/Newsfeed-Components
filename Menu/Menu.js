d05a38c6b9be579125e670ef4f652b59379057ae
Alexis <reche06@gmail.com>
2019-11-07 19:14:47 -0500

    still debugging menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,13 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
-
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
+
 
 /* 
 
@@ -33,3 +33,27 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+function createMenu (arr) {
+  const menu=document.createElement ('div');
+  const ul=document.createElement ('ul');
+  menu.classList.add('menu');
+  menu.appendChild(ul);
+
+ /* Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Add those items to the <ul>*/
+   arr.forEach(element => {
+     let li=document.createElement('li');
+     li.textContent=element;
+     ul.appendChild(li);
+   });
+  /*
+  Step 3: Using a DOM selector, select the menu button currently on the DOM.*/
+  const menuButton=document.querySelector ('.menu-button');
+  /*Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself*/
+  menuButton.addEventListener('click', event=> {menu.classList.toggle('menu--open')});
+
+  // Step 5: return the menu component.
+return menu;
+  }
+  // Step 6: add the menu component to the DOM.
+    document.querySelector('.header').appendChild(createMenu(menuItems));
