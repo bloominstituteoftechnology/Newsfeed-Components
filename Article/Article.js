cd448aeb94b5693122dc6adde448a1a19bbd647d
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2019-12-11 16:39:12 -0500

    text not showing

--- Article/Article.js
+++ Article/Article.js
@@ -85,15 +85,6 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  },
-  {
-    title: 'This is my added article',
-    date: 'December 10th, 2019',
-    firstParagraph: `This is the first paragraph of my added article!!`,
-
-    secondParagraph: `Woah, hello another added paragraph!!!`,
-
-    thirdParagraph: `WAIT, you guessed it?! Another paragraph???`
   }
 ];
 
@@ -134,12 +125,12 @@ function createArticle(title, date, firstParagraph, secondParagraph, thirdParagr
   const paraThree = document.createElement('p');
   const button = document.createElement('span');
 
-  article.appendChild(articleTitle);
-  article.appendChild(articleDate);
-  article.appendChild(paraOne);
-  article.appendChild(paraTwo);
-  article.appendChild(paraThree);
-  article.appendChild(button);
+  article.appendChild(articleTitle)
+  article.appendChild(articleDate)
+  article.appendChild(paraOne)
+  article.appendChild(paraTwo)
+  article.appendChild(paraThree)
+  article.appendChild(button)
 
   article.classList.add('article');
   articleDate.classList.add('date');
@@ -150,15 +141,18 @@ function createArticle(title, date, firstParagraph, secondParagraph, thirdParagr
   paraOne.textContent = firstParagraph;
   paraTwo.textContent = secondParagraph;
   paraThree.textContent = thirdParagraph;
+  button.textContent = 'Click to Expand';
 
   button.addEventListener('click', (e) => {
     article.classList.toggle('article-open');
   })
 
+
   return article;
 }
 
-data.forEach((data) => {
-  articles.appendChild(createArticle(data.articleTitle, data.articleDate, data.paraOne, data.paraTwo, data.paraThree));
-})
+data.map((a) => {
+    return articles.appendChild(createArticle
+    (a.articleTitle, a.articleDate, a.paraOne, a.paraTwo, a.paraThree))
+});
 
