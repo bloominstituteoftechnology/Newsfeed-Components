8f34d9d33c969072b080fc271d1812f444d3e91a
Rob Houck <rbhouck32@gmail.com>
2020-01-23 05:09:45 -0500

    This commit has created the component function for the newsfeed page.

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -112,3 +112,71 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const articlePanelCreator = (title, date, p1, p2, p3) => {
+  // create panel div
+  const panelDiv = document.createElement("div");
+  // add class name "article"
+  panelDiv.classList.add("article");
+
+  // create panel h2
+  const panelTitle = document.createElement("h2");
+
+  // create panel paragraph
+  const panelDate = document.createElement("p");
+  // add class name "date"
+  panelDate.classList.add("date");
+
+  // create three seperate paragraph elements
+  const paragraphOne = document.createElement("p");
+  const paragraphTwo = document.createElement("p");
+  const paragraphThree = document.createElement("p");
+
+  // create panel span
+  const panelExpandBtn = document.createElement("span");
+  // add class name "expandButton"
+  panelExpandBtn.classList.add("expandButton");
+
+  // add elements to parent container panelDiv
+  panelDiv.appendChild(panelTitle);
+  panelDiv.appendChild(panelDate);
+  panelDiv.appendChild(paragraphOne);
+  panelDiv.appendChild(paragraphTwo);
+  panelDiv.appendChild(paragraphThree);
+  panelDiv.appendChild(panelExpandBtn);
+
+  // addcontent to the created elements
+  panelDate.textContent = date;
+  panelTitle.textContent = title;
+  paragraphOne.textContent = p1;
+  paragraphTwo.textContent = p2;
+  paragraphThree.textContent = p3;
+
+  // add functionality to the expandbutton span
+
+  panelDiv.addEventListener("click", () => {
+    panelDiv.classList.toggle("article-open");
+  });
+
+  // define panelDiv
+  return panelDiv;
+};
+// define parentContainer so you can attach panel Component
+const parentComponent = document.querySelector(".articles");
+
+data.forEach(item => {
+  const newPanel = articlePanelCreator(
+    item.title,
+    item.date,
+    item.firstParagraph,
+    item.secondParagraph,
+    item.thirdParagraph
+  );
+  parentComponent.appendChild(newPanel);
+});
+
+// // define child you are appending to parentContainer
+// const panelComponent = articlePanelCreator();
+
+// // add panel to parentContainer
+// parentComponent.appendChild(panelComponent);
