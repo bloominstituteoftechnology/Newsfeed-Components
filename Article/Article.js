5b7c04a0ad28778f47da9072f08f99a4e913a397
Mosharraf Musa <mosharrafny@gmail.com>
2020-02-18 20:03:02 -0500

    MVP part-2 Add Article

--- Article/Article.js
+++ Article/Article.js
@@ -85,6 +85,25 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+
+  {
+    title: 'Added New Article',
+    date: 'Feb 18, 2020',
+    firstParagraph: `I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. 
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. `,
+
+    secondParagraph: `I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. 
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. `,
+
+    thirdParagraph: `I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component.
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. 
+    I am excited to see the power of component. I am excited to see the power of component. I am excited to see the power of component. `,
   }
 ];
 
