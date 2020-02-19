4db7bef2467ed4cfd998a10a61270ab75dff18ea
Mirage Kamran <55200336+miragekamran@users.noreply.github.com>
2020-02-18 18:16:10 -0800

    project updated

--- Article/Article.js
+++ Article/Article.js
@@ -112,6 +112,10 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const articles = document.querySelector('.articles');
+console.log(articles);
+
 // Step 1: Create a function that creates a component.
 const componentCreator = (title, date, p1, p2, p3) => {
 
