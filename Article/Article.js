fea66b70335701688a206310f1b3ccacb01394a0
brianaabreu <briana.abreu01@gmail.comgit config --global user.email briana.abreu01@gmail.comgit config --global user.name brianaabreugit config --global user.email briana.abreu01@gmail.com>
2020-02-20 21:37:09 -0800

    finished MVP

--- Article/Article.js
+++ Article/Article.js
@@ -1,8 +1,9 @@
 /* This is the data we will be using to create our article components */
 /* Look over this data, then proceed to line 91*/
-const data = [{
+const data = [
+  {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -22,8 +23,8 @@ const data = [{
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -42,8 +43,8 @@ const data = [{
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -70,8 +71,8 @@ const data = [{
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -84,6 +85,16 @@ const data = [{
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "Briana",
+    date: "May 22, 2001",
+
+    firstParagraph: "lorem",
+
+    secondParagraph: "Lorem ipsum dolor sit amet",
+
+    thirdParagraph: "Lorem ipsum dolor sit amet"
   }
 ];
 
@@ -112,57 +123,57 @@ const data = [{
 
 */
 
-function article(title, date, p1, p2, p3) {
-  //creating elements
-  const article = document.createElement('div');
-  const title = document.createElement('h2');
-  const date = document.createElement('p');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p');
-  const p3 = document.createElement('p');
-  const expandSpan = document.createElement('span');
+const article = (
+  title,
+  date,
+  firstParagraph,
+  secondParagraph,
+  thirdParagraph
+) => {
+  // Create Elements
+  const article = document.createElement("div");
+  const articleTitle = document.createElement("h2");
+  const articleDate = document.createElement("p");
+  const firstP = document.createElement("p");
+  const secondP = document.createElement("p");
+  const thirdP = document.createElement("p");
+  const expandBtn = document.createElement("p");
 
-  expandSpan.addEventListener('click', (e) => {
-    article.classList.toggle("article-open")
+  //Append
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(firstP);
+  article.appendChild(secondP);
+  article.appendChild(thirdP);
+  article.appendChild(expandBtn);
 
-    article.appendChild(title);
-    article.appendChild(date);
-    article.appendChild(p1);
-    article.appendChild(p2);
-    article.appendChild(p3);
-    article.appendChild(expandSpan);
+  article.classList.add("article");
+  articleDate.classList.add("date");
+  expandBtn.classList.add("expandButton");
 
-    article.classList.add('article');
-    date.classList.add('date');
-    expandSpan.classList.add('expandButton');
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  firstP.textContent = firstParagraph;
+  secondP.textContent = secondParagraph;
+  thirdP.textContent = thirdParagraph;
+  expandBtn.textContent = "Expand";
 
-    title.textContent = title;
-    date.textContent = date;
-    p1.textContent = p1;
-    p2.textContent = p2;
-    p3.textContent = p3;
-    expandSpan.textContent = 'Click to Expand';
-
+  expandBtn.addEventListener("click", () => {
+    article.classList.toggle("article-open");
   });
 
   return article;
-}
+};
 
-data.map((article) => {
-  return articles.appendChild(article(article.title, article.date, article.firstParagraph, article.secondParagraph, article.thirdParagraph))
-})
-
-let add = {
-  title: "Briana",
-  date: "May 22, 2001",
-
-  firstParagraph: "lorem",
-
-  secondParagraph: "Lorem ipsum dolor sit amet",
-
-  thirdParagraph: "Lorem ipsum dolor sit amet",
-}
-
-add.map((article) => {
-  return articles.appendChild(componentCreator(article.title, article.date, article.firstParagraph, article.secondParagraph, article.thirdParagraph))
-})
+const show = document.querySelector(".articles");
+data.forEach(item => {
+  show.appendChild(
+    article(
+      item.title,
+      item.date,
+      item.firstParagraph,
+      item.secondParagraph,
+      item.thirdParagraph
+    )
+  );
+});
