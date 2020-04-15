82081b3179d467fc3643f3fd6c62e3642b4bec57
Cassandra Dew <cpdew2@gmail.com>
2020-04-14 21:18:23 -0400

    complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,19 @@ let menuItems = [
   'Log Out'
 ];
 
+menuItems.forEach((link) => {
+  const item = document.createElement(li);
+  item.textContent = link;
+  ListOfLinks.appendChild(item);
+  item.style.cursor ='pointer';
+})
+
+navDiv.classList.add('menu');
+menuButton.addEventListener('click', (e) => {
+  navDiv.classList.toggle('menu--open');
+
+})
+return navDiv;
 /* 
 
   Step 1: Write a function that will create a menu component as seen below:
