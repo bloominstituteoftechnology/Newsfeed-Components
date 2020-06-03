309875cf970d822664b1b0046e66423d12149281
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-03 13:26:21 -0600

    wip

--- Article/Article.js
+++ Article/Article.js
@@ -111,3 +111,18 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+function articleMaker(articleData) {
+  const article = document.createElement('div')
+  const title = document.createElement('h2')
+  const date = document.createElement('p')
+
+  const paragraphData = [
+    articleData.firstParagraph,
+    articleData.secondParagraph,
+    articleData.thirdParagraph
+  ]
+
+  const paragraphs = paragraphData.map(paragraph => document.createElement('p'))
+}
+
+const articles = articleMaker(data[0]);
