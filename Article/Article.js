262bce861bf3c868997e89cde92488c301897285
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-06-10 23:57:53 -0700

    added a new article

--- Article/Article.js
+++ Article/Article.js
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Ahmad Sayadi  Lambda School Full Stock Wev Development Student in 2020',
+    date: 'June 10th 2020',
+    firstParagraph: `FSWD`,
+
+    secondParagraph: `Lambda School`,
+
+    thirdParagraph: `Lambda School`
   }
 ];
 
