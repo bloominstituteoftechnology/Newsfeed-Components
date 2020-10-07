d317dc5300941a33193e3f2f534a05264a90075a
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-10-06 18:32:33 -0600

    updated

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+    'Students',
+    'Faculty',
+    "What's New",
+    'Tech Trends',
+    'Music',
+    'Log Out'
 ];
 
 /* 
