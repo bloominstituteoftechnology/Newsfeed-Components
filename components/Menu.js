6ed6556c00a08770655eee5b47a86aac3ceb9fb5
Diego Roman <diegormnv209@gmail.com>
2020-10-28 18:07:01 -0700

    mostly done with menu and article

--- components/Menu.js
+++ components/Menu.js
@@ -24,51 +24,38 @@ let menuItems = [
     //created elements
     const menu = document.createElement('div');
     const list = document.createElement('ul');
-    const item1 = document.createElement('li');
-    const item2 = document.createElement('li');
-    const item3 = document.createElement('li');
-    const item4 = document.createElement('li');
-    const item5 = document.createElement('li');
-    const item6 = document.createElement('li');
+    
 
     //set structure
     menu.appendChild(list);
-    list.appendChild(item1);
-    list.appendChild(item2);
-    list.appendChild(item3);
-    list.appendChild(item4);
-    list.appendChild(item5);
-    list.appendChild(item6);
 
     //Gave clasification to appropriate elements
     menu.classList.add('menu')
 
-    //Gave text content for list items
-    item1.textContent = menuItems[0];
-    item2.textContent = menuItems[1]
-    item3.textContent = menuItems[2]
-    item4.textContent = menuItems[3]
-    item5.textContent = menuItems[4]
-    item6.textContent = menuItems[5]
-
-
-    return menu;
-
-  }
-
-
   /*Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
   */
+    menuItems.forEach((items) =>{
+      const item = document.createElement('li');
+      item.textContent = items;
+      list.appendChild(item);
+    });
 
   //Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+    const menuButton = document.querySelector('.menu-button');
 
 
   //Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
-
+    menuButton.addEventListener('click', (event) =>{
+      menu.classList.toggle('menu--open');
+    })
 
   //Step 5: Don't forget to return your div.menu.
+return menu;
+}
 
+const dropDown = document.querySelector('.header');
+dropDown.appendChild(menuMaker(menuItems));
 
   //Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 
