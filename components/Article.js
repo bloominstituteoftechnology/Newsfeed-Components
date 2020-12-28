43295e78247015db6c384060ddfe912065650f3e
Vanessa Dixon <vanessazdixon@gmail.com>
2020-12-28 00:54:41 -0500

    completed mvp

--- components/Article.js
+++ components/Article.js
@@ -1,6 +1,4 @@
-// This is the data we will be using to create our articles. Look at it, then proceed to line 93.
-// OPTIONAL: if you're feeling adventurous, try to make this data an export from a different module, and import it here.
-// You can read about ES6 modules here: https://exploringjs.com/es6/ch_modules.html#sec_basics-of-es6-modules
+
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
