f2b16c336d4919c9586bb8ad5f8c0a63a1cb0bcb
Tarah <tarah.agbokhana@gmail.com>
2021-02-11 01:36:10 -0500

    First & Final Commit

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,6 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
-let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
-];
-
+import menuItems from './MenuItems';
 /* 
   Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
 
@@ -31,3 +23,31 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+const header = document.createElement('header');
+const navbar = document.createElement('nav');
+const menu_ul = document.createElement('ul');
+const btn = document.querySelector('.menu-button');
+const div_header = document.querySelector('.header');
+const body = document.querySelector('body');
+const menu_div = document.createElement('div');
+
+function menuMaker(array) {
+  array.forEach(item => {
+    const menu_li = document.createElement('li');
+    menu_li.textContent = item;
+    menu_ul.appendChild(menu_li);
+ //   console.log(menu_li);
+  });
+  body.prepend(header);
+  header.appendChild(div_header);
+  header.appendChild(menu_div);
+  menu_div.appendChild(navbar);
+  navbar.appendChild(menu_ul);
+  menu_div.classList.add('menu');
+}
+menuMaker(menuItems);
+
+btn.addEventListener('click', (event) => {
+  console.log("button clicked!")
+  menu_div.classList.toggle('menu--open');
+})
---
+++ components/MenuItems.js
@@ -0,0 +1,9 @@
+let menuItems = [
+    'Students',
+    'Faculty',
+    "What's New",
+    'Tech Trends',
+    'Music',
+    'Log Out'
+  ];
+  export default menuItems;
