e315c030ec588a8a89fb79094c94a357c78e33cb
GregoryPWalton <75237506+GregoryPWalton@users.noreply.github.com>
2021-03-10 19:42:19 -0800

    gregory-walton-reset

--- components/Article.js
+++ components/Article.js
@@ -93,7 +93,11 @@ const data = [
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
-
+    
+      
+    
+  
+  
   <div class="article"> 
     <h2>{title of the article}</h2> 
     <p class="date">{date of the article}</p> 
@@ -102,7 +106,10 @@ const data = [
  
     <span class="expandButton">+</span> 
   </div> 
- 
+     
+    
+  
+  
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
 
@@ -114,5 +121,50 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
-
+    
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+      
+    
+  
+  
 
