6e933e8725b4e0326e2f1ac569919aeb0d009aa9
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-04-07 13:36:02 -0400

    create nodes

--- components/Article.js
+++ components/Article.js
@@ -92,25 +92,42 @@ const data = [
 /*
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
+  and returns a DOM node looking like the one below:*/
+  const articleMaker = obj =>{
+    //<div class="article">
+    const articleDiv = document.createElement('div');
+    //   <h2>{title of the article}</h2>
+    const title = document.createElement('h2');
+    //   <p class="date">{date of the article}</p>
+    const date = document.createElement('p');
+    //   {three separate paragraph elements}
+    const pElements = document.createElement('p');
+    //   <span class="expandButton">+</span>
+    const button = document.createElement('span');
+  }
 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
+  // <div class="article">
+  //   <h2>{title of the article}</h2>
+  //   <p class="date">{date of the article}</p>
 
-    {three separate paragraph elements}
+  //   {three separate paragraph elements}
 
-    <span class="expandButton">+</span>
-  </div>
-
+  //   <span class="expandButton">+</span>
+  // </div>
+/*
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
-  This listener should toggle the class 'article-open' on div.article.
+  This listener should toggle the class 'article-open' on div.article.*/
+/*
 
   Step 3: Don't forget to return something from your function!
+  */
 
+/*
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   to create a div.article element and append it to the DOM inside div.articles (see index.html).
+  */
 
+/*
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
