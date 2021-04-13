29ec6e8316d36f68b91c14874c68ef490965bfd8
josh jacques <jp.jacques@yahoo.com>
2021-04-13 16:25:15 -0500

    added article

--- components/Article.js
+++ components/Article.js
@@ -86,6 +86,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
     Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
     Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'pineapple on pizza is wrong',
+    date: 'Jan 41st, 1679',
+    firstParagraph: `why would you do that to a pizza `,
+
+    secondParagraph: `that just ain't right`,
+
+    thirdParagraph: `boycott pineapple pizza!`
   }
 ];
 
