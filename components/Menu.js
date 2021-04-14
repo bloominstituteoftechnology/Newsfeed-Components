e6207caf154033a78476258c37d2eca49f3e97af
josh jacques <jp.jacques@yahoo.com>
2021-04-13 19:53:47 -0500

    everything is being put together properly, adding the class list menu--open does nothing

--- components/Menu.js
+++ components/Menu.js
@@ -9,25 +9,55 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
-  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
+/*
+Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as an <li>}
-    </ul>
-  </div>
+<div class="menu">
+<ul>
+{each menu item as an <li>}
+</ul>
+</div>
 
-  The 'menuMaker' takes an array of menu items as its only argument.
+The 'menuMaker' takes an array of menu items as its only argument.
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+Add those items to the <ul>
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
 
-  Step 5: Don't forget to return your div.menu.
+Step 5: Don't forget to return your div.menu.
 
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+const menuMaker = (data) => {
+  //creates elements
+  const menuDiv = document.createElement('div');
+  const menuList = document.createElement('ul');
+
+  //adds class to menuDiv
+  menuDiv.classList.add('menu');
+
+  //loops over the menu array
+  data.forEach(e => {
+    const listItem = document.createElement('li');
+    listItem.textContent = e;
+    menuList.appendChild(listItem);
+  });
+
+  //grabs the element with the menuButton class
+  const menuButton = document.querySelector('.menu-button');
+
+  menuButton.addEventListener('click', () => {
+    menuDiv.classList.toggle('menu--open');
+  });
+
+  menuButton.appendChild(menuDiv);
+  menuDiv.appendChild(menuList);
+
+  return menuDiv;
+};
+
+console.log(menuMaker(menuItems));
