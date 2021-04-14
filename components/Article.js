e866729bc883ac9071da268545c07129813639ef
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-14 15:33:58 -0500

    first commit made articleMaker component

--- components/Article.js
+++ components/Article.js
@@ -114,3 +114,15 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+function articleMaker(article){
+  const articleDiv = document.createElement('div')
+  const articleTitle = document.createElement('h2')
+
+  const articlePar1 = document.createElement('p')
+  const articlePar2 = document.createElement('p')
+  const articlePar3 = document.createElement('p')
+
+  const articleSpan = document.createElement('span')
+
+  
+}
