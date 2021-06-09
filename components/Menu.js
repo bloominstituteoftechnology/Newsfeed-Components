ee21d2d967617e53d47baca2f626eecafda676d8
unknown <victoria90dupont@gmail.com>
2021-06-08 19:40:01 -0700

    menu commit

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,21 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+function menuMaker(menuItems
+  ){
+    const menu = document.createElement('div')
+
+    const ul = document.createElement('ul')
+
+    const students1 =document.createElement('li')
+
+    const faculty1 = document.createElement('li')
+
+    const whatsNew1 = document.createElement('li')
+
+    const trends = document.createElement('li')
+
+    const music1 = document.createElement('li')
+
+    const logOut = document.createElement('li')
+ }
