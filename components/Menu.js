0e5e45958e365d8ecd1fd94b01054d81112801e3
marciemaclean <macleanphoenix@gmail.com>
2021-07-07 19:22:00 -0400

    periodic update. menuitems ~50% complete

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,6 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
-let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
-];
+let menuItems = [ '1' , '2' , '3' , '4' , '5' , '6'];
 
 /* 
   Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
@@ -32,5 +25,41 @@ let menuItems = [
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
 
+function menuMaker(menuItems){
+  const header = document.querySelector('.header');
+  const div = document.createElement('div');
+  const ulElement = document.createElement('ul');
+  
+  div.classList.add('menu');
 
-function menuMaker(menuItems){}
+  header.appendChild(div);
+  div.appendChild(ulElement);
+  
+
+  function liMaker(i){
+    const li = document.createElement('li');
+    li.textContent = menuItems[i]
+    return li;
+  }
+
+  for(let i = 0; i < menuItems.length; i++){
+    const li = document.createElement('li')
+    li.textContent = menuItems[i];
+    ulElement.appendChild(li);
+  }
+
+  const menuButton = document.querySelector('.menu-button')
+
+  menuButton.addEventListener('click', () =>{
+    div.classList.toggle('menu--open')
+  })
+
+  return div;
+}
+
+menuItems.forEach(e=> {
+  const menu = menuMaker(e);
+  document.querySelector('.menu').append(menu);
+})
+
+
