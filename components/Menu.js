5eca02de2729297bb84f440a12e49967f45f5ecb
brad <bradley.michaels@outlook.com>
2021-09-29 18:38:30 -0400

    nav on click works

--- components/Menu.js
+++ components/Menu.js
@@ -22,6 +22,7 @@ let menuItems = [
     function menuMaker(menuItems){
       
       const menu = document.createElement('div');
+      menu.classList.add('menu');
       const ul = document.createElement('ul');
       menu.appendChild(ul);
 
@@ -41,11 +42,13 @@ menuItems.forEach(element => {
 
 
 //   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
-    const menuButton = document.querySelector('menu-button');
+    const menuButton = document.querySelector('.menu-button');
 
 
 //   Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
-
+    menuButton.addEventListener('click', evt => {
+      menu.classList.toggle('menu--open');
+    })
 //   Step 5: Don't forget to return your div.menu.
       return menu;
     }
