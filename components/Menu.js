64e2a4fab0af7ffdac54b2a7e06cb1fc7b0d2b3a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-09-29 23:22:55 -0700

    finish Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -22,9 +22,10 @@ let menuItems = [
 
   function menuMaker(arr) {
 
-    const menu = document.createElement('div');
-    const list = document.createElement('ul');    
-  
+    const menuDiv = document.createElement('div');
+    const list = document.createElement('ul');  
+    menuDiv.appendChild(list);  
+
 /*
   Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul> */
@@ -36,15 +37,27 @@ let menuItems = [
       list.appendChild(x[i]);
   }
 
+  menuDiv.classList.add('menu');
+
 /*
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button'). */
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  const menuSect = document.querySelector('.menu-button');  
+  
+  // menuSect.appendChild(menuDiv);   
 
+/*
+  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class). */
+  
+  menuSect.addEventListener('click', evt => {    
+    menuDiv.classList.toggle('menu--open');
+  })    
+
+/* 
   Step 5: Don't forget to return your div.menu. */
-
-  return menu;
-  }
+  
+  return menuDiv;
+  }  
 
   const testMenu = menuMaker(menuItems);
 
@@ -53,3 +66,18 @@ let menuItems = [
 /*
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+const menuItem = document.querySelector('.header');
+
+const menuElements = menuMaker(menuItems);
+
+console.log('menuElements: ', menuElements);
+
+menuItem.appendChild(menuElements);
+
+
+// const headerDiv = document.querySelector('.header');
+// // console.log(headerDiv)
+// const menuEl = menuMaker(menuItems);
+// //console.log(menuEl)
+// headerDiv.appendChild(menuEl);
