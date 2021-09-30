420db160b2c9b3a7ea38b57e121954d2b054c9f6
arsalan rahimi <years1373@yahoo.com>
2021-09-29 18:02:10 -0700

    finished

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,50 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+const header = document.querySelector(‘.header’)
+const image = document.querySelector(‘.menu-button’)
+const menu = document.createElement(‘div’)
+menu.classList.add(‘menu’)
+header.appendChild(menu)
+const list = document.createElement(‘ul’)
+menu.appendChild(list)
+image.addEventListener(‘click’, (event) => {
+    menu.classList.toggle(‘menu--open’)
+})
+function menuMaker(items) {
+    let listItem = document.createElement(‘li’)
+    listItem.textContent = items
+    return listItem
+}
+menuItems.forEach(item => {
+    const newList = menuMaker(item)
+    list.appendChild(newList)
+    // console.log(newList)
+})
+const menuButton = document.querySelector(‘.menu-button’);
+const header = document.querySelector(‘.header’)
+const menu = document.createElement(‘div’);
+header.appendChild(menu);
+menu.classList.add(‘menu’);
+const list = document.createElement(‘ul’);
+menu.appendChild(list);
+function menuMaker(menutxt) {
+const menuItem = document.createElement(‘li’);
+
+list.appendChild(menuItem);
+
+menuItem.textContent = menutxt
+
+return menuItem;
+}
+menuButton.addEventListener(‘click’,event => {
+  menu.classList.toggle(‘menu--open’)
+})
+
+const newMenu = menuItems.map(item => {
+  return menuMaker(item)
+})
+newMenu.forEach(item => {
+  list.appendChild(item)
+})
