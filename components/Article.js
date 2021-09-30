40aa14a8a4fe2642de2dd1e664bc7626267fdfa4
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-09-29 20:30:51 -0700

    update Step 5 on  Article.js

--- components/Article.js
+++ components/Article.js
@@ -23,8 +23,11 @@ function articleMaker(data) {
     articleDate.textContent = data.date;
     paragraphs.innerHTML = ` 
     <div>   
+    
       <div>${data.firstParagraph}</div>
+
       <div>${data.secondParagraph}</div>
+
       <div>${data.thirdParagraph}<div>
     </div>`;
     expandButton.textContent = '+';
@@ -69,6 +72,58 @@ console.log('testArticle: ', testArticle);
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
   to create a div.article element and append it to the DOM inside div.articles (see index.html). */
 
+  // const articleElements = articleData.map(elem => {
+  //   return articleMaker(elem);
+  // })
+  
+  // console.log(articleElements);
+  
+  // articleElements.forEach(elemToAdd => {
+  //   articles.appendChild(elemToAdd);
+  // });
+
+/*
+  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  Refresh the page to see the new article.
+*/
+
+  function addArticle(data, elem) {
+    data.push(elem);
+    return data;
+  };
+
+    const newAddelement = {
+      title: 'The future of the React, Angular, Vue',
+      date: 'June 7th, 2019',
+      firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Squirtle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wartortle Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Blastoise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Caterpie Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Metapod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Butterfree
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit.`,
+  
+      secondParagraph: `Pidgey Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Fearow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ekans Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Arbok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pikachu Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Raichu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sandshrew Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Sandslash Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
+  
+      thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
+          Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
+          Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
+
+  }
+
+  console.log('articleData: ', addArticle (articleData, newAddelement));
+  
   const articleElements = articleData.map(elem => {
     return articleMaker(elem);
   })
@@ -78,8 +133,3 @@ console.log('testArticle: ', testArticle);
   articleElements.forEach(elemToAdd => {
     articles.appendChild(elemToAdd);
   });
-
-/*
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
-  Refresh the page to see the new article.
-*/
