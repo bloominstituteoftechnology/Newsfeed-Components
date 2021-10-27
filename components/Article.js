ffdab7916c277fa876073d9cd555af2382730c29
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-10-27 15:03:28 -0600

    sarted

--- components/Article.js
+++ components/Article.js
@@ -93,7 +93,7 @@ const data = [
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
-
+ 
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
@@ -114,3 +114,9 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+//Step 1
+function articleMaker({title, date, firstParagraph, secondParagraph, thirdParagraph}){
+  const divv = document.createElement('div');
+  const h2 = document.createElement('h2');
+}
