ec5b903a150514b367e65bdd19434b468b819a64
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-12-01 16:49:07 -0500

    created menu component and appended it to header

--- components/Menu.js
+++ components/Menu.js
@@ -9,25 +9,88 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
-  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as an <li>}
-    </ul>
-  </div>
+  // Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
 
-  The 'menuMaker' takes an array of menu items as its only argument.
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as an <li>}
+  //   </ul>
+  // </div>
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  // The 'menuMaker' takes an array of menu items as its only argument.
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  // // function
 
-  Step 5: Don't forget to return your div.menu.
+  // Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+  // Add those items to the <ul>
 
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
-*/
+  // Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+
+  // Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+
+  // Step 5: Don't forget to return your div.menu.
+
+  // Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+
+const menuMaker = menuArr => {
+  // create elements
+  const menuDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+
+  // create loop to create li elements & setup structure in loop
+  menuArr.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    ul.appendChild(li);
+  });
+
+// setup structure of remaining elements
+  menuDiv.appendChild(ul);
+
+// add classnames
+  menuDiv.classList.add('menu');
+ 
+// create button with click toggle event
+  const menuBtn = document.querySelector('.menu-button');
+  menuBtn.addEventListener('click', () => {
+    menuDiv.classList.toggle('menu--open');
+  })
+  return menuDiv;
+}
+
+const newMenu = menuMaker(menuItems);
+const header = document.querySelector('.header');
+header.appendChild(newMenu);
+console.log(newMenu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // function menuMaker(menuArr) {
+  //   const menu = document.createElement('div');
+  //   const menuList = document.createElement('ul');
+  //   menuArr.forEach(menuItem => {
+  //     const listItem = document.createElement('li');
+  //     listItem.textContent = menuItem;
+  //     menuList.appendChild(listItem)
+  //   });
+  //   menu.appendChild(menuList);
+
+  // }
