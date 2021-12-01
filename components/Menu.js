e09e9fa079239fabd03d9ed2d20cc76f2818eaed
John S <johnc.sime@gmail.com>
2021-12-01 17:54:15 -0500

    added the function articleMaker

--- components/Menu.js
+++ components/Menu.js
@@ -32,27 +32,27 @@ let menuItems = [
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
 
-const menuButton = document.querySelector('.menu-button');
-function menuMaker(text) {
-  const menuTable = document.createElement('div');
-  const menuList = document.createElement('ul');
-  const things = document.createElement('li');
+// const menuButton = document.querySelector('.menu-button');
+// function menuMaker(text) {
+//   const menuTable = document.createElement('div');
+//   const menuList = document.createElement('ul');
+//   const things = document.createElement('li');
  
-  menuTable.appendChild(menuList);
-  menuList.appendChild(things);
+//   menuTable.appendChild(menuList);
+//   menuList.appendChild(things);
  
-  menuTable.classList.add('menu');
+//   menuTable.classList.add('menu');
  
-  things.textContent = text;
+//   things.textContent = text;
  
-  menuButton.addEventListener('click', () => {
-    menuTable.classList.toggle('menu--open');
-  });  
+//   menuButton.addEventListener('click', () => {
+//     menuTable.classList.toggle('menu--open');
+//   });  
  
-  return menuTable;
-}
+//   return menuTable;
+// }
 
-menuItems.forEach((elem) => {
-  let newElem = menuMaker(elem);
-  document.querySelector('.header').appendChild(newElem);
-});
+// menuItems.forEach((elem) => {
+//   let newElem = menuMaker(elem);
+//   document.querySelector('.header').appendChild(newElem);
+// });
