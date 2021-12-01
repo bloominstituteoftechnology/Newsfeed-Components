c4a64ec0c0f5d121c8acfcc50f7636b5704d35cb
Andrew Gary <andrew.gary91@gmail.com>
2021-12-01 14:40:15 -0600

    finished all the goals, going to work on strech goals tonight after I get off work

--- components/Menu.js
+++ components/Menu.js
@@ -18,9 +18,51 @@ let menuItems = [
     </ul>
   </div>
 
-  The 'menuMaker' takes an array of menu items as its only argument.
+  The 'menuMaker' takes an array of menu items as its only argument.*/
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+function menuMaker(menuItemsArray){
+  const panel = document.createElement('div');
+  const panelList = document.createElement('ul');
+  const panelItem = document.createAttribute('li');
+  console.log(panel);
+
+  panel.appendChild(panelList);
+  // panelList.appendChild(panelItem);
+
+  panel.classList.add('menu');
+
+  menuItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    panelList.appendChild(listItem);
+
+  });
+
+  const button = document.querySelector('.menu-button');
+  button.addEventListener('click', function(e) {
+    const idk = document.querySelector('div .menu');
+
+    if(idk.style.display === 'none'){
+      idk.style.display = '';
+    }else {
+      idk.style.display = 'none';
+    }
+    // idk.style.display = 'true';
+    // console.log(idk.style.display);
+    // idk.style.display = 'none';
+    // console.log(idk.style.display);
+    
+  })
+
+  return panel;
+}
+
+const letsSee = menuMaker(menuItems);
+const header = document.querySelector('.header');
+header.append(letsSee);
+
+
+  /*Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
