2c8e751c98d4fbfc8e7492fe2923c4f2e67336b1
Brittany Petersen <britt.petersen100@gmail.com>
2021-12-01 15:54:57 -0700

    added toggle code

--- components/Article.js
+++ components/Article.js
@@ -122,7 +122,7 @@ const articleMaker = artObj => {
   const paraTwo = document.createElement('p');
   const paraThree = document.createElement('p');
   const expandButtonSpan = document.createElement('span');
-  const expandButton = document.createElement('button');
+
 
   article.classList.add('article');
   date.classList.add('date');
@@ -139,6 +139,33 @@ const articleMaker = artObj => {
   paraTwo.textContent = artObj.secondParagraph;
   paraThree.textContent = artObj.thirdParagraph;
 
+  expandButtonSpan.textContent = '+';
+
+  // Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+  // This listener should toggle the class 'article-open' on div.article.
+  
+  expandButtonSpan.addEventListener('click', () => {
+    article.toggle('article-open');
+  })
+
+
+
+  // Step 3: Don't forget to return something from your function!
+
+
 
   return article;
 }
+
+
+
+
+
+  // Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+  // to create a div.article element and append it to the DOM inside div.articles (see index.html).
+
+
+
+
+  // Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  // Refresh the page to see the new article.
